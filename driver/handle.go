9731d4ddc379de4262a8b7171cf586ab508b8f2c
cneira <cneirabustos@gmail.com>
2019-11-10 16:30:34 -0300

    sync license

--- driver/handle.go
+++ driver/handle.go
@@ -1,9 +1,24 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Copyright (c) 2019, Carlos Neira cneirabustos@gmail.com
+/* Firecracker-task-driver is a task driver for Hashicorp's nomad that allows
+ * to create microvms using AWS Firecracker vmm
+ * Copyright (C) 2019  Carlos Neira cneirabustos@gmail.com
+ * 
+ * This file is part of Firecracker-task-driver.
+ * 
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Firecracker-task-driver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Firecracker-task-driver. If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 
 package firevm
 
