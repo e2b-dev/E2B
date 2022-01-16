bd153ff0a31ed81a2cd475cab7f184320b08f0c5
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-01-16 10:23:20 +0100

    Add React example project

---
+++ examples/react-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { Devbook } from '@devbookhq/dbk-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
