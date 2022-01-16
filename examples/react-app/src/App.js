a5305c06588f9b5633cfe43090e2d2834b08f1f5
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-01-16 10:29:54 +0100

    Uninstall react in the SDK folder

--- examples/react-app/src/App.js
+++ examples/react-app/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Devbook
+          code="console.log('Hello')"
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
