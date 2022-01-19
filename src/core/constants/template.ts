0528e652087a340444c08795d251a09e0cc8d985
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-01-19 10:34:25 +0100

    Change names of types and fields to be less confusing for users

--- src/core/constants/template.ts
+++ src/core/constants/template.ts
@@ -1,13 +1,19 @@
-import { TemplateConfig } from '../../common-ts/TemplateConfig'
+import { TemplateConfig } from 'src/common-ts/TemplateConfig'
 
-export type TemplateID = 'nodejs-v16'
+/**
+ * Normally, we would name this enum `TemplateID` but since this enum is exposed to users
+ * it makes more sense to name it `Env` because it's less confusing for users.
+ */
+export enum Env {
+  NodeJS = 'nodejs-v16',
+}
 
-export const templates: { [key in TemplateID]: TemplateConfig & { command: string } } = {
+export const templates: { [key in Env]: TemplateConfig & { command: string } } = {
   'nodejs-v16': {
-    'id': 'nodejs-v16',
-    'image': 'us-central1-docker.pkg.dev/devbookhq/devbook-runner-templates/nodejs-v16:latest',
-    'root_dir': '/home/runner',
-    'code_cells_dir': '/home/runner/src',
-    'command': 'node -e ',
+    id: 'nodejs-v16',
+    image: 'us-central1-docker.pkg.dev/devbookhq/devbook-runner-templates/nodejs-v16:latest',
+    root_dir: '/home/runner',
+    code_cells_dir: '/home/runner/src',
+    command: 'node -e ',
   },
 }
--- src/core/devbook.ts
+++ src/core/devbook.ts
@@ -1,6 +1,6 @@
 import { makeIDGenerator } from 'src/utils/id'
 import {
-  TemplateID,
+  Env,
   templates,
 } from './constants'
 import Runner from './runner'
@@ -10,7 +10,7 @@ import EvaluationContext from './evaluationContext'
 const generateExecutionID = makeIDGenerator(6)
 
 interface Opts {
-  templateID: TemplateID
+  env: Env
   onStdout?: (stdout: string) => any
   onStderr?: (stderr: string) => any
   onEnvChange?: (env: RunningEnvironment) => any
@@ -19,12 +19,12 @@ interface Opts {
 
 class Devbook {
   private readonly context: EvaluationContext
-  private readonly templateID: TemplateID
+  private readonly env: Env
   private readonly executionID: string
   private readonly contextID = 'default'
 
   constructor({
-    templateID,
+    env,
     onStderr,
     onStdout,
     onEnvChange,
@@ -32,7 +32,7 @@ class Devbook {
   }: Opts) {
     if (!Runner.obj) throw new Error('Runner is not defined')
 
-    this.templateID = templateID
+    this.env = env
     const executionID = generateExecutionID()
     this.executionID = executionID
 
@@ -52,14 +52,14 @@ class Devbook {
     })
 
     this.context.createRunningEnvironment({
-      templateID,
+      templateID: env,
     })
   }
 
   evaluate(code: string) {
-    const command = `${templates[this.templateID].command}"${code}"`
+    const command = `${templates[this.env].command}"${code}"`
     this.context.executeCommand({
-      templateID: this.templateID,
+      templateID: this.env,
       executionID: this.executionID,
       command,
     })
