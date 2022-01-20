5b5560ca6f2420df3c2b4c5df6567d127935efed
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-01-20 12:25:43 +0000

    Add docusaurus component with Devbook

---
+++ examples/docusaurus/src/components/DevbookView/index.tsx
@@ -0,0 +1,50 @@
+import React, {
+  useState,
+  useCallback,
+} from 'react';
+
+import { useDevbook, Env } from '@devbookhq/sdk';
+import Splitter from '@devbookhq/splitter';
+
+import Editor from './Editor';
+import Output from './Output';
+
+
+interface Props {
+  code?: string
+}
+
+function App({ code: initialCode = '' }: Props) {
+  const [sizes, setSizes] = useState([50, 50]);
+  const [code, setCode] = useState(initialCode);
+
+  const { stderr, stdout, runCode } = useDevbook({ debug: true, env: Env.NodeJS });
+
+  const handleEditorChange = useCallback((content: string) => {
+    setCode(content);
+  }, [setCode]);
+
+  return (
+    <div className="">
+      <button className="run-btn" onClick={() => runCode(code)}>Run</button>
+      <Splitter
+        classes={['flex', 'flex']}
+        initialSizes={sizes}
+        onResizeFinished={(_, sizes) => {
+          setSizes(sizes);
+        }}
+      >
+        <Editor
+          initialCode={initialCode}
+          onChange={handleEditorChange}
+        />
+        <Output
+          stdout={stdout}
+          stderr={stderr}
+        />
+      </Splitter>
+    </div >
+  );
+}
+
+export default App;
