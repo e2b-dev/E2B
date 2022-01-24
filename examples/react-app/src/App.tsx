bde8f9eaf03af5766edb6dc5e22696beb996f0cb
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-01-24 10:12:48 +0100

    Add command execution to react example

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -18,22 +18,47 @@ const initialCode = `const os = require('os');
 console.log('Hostname:', os.hostname());
 console.log(process.env)`
 
+const initialCmd =
+`ls -l
+`
+
 function App() {
   const [sizes, setSizes] = useState([50, 50]);
   const [code, setCode] = useState(initialCode);
+  const [cmd, setCmd] = useState(initialCmd);
+  const [execType, setExecType] = useState('code');
 
-  const { stderr, stdout, runCode, status } = useDevbook({ debug: true, env: Env.NodeJS });
+  const { stderr, stdout, runCode, runCmd, status } = useDevbook({ debug: true, env: Env.NodeJS });
 
   const handleEditorChange = useCallback((content: string) => {
-    setCode(content);
-  }, [setCode]);
+    if (execType === 'code') {
+      setCode(content);
+    } else {
+      setCmd(content);
+    }
+  }, [setCode, execType]);
+
+  const run = useCallback(() => {
+    if (execType === 'code') {
+      runCode(code);
+    } else {
+      runCmd(cmd);
+    }
+  }, [runCode, runCmd, code, cmd, execType]);
+
 
   return (
     <div className="app">
       {status === DevbookStatus.Disconnected && <div>Status: Disconnected, will start VM</div>}
       {status === DevbookStatus.Connecting && <div>Status: Starting VM...</div>}
       {status === DevbookStatus.Connected && (
-        <button className="run-btn" onClick={() => runCode(code)}>Run</button>
+        <div className="controls">
+          <select value={execType} onChange={e => setExecType(e.target.value)}>
+            <option value="code">Code</option>
+            <option value="cmd">Command</option>
+          </select>
+          <button className="run-btn" onClick={run}>Run</button>
+        </div>
       )}
 
       <Splitter
@@ -44,7 +69,7 @@ function App() {
         }}
       >
         <Editor
-          initialCode={initialCode}
+          initialCode={execType === 'code' ? initialCode : initialCmd}
           onChange={handleEditorChange}
         />
         <Output
