a38ececda0729dbd7ef5fc955276cc45383fcdd8
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-05-20 17:17:10 +0000

    Add basic ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    semi: [2, 'never'],
+    quotes: [2, 'single', { avoidEscape: true }],
+  },
+};
