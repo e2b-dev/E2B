5563ead1b95b64aa2054c8dfef0999a71222b325
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-05-22 08:12:50 +0000

    Add openapi URL spec

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-export const API_URL = 'https://3000-devbookhq-orchestration-axk7sf5j4y4.ws-eu45.gitpod.io'
-
 export const SESSION_REFRESH_PERIOD = 4_000 // 4s
 
 export const SESSION_DOMAIN = 'ondevbook.com'
