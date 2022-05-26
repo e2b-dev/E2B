1dac19006a31a32a1bb5102b0d0a01c900d78e81
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-05-26 22:54:10 +0000

    Update WS handling for code run and subscriptions; Fix code escaping [WIP]

--- src/session.ts
+++ src/session.ts
@@ -1,134 +1,155 @@
 import {
   RpcWebSocketClient,
   IRpcNotification,
-} from 'rpc-websocket-client';
+} from 'rpc-websocket-client'
 
-import api, { components, getSessionURL } from './api'
+import api, {
+  components,
+  getSessionURL,
+} from './api'
 import wait from './utils/wait'
 import {
+  SESSION_DOMAIN,
   SESSION_REFRESH_PERIOD,
   WS_PORT,
 } from './constants'
+import Logger from './utils/logger'
+
+type CodeSnippetState = 'running' | 'stopped'
+
+type StateHandler = (state: CodeSnippetState) => void
+type StderrHandler = (stderr: string) => void
+type StdoutHandler = (stdout: string) => void
+
+type SubscriptionEvent = 'state' | 'stderr' | 'stdout'
+type SubscriptionHandler = StateHandler | StderrHandler | StdoutHandler
+
+type SubscriptionHandlerType = {
+  'state': StateHandler
+  'stderr': StderrHandler
+  'stdout': StdoutHandler
+}
+
+interface Subscriber {
+  id: string
+  event: SubscriptionEvent
+  handler: SubscriptionHandler
+}
 
 const getSession = api.path('/sessions').method('post').create()
 const refreshSession = api.path('/sessions/{sessionID}/refresh').method('put').create()
 
 class Session {
   private session?: components['schemas']['Session']
-  //private ws?: WebSocket.Client
-  private rpc = new RpcWebSocketClient()
-  private isRunning = false
+  private readonly rpc = new RpcWebSocketClient()
+  private subscribers: Subscriber[] = []
+  private _isActive = false
+  private readonly logger: Logger
 
-  // TODO: Type
-  private subscribers: any = {}
+  get isActive() {
+    return this._isActive
+  }
+  private set isActive(value: boolean) {
+    this._isActive = value
+  }
 
   constructor(
     private readonly codeSnippetID: string,
     private readonly onDisconnect?: () => void,
-  ) { }
+  ) {
+    this.logger = new Logger('Session', true)
+    this.logger.log(`Session for code snippet "${codeSnippetID}" initialized`)
+  }
+
+  unsubscribe(event: SubscriptionEvent, handler: SubscriptionHandler) {
+    this.subscribers = this.subscribers.filter(s => s.handler !== handler || s.event !== event)
+    this.logger.log(`Unsubscribed from event "${event}"`)
+  }
+
+  async subscribe<E extends SubscriptionEvent>(event: E, handler: SubscriptionHandlerType[E]) {
+    const id = await this.rpc.call('codeSnippet_subscribe', [event])
+
+    if (typeof id !== 'string') {
+      throw new Error(`Cannot subscribe to event ${event}`)
+    }
+
+    this.subscribers.push({
+      id,
+      event,
+      handler,
+    })
+    this.logger.log(`Subscribed to event "${event}" with id "${id}"`)
+  }
+  async run(code: string) {
+    await this.rpc.call('codeSnippet_run', [code])
+    this.logger.log('Started running code', code)
+  }
+
+  async stop() {
+    await this.rpc.call('codeSnippet_stop')
+    this.logger.log('Stopped running code')
+  }
 
   disconnect() {
-    if (this.isRunning) {
-      this.isRunning = false
-      this.ws?.close()
+    if (this.isActive) {
+      this.isActive = false
+      this.rpc.ws.close()
       this.onDisconnect?.()
+      this.logger.log('Disconected from the session')
     }
   }
-
-  //call(method: string, params: any[]) {
-  //  return this.rpc.call(method, params)
-  //}
-
-  async subscribe(event: string, cb: () => void) {
-    const subID: string | undefined = await this.rpc.call('codeSnippet_subscriber', [event])
-    if (subID) {
-      // TODO: Save cb for subscription ID
-
-    }
-  }
-
-  unsubscribe(subscriptionID: string) {
-  }
-
-
-
-
-  // Stops the running process that's executing code.
-  stop() {
-  }
-
-  // Starts executing the code.
-  run() {
-  }
-
 
   async connect() {
-    if (this.isRunning || !!this.session) {
-      return
+    if (this.isActive || !!this.session) {
+      throw new Error('Session.connect was already called')
     } else {
-      this.isRunning = true
+      this.isActive = true
     }
+
 
     const res = await getSession({ codeSnippetID: this.codeSnippetID })
     this.session = res.data
+    this.logger.log('Aquired session:', this.session)
+
     this.refresh(this.session.sessionID)
 
-    //const sessionURL = `wss://${getSessionURL(this.session, WS_PORT)}`
+    // const sessionURL = `wss://${getSessionURL(this.session, WS_PORT)}`
     const sessionURL = 'ws://localhost:8010/ws'
-    this.rpc.connect(sessionURL)
+    // const sessionURL = 'wss://8010-devbookhq-orchestration-axk7sf5j4y4.ws-eu46.gitpod.io:8010/ws'
 
-    this.ws = new WebSocket.Client(sessionURL)
-    this.ws.on('update', function(...args: any) {
-      console.log({args})
-    })
+    this.rpc.onNotification.push(this.handleNotification.bind(this))
 
-    let resolveWaitForOpen: undefined | (() => void)
-    let rejectWaitForOpen: undefined | ((reason: string) => void)
-    const waitForOpen = new Promise<void>((resolve, reject) => {
-      resolveWaitForOpen = resolve
-      rejectWaitForOpen = reject
-    })
+    this.rpc.onClose(this.disconnect.bind(this))
+    this.rpc.onError(this.disconnect.bind(this))
 
-    // eslint-disable-next-line prefer-const
-    let unsubscribe: undefined | (() => void)
+    this.logger.log('Connection to session:', this.session)
+    await this.rpc.connect(sessionURL)
+    this.logger.log('Connected to session:', this.session)
+  }
 
-    const handleClose = () => {
-      unsubscribe?.()
-      this.disconnect()
-    }
-    const handleError = () => {
-      unsubscribe?.()
-      rejectWaitForOpen?.('WS connection to session failed')
-    }
-    const handleOpen = () => {
-      unsubscribe?.()
-      resolveWaitForOpen?.()
-    }
-
-    unsubscribe = () => {
-      this.ws?.off('close', handleClose)
-      this.ws?.off('error', handleError)
-      this.ws?.off('open', handleOpen)
-    }
-
-    this.ws.once('close', handleClose)
-    this.ws.once('error', handleError)
-    this.ws.once('open', handleOpen)
-
-    return waitForOpen
+  private handleNotification(data: IRpcNotification) {
+    // this.logger.log('Handling notification:', data)
+    this.subscribers
+      .filter(s => s.id === data.params?.subscription)
+      .forEach(s => s.handler(data.params?.result))
   }
 
   private async refresh(sessionID: string) {
     try {
+      this.logger.log(`Started refreshing session "${sessionID}"`)
       // eslint-disable-next-line no-constant-condition
       while (true) {
-        if (!this.isRunning) {
+        if (!this.isActive) {
           throw new Error('Cannot refresh session - it was closed')
         }
 
         await refreshSession({ sessionID })
+        this.logger.log(`Refreshed session "${sessionID}"`)
         await wait(SESSION_REFRESH_PERIOD)
       }
+    } catch (e) {
+      this.logger.log(`Refreshing session "${sessionID}" failed:`, e)
+      throw e
     } finally {
       this.disconnect()
     }
--- test/index.mjs
+++ test/index.mjs
@@ -4,18 +4,28 @@ function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function handleStateChange(state: 'running' | 'stopped') {
-}
-
 async function main() {
-  const session = new Session('GpN3WCcvIGAQ')
+  const session = new Session('test')
 
   try {
     await session.connect()
-    session.subscribe('state', handleStateChange)
+    await session.subscribe('state', (data) => {
+      console.log(data)
+    })
 
+    await session.subscribe('stderr', (data) => {
+      console.log(data)
+    })
 
-    await wait(3000)
+    await session.subscribe('stdout', (data) => {
+      console.log(data)
+    })
+
+    String.raw
+    await session.run('console.log("\n4")')
+
+    console.log('run')
+    await wait(4000)
 
   } catch (e) {
     console.error(e)
