a067756a06225eab52f0230e2b8b6f341d105d5e
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-06-02 13:50:19 +0000

    Update example

--- examples/index.mjs
+++ examples/index.mjs
@@ -1,35 +1,52 @@
 import { Session } from '../dist/cjs/index.js'
 
 async function main() {
-  const session = new Session('cCDxnr1s7su3', {
-    onStateChange(state) {
-      console.log(state)
+  const session = new Session({
+    id: 'aykyOKeGxc0h',
+    codeSnippet: {
+      onStateChange(state) {
+        console.log(state)
+      },
+      onStderr(stderr) {
+        console.log(stderr)
+      },
+      onStdout(stdout) {
+        console.log(stdout)
+      },
     },
-    onStderr(stderr) {
-      console.log(stderr)
-    },
-    onStdout(stdout) {
-      console.log(stdout)
-    },
-  },
-    true,
-  )
+    debug: true,
+    saveFSChanges: true,
+  })
 
   try {
-    await session.connect()
+    await session.open()
+    await session.codeSnippet.run(String.raw`
+    const fs = require('fs')
 
-    await session.run(String.raw`
-    const http = require('http');
+    setInterval(() => {
+      try {
+        const rest = fs.readFileSync('/test.file').toString()
+        console.log('file', rest)
+      } catch (e) {
+        console.error('no file')
+      }
+    }, 2000)
+    
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+    
+    `)
+    //     await session.codeSnippet.run(String.raw`
+    //     const http = require('http');
 
-const server = http.createServer(requestListener);
-server.listen(8080, () => {
-  console.log('Listening on 8080')
-});`)
+    // const requestListener = function (req, res) {
+    //   res.writeHead(200);
+    //   res.end('Hello, World!');
+    // }
+
+    // const server = http.createServer(requestListener);
+    // server.listen(8080, () => {
+    //   console.log('Listening on 8080')
+    // });`)
 
   } catch (e) {
     console.error(e)
