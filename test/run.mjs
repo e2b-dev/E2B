672f479360ccb53cb6f182199f9597f04a48e456
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-08-21 23:31:18 +0000

    Bump version; Update testing file

--- test/run.mjs
+++ test/run.mjs
@@ -2,18 +2,18 @@ import { Session } from '../dist/cjs/index.js'
 
 async function main() {
   const session = new Session({
-    id: 'OgHOvvX1rR0o',
-    codeSnippet: {
-      onStateChange(state) {
-        console.log(state)
-      },
-      onStderr(stderr) {
-        console.log(stderr)
-      },
-      onStdout(stdout) {
-        console.log(stdout)
-      },
-    },
+    id: '1k7EcyQxhviR',
+    // codeSnippet: {
+    //   onStateChange(state) {
+    //     console.log(state)
+    //   },
+    //   onStderr(stderr) {
+    //     console.log(stderr)
+    //   },
+    //   onStdout(stdout) {
+    //     console.log(stdout)
+    //   },
+    // },
     debug: true,
     // editEnabled: true,
   })
@@ -69,22 +69,35 @@ async function main() {
     // //   console.log('Listening on 8080')
     // // });`)
 
-    const file = '/code/test.js'
-    const content = 'testing'
-    await session.filesystem.writeFile(file, content)
-    const fileContent = await session.filesystem.readFile(file)
-    console.log('file content', fileContent)
+    // const file = '/code/test.js'
+    // const content = 'testing'
+    // await session.filesystem.writeFile(file, content)
+    // const fileContent = await session.filesystem.readFile(file)
+    // console.log('file content', fileContent)
 
-    const fileNames = await session.filesystem.listAllFiles('/')
-    console.log('file names', fileNames)
+    // const fileNames = await session.filesystem.listAllFiles('/')
+    // console.log('file names', fileNames)
 
-    const term = await session.terminal.createSession(
-      (data) => console.log('terminal data', data),
-      (cps) => console.log('child processes', cps),
-      { rows: 40, cols: 90 },
-    )
-    console.log('terminalID', term.terminalID)
-    await term.sendData('sleep 10\n')
+    // const term = await session.terminal.createSession(
+    //   (data) => console.log('terminal data', data),
+    //   (cps) => console.log('child processes', cps),
+    //   { rows: 40, cols: 90 },
+    // )
+    // console.log('terminalID', term.terminalID)
+    // await term.sendData('sleep 10\n')
+
+    await session.process.start({
+      cmd: 'echo 4',
+      onExit: () => {
+        console.log('Exit')
+      },
+      onStdout: (o) => {
+        console.log(o)
+      },
+      onStderr: (o) => {
+        console.log(o)
+      },
+    })
 
   } catch (e) {
     console.error(e)
