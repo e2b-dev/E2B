4ca54ed524144df1c3cf3995bbed256c9b8bc544
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-08-25 17:58:10 +0000

    Make run example runnable

--- test/run.mjs
+++ test/run.mjs
@@ -2,12 +2,27 @@ import { Session } from '../dist/cjs/index.js'
 
 async function main() {
   const session = new Session({
+    id: 'Nodejs',
     debug: true,
+    onDisconnect() {
+      console.log('disconnect')
+    },
+    onReconnect() {
+      console.log('reconnect')
+    },
+    onClose() {
+      console.log('close')
+    },
+    // __debug_hostname: '127.0.0.1',
+    // __debug_devEnv: 'local',
   })
 
   try {
     await session.open()
-    await new Promise()
+    console.log('connected')
+
+    const hostname = session.getHostname()
+    console.log(hostname)
   } catch (e) {
     console.error(e)
   }
