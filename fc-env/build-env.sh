c3965803a73218d64c0fc4645971e97335c6c6cb
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-09-20 15:46:36 +0000

    Increase default env size

--- fc-env/build-env.sh
+++ fc-env/build-env.sh
@@ -107,7 +107,7 @@ function mkrootfs() {
   echo "===> Making rootfs..."
 
   local tag=rootfs-${RUN_UUID}
-  local free=1000000000 # 1000MB in B
+  local free=1500000000 # 1000MB in B
 
   echo -e "$DOCKERFILE" | docker build -t $tag -f - $SCRIPTDIR
   local container_id=$(docker run -dt $tag /bin/sh)
