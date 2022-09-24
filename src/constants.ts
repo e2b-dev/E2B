65e983b8253e2b9bc3521b9ff97d59aba46190f6
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-09-24 16:39:18 +0000

    Restore the ws reconnect time

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SESSION_REFRESH_PERIOD = 5_000 // 5s
-export const WS_RECONNECT_INTERVAL = 2500 // 250ms
+export const WS_RECONNECT_INTERVAL = 100 // 100ms
 
 export const SESSION_DOMAIN = 'ondevbook.com'
 export const WS_PORT = 8010
