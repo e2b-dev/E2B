f85eb7f37735d20dfa163df72535e1097b3f3d68
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-09-25 01:00:05 +0000

    Add log for each rpc call; Change run example

--- test/run.mjs
+++ test/run.mjs
@@ -4,11 +4,17 @@ async function main() {
   const session = new Session({
     id: 'Nodejs',
     debug: true,
-    // codeSnippet: {
-    //   onStateChange(state) {
-    //     console.log(state)
-    //   },
-    // },
+    codeSnippet: {
+      onStateChange(state) {
+        console.log(state)
+      },
+      onStdout(out) {
+        console.log(out)
+      },
+      onStderr(err) {
+        console.log(err)
+      },
+    },
     onDisconnect() {
       console.log('disconnect')
     },
@@ -24,26 +30,43 @@ async function main() {
 
   try {
     await session.open()
-    console.log('connected')
+    await session.codeSnippet.run('console')
+    await session.codeSnippet.run('echo 2')
+    await session.codeSnippet.stop()
+    await session.codeSnippet.run('sleep 2')
+    await session.codeSnippet.stop()
+    const term = await session.terminal.createSession({
+      onData: (data) => console.log(data),
+      size: { cols: 20, rows: 40 },
+      onChildProcessesChange: (cp) => console.log(cp),
+    })
 
-    // const term = await session.terminal.createSession({
-    //   onData: (data) => console.log(data),
-    //   size: { cols: 20, rows: 40 },
-    // })
+    const term2 = await session.terminal.createSession({
+      onData: (data) => console.log(data),
+      size: { cols: 20, rows: 40 },
+    })
 
-    // const term2 = await session.terminal.createSession({
-    //   onData: (data) => console.log(data),
-    //   size: { cols: 20, rows: 40 },
-    // })
 
-    await session.process.start({
+    const p = await session.process.start({
       cmd: 'echo 22',
       onStdout: (o) => console.log(o.line)
     })
+    // await p.sendStdin('xx')
+    await term.sendData('sleep 2\n')
+    await p.kill()
+    await term2.destroy()
 
-    // await term.destroy()
-    const hostname = session.getHostname()
-    console.log(hostname)
+    // const files = await session.filesystem.listAllFiles('/')
+    // console.log(files)
+    // await session.filesystem.writeFile('/new', '>>--')
+    // const content = await session.filesystem.readFile('/new')
+    // console.log(content)
+    // await session.filesystem.removeFile('/new')
+    // const lastFiles = await session.filesystem.listAllFiles('/')
+    // console.log(lastFiles)
+
+    // const hostname = session.getHostname()
+    // console.log(hostname)
   } catch (e) {
     console.error(e)
   }
