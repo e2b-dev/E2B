5adca7cf5fc245071d4bc3ac7c069551c8e63e96
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-09-27 12:03:32 +0000

    Change default port to ephemeral port

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const SESSION_REFRESH_PERIOD = 5_000 // 5s
 export const WS_RECONNECT_INTERVAL = 100 // 100ms
 
 export const SESSION_DOMAIN = 'ondevbook.com'
-export const WS_PORT = 14419
+export const WS_PORT = 49982
 export const WS_ROUTE = '/ws'
