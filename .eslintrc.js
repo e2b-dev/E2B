9c111cea1e01bf41dfc9092ba852047108797ff3
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-10-09 15:50:11 +0000

    Fix string errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
       {
         code: 90,
         ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
       },
     ],
     'prettier/prettier': ['error'],
