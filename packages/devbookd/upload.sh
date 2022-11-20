f229980a0813d42af89104fbe8fdb0a54fcfc671
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-11-20 22:12:52 +0000

    FIx devbookd upload

--- packages/devbookd/upload.sh
+++ packages/devbookd/upload.sh
@@ -2,17 +2,5 @@
 
 set -euo pipefail
 
-[ -e devbookd-env.tar.gz ] && rm env.tar.gz
-[ -d devbookd-env ] && rm -rf env
-
-mkdir devbookd-env
-
-cp dist/devbookd_linux_amd64_v1 devbookd-env/devbookd
-
-tar czf devbookd-env.tar.gz devbookd-env
-
 gsutil -h "Cache-Control:no-cache, max-age=0" \
-  cp c.tar.gz gs://devbook-environment-pipeline
-
-rm devbookd-env.tar.gz
-rm -rf devbookd-env
+  cp dist/devbookd_linux_amd64_v1 gs://devbook-environment-pipeline/devbookd
