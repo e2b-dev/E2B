0f0a631da6d5c876092a27266b208c606d3edc43
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-12-20 23:32:01 +0000

    Cleanup

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,6 +1,5 @@
 export const SESSION_REFRESH_PERIOD = 5_000 // 5s
 export const WS_RECONNECT_INTERVAL = 600 // 600ms
-export const MAX_RECONNECTS = 200
 
 export const SESSION_DOMAIN = 'ondevbook.com'
 export const WS_PORT = 49982
