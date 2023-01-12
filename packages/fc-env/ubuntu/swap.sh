34cc1c4d483e61855872748fc93aec9c9e08e4f8
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-01-12 00:19:45 +0000

    Cleanup

--- packages/fc-env/ubuntu/swap.sh
+++ packages/fc-env/ubuntu/swap.sh
@@ -2,7 +2,7 @@
 
 # https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-22-04
 
-sudo fallocate -l 500M /swapfile
+fallocate -l 500M /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
