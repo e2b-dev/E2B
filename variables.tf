14e225b629aa52a093f0389b1c976d8e782884ed
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-02-01 10:27:17 +0000

    Change machine type

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "client_cluster_size" {
 
 variable "client_machine_type" {
   type    = string
-  default = "n1-highcpu-8"
+  default = "n1-custom-10-10240"
 }
 
 variable "client_proxy_health_port" {
