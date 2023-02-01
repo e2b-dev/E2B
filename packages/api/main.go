99d4c562844e040fed625d579d613eff7654fa89
Tomáš Valenta <valenta.and.thomas@gmail.com>
2023-02-01 17:54:16 +0000

    Add profiler to API

--- packages/api/main.go
+++ packages/api/main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/gin-contrib/pprof"
+
 	"github.com/devbookhq/devbook-api/packages/api/internal/api"
 	"github.com/devbookhq/devbook-api/packages/api/internal/handlers"
 	customMiddleware "github.com/devbookhq/devbook-api/packages/api/internal/middleware"
@@ -38,6 +40,8 @@ func NewGinServer(apiStore *handlers.APIStore, port int) *http.Server {
 	swagger.Servers = nil
 
 	r := gin.New()
+
+	pprof.Register(r, "debug/pprof")
 
 	// We use custom otelgin middleware because we want to log 4xx errors in the otel
 	otelMiddleware := customMiddleware.ExcludeRoutes(customMiddleware.Otel(serviceName), ignoreLoggingForPaths...)
@@ -71,12 +75,18 @@ func main() {
 	port := flag.Int("port", 80, "Port for test HTTP server")
 	flag.Parse()
 
+	debug := flag.String("true", "false", "is debug")
+
+	if *debug != "true" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	rand.Seed(time.Now().UnixNano())
 
 	if *telemetryAPIKey == "" {
 		otelLauncher := launcher.ConfigureOpentelemetry(
 			launcher.WithServiceName(serviceName),
-			launcher.WithMetricReportingPeriod(10*time.Second),
+			launcher.WithMetricReportingPeriod(20*time.Second),
 			launcher.WithSpanExporterEndpoint(otelCollectorGRPCEndpoint),
 			launcher.WithMetricExporterEndpoint(otelCollectorGRPCEndpoint),
 			launcher.WithMetricExporterInsecure(true),
@@ -97,6 +107,7 @@ func main() {
 	defer apiStore.Close()
 
 	s := NewGinServer(apiStore, *port)
+
 	// And we serve HTTP until the world ends.
 	log.Fatal(s.ListenAndServe())
 }
