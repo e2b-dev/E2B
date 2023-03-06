3e28474ee9904a08e39c19f25785d83cc9b484f7
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-06 10:40:33 +0100

    commit

--- api/generate.py
+++ api/generate.py
@@ -21,6 +21,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+
         self.wfile.write(
             str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
         return
