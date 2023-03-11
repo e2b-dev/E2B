e313720acf25f862e9eaa6187e2c2da3343248e5
Tomáš Valenta <valenta.and.thomas@gmail.com>
2023-03-11 12:51:00 +0000

    Fix client import; Improve types; Fix generating

--- api-service/playground_client/main.py
+++ api-service/playground_client/main.py
@@ -1,14 +1,24 @@
 import openapi_client
 
 from enum import Enum
+from typing import TypedDict, Union
+
 
 configuration = openapi_client.Configuration(
     host = 'https://localhost:9001',
 )
 
-class FilesystemEntry(Enum):
+class FilesystemEntryType(Enum):
     FILE = 'file'
     DIR = 'directory'
+
+class FilesystemEntry(TypedDict):
+    name: str
+    type: FilesystemEntryType
+
+class CommandOutput(TypedDict):
+    error: Union[str, None]
+    output: str
 
 class Playground:
     def __init__(self, env_id: str):
@@ -31,10 +41,10 @@ class Playground:
         err_lines = [err.line for err in result.stderr]
         out_lines = [out.line for out in result.stdout]
 
-        return {
-            'error': '\n'.join(err_lines),
-            'output': '\n'.join(out_lines),
-        }
+        return CommandOutput(
+            error=err_lines.join('\n') if len(err_lines) > 0 else None,
+            output='\n'.join(out_lines)
+        )
 
     def read_file(self, path: str):
         result = self.api.read_filesystem_file(self.session.id, path)
@@ -43,10 +53,10 @@ class Playground:
     def list_dir(self, path: str):
         result = self.api.list_filesystem_dir(self.session.id, path)
         return [
-            {
-                'type': FilesystemEntry.DIR.value if entry.is_dir else FilesystemEntry.FILE.value,
-                'name': entry.name,
-            }
+            FilesystemEntry(
+                name=entry.name,
+                type=FilesystemEntryType.DIR if entry.is_dir else FilesystemEntryType.FILE
+            ) 
             for entry in result.entries
         ]
 
--- api-service/scripts/generate-playground-client.sh
+++
@@ -1,4 +0,0 @@
-cd playground && yarn build
-cd ..
-rm -rf ./playground_client/openapi_client
-npx @openapitools/openapi-generator-cli generate -i playground/swagger.json -g python-nextgen -o ./playground_client/ --additional-properties=generateSourceCodeOnly=true --additional-properties=disallowAdditionalPropertiesIfNotPresent=false
