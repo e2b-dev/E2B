8df0ebf28011c6394c5d69b5ba4bd713ab52ea59
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-03-14 20:27:28 +0000

    Switch to pure code execution tool

--- api-service/codegen/tools/playground/tools/code.py
+++ api-service/codegen/tools/playground/tools/code.py
@@ -51,15 +51,19 @@ def create_code_tools(playground: NodeJSPlayground, mock: MockRequestFactory):
         mock_request_cmd = mock.terminal_command()
 
         port = 3000
-        request_output, server_output = playground.run_typescript_server_code_with_request(
-            code=code, request_cmd=mock_request_cmd, port=port,
+        (
+            request_output,
+            server_output,
+        ) = playground.run_typescript_server_code_with_request(
+            code=code,
+            request_cmd=mock_request_cmd,
+            port=port,
         )
 
         request_result = encode_command_output(request_output)
         server_result = encode_command_output(server_output)
 
         return f"Request result:\n{request_result}\nCode execution result:\n{server_result}"
-
 
     # yield run_typescript_code
 
@@ -76,37 +80,37 @@ def create_code_tools(playground: NodeJSPlayground, mock: MockRequestFactory):
     # yield check_typescript_code_types
 
     # This tool is just for executing JavaScript without doing the request to server
-    # @tool("RunJavaScriptCode")
-    # def run_javascript_code(code: str) -> str:
-    #     """
-    #     Run JavaScript code and return errors and output.
-    #     Input should be a valid JavaScript code.
-    #     """
-    #     output = playground.run_javascript_code(extract_code(code))
-    #     result = encode_command_output(output)
-    #     return result if len(result) > 0 else "Code execution finished without error"
-
-    # yield run_javascript_code
-
     @tool("RunJavaScriptCode")
     def run_javascript_code(code: str) -> str:
         """
-        Execute JavaScript code and make a request to see if server correctly handles needed requests.
-        The input should be a valid JavaScript code.
-        The server should run on http://localhost:3000.
-        The returned result is a the request's output followed by the server code output and errors.
+        Run JavaScript code and return errors and output.
+        Input should be a valid JavaScript code.
         """
-
-        mock_request_cmd = mock.terminal_command()
-
-        port = 3000
-        request_output, server_output = playground.run_javascript_server_code_with_request(
-            code=code, request_cmd=mock_request_cmd, port=port,
-        )
-
-        request_result = encode_command_output(request_output)
-        server_result = encode_command_output(server_output)
-
-        return f"Request result:\n{request_result}\nCode execution result:\n{server_result}"
+        output = playground.run_javascript_code(extract_code(code))
+        result = encode_command_output(output)
+        return result if len(result) > 0 else "Code execution finished without error"
 
     yield run_javascript_code
+
+    # @tool("RunJavaScriptCode")
+    # def run_javascript_code(code: str) -> str:
+    #     """
+    #     Execute JavaScript code and make a request to see if server correctly handles needed requests.
+    #     The input should be a valid JavaScript code.
+    #     The server should run on http://localhost:3000.
+    #     The returned result is a the request's output followed by the server code output and errors.
+    #     """
+
+    #     mock_request_cmd = mock.terminal_command()
+
+    #     port = 3000
+    #     request_output, server_output = playground.run_javascript_server_code_with_request(
+    #         code=code, request_cmd=mock_request_cmd, port=port,
+    #     )
+
+    #     request_result = encode_command_output(request_output)
+    #     server_result = encode_command_output(server_output)
+
+    #     return f"Request result:\n{request_result}\nCode execution result:\n{server_result}"
+
+    # yield run_javascript_code
