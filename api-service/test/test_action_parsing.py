1ef4f6817c256aaae9f1afea2f768a8320f02b50
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-21 16:12:37 +0100

    commit

--- api-service/test/test_action_parsing.py
+++ api-service/test/test_action_parsing.py
@@ -2,7 +2,7 @@
 
 # import pytest
 
-from typing import Dict, List, NamedTuple
+from typing import Dict, List, NamedTuple, Any
 from codegen.agent.base import (
     separate_thought_and_action,
     parse_action_string,
@@ -14,7 +14,7 @@ class Action(NamedTuple):
     input: str
 
 
-llm_outputs: List[Dict[str, str]] = [
+llm_outputs: List[Dict[str, Any]] = [
     {
         "llm_output": """Thought: Lorem ipsum
 
@@ -122,53 +122,6 @@ app.listen(port, () => {
         ],
     },
 ]
-
-# llm_outputs = [
-#     """Thought: I need to install the `email-validator` package using `InstallNPMDependencies`. Then, I need to write the post handler function that checks if the email is valid and sends the appropriate response. Finally, I need to add the post handler to the Express app and start the server using `app.listen()`.
-
-# Action:
-# ```
-# <action tool="InstallNPMDependencies">
-# email-validator
-# </action>
-# ```
-
-# ```
-# <action tool="RunJavaScriptCode">
-# import express from 'express';
-# import validator from 'email-validator';
-
-# const app = express();
-# const port = 3000;
-
-# app.use(express.json());
-
-# app.post('/', (req, res) => {
-#   const email = req.body.email;
-#   if (!validator.validate(email)) {
-#     res.status(400).json({ error: 'Invalid email format' });
-#   } else {
-#     res.status(200).json({});
-#   }
-# });
-
-# app.listen(port, () => {
-#   console.log(`Server listening on port ${port}`);
-# });
-# </action>
-# ```
-
-# """,
-#     '<action tool="Name">content</action>',
-#     """
-# Thought: lorem ipsum
-
-# Action:
-# ```
-# <action tool="Name1">content</action 1><action tool="Name2">content 2</action><action tool="Name3">content 3</action>'
-# ```
-# """,
-# ]
 
 
 def test_parsing_llm_action_output():
