5bfa0ab371a74ca9049675ee4e8daff23820c38d
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-03-22 12:09:11 +0000

    Cleanup log handling

---
+++ api-service/codegen/callbacks/log_queue.py
@@ -0,0 +1,27 @@
+from asyncio import Queue, ensure_future
+from typing import Coroutine
+
+
+class LogQueue:
+    def __init__(self) -> None:
+        self.queue: Queue[Coroutine] = Queue()
+        self.work = ensure_future(self.worker())
+
+    async def worker(self):
+        while True:
+            for _ in range(self.queue.qsize() - 1):
+                old_coro = self.queue.get_nowait()
+                try:
+                    old_coro.close()
+                except Exception as e:
+                    print(e)
+
+            task = await self.queue.get()
+            try:
+                await ensure_future(task)
+                self.queue.task_done()
+            except Exception as e:
+                print(e)
+
+    def close(self):
+        self.work.cancel()
