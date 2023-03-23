17a9de23f5c34a3071aabe9dc6cd71615705d0e9
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-03-23 00:26:22 +0000

    Deploy api on dynamic port

--- api-service/start.sh
+++ api-service/start.sh
@@ -4,8 +4,7 @@
 cd /playground && exec node lib/server.js &
   
 # Start python app
-# TODO: Start the server on the $PORT port instead of the fixed port.
-cd /app && exec poetry run gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 8 --timeout 0 app:app -k uvicorn.workers.UvicornWorker &
+cd /app && exec poetry run gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 0 app:app -k uvicorn.workers.UvicornWorker &
   
 # Wait for any process to exit
 wait -n
