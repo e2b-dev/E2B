3e64720d8b60dfd7c8968588aa238a515bfee3e6
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-04-05 23:56:05 +0000

    Try autocomplete with more packages

--- hooks/useReferences.ts
+++ hooks/useReferences.ts
@@ -4,17 +4,20 @@ import {
 import Fuse from 'fuse.js'
 
 import { ReferenceType, Reference } from 'editor/referenceType'
+// Top 10k NPM packages
+// https://gist.github.com/anvaka/8e8fa57c7ee1350e3491
+import topNPMPackages from 'editor/referencePackages.json'
+
+const packageReferences: Reference[] = topNPMPackages.map(p => ({
+  type: ReferenceType.NPMPackage,
+  value: p,
+}))
+
 
 function useReferences(): [Fuse<Reference>, Reference[]] {
   const references = useMemo<Reference[]>(() => [
-    {
-      type: ReferenceType.NPMPackage,
-      value: '@slack/web-api',
-    },
-    {
-      type: ReferenceType.DEPLOYMENT,
-      value: 'AWS Lambda',
-    },
+    // TODO: Package search is synchronous and even with just 10k packages it freezes the whole app.
+    // ...packageReferences,
   ], [])
 
   const engine = useMemo(() => new Fuse(references, {
