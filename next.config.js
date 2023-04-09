9bb6b28cdc452d011d8200a8c0060bd7c8cd4570
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-04-09 20:42:57 +0000

    Modify next config for Docker build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
+const fastDockerBuild = {
+  compress: false,
+  productionBrowserSourceMaps: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  optimizeFonts: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...process.env.BUILD === 'docker' && fastDockerBuild,
   swcMinify: true,
   experimental: {
     swcPlugins: [['next-superjson-plugin', {}]],
