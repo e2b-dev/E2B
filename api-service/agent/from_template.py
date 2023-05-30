d0573924201669769ca86f535d81262b23f157d3
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-05-29 18:33:53 -0700

    Remove log

--- api-service/agent/from_template.py
+++ api-service/agent/from_template.py
@@ -3,7 +3,6 @@ from agent.smol_agent import SmolAgent
 
 
 def get_agent_factory_from_template(template_id: str):
-    print("tempalte", template_id)
     match template_id:
         case "SmolDeveloper":
             return SmolAgent.create
