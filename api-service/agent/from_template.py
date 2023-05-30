d731c8cce845e98ad5853999b1bf81294d0d93f3
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-05-29 18:30:36 -0700

    Fix template agent

--- api-service/agent/from_template.py
+++ api-service/agent/from_template.py
@@ -3,8 +3,9 @@ from agent.smol_agent import SmolAgent
 
 
 def get_agent_factory_from_template(template_id: str):
+    print("tempalte", template_id)
     match template_id:
         case "SmolDeveloper":
-            return BasicAgent.create
-        case _:
             return SmolAgent.create
+        case _:
+            return BasicAgent.create
