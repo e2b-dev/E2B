2e1612790a891b7d9b5027436f99f5748cc18e20
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-06-14 17:57:19 -0700

    Disable sentry for dev

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,15 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://49127069fd7d43b9b07ee1ad5acad931@o1382733.ingest.sentry.io/4505360685924352",
+  dsn: 'https://49127069fd7d43b9b07ee1ad5acad931@o1382733.ingest.sentry.io/4505360685924352',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+  enabled: process.env.NODE_ENV !== 'development',
+})
