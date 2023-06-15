cf2f8e7a5e207112efa7cc3bb1f93346e6185c58
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-06-14 21:07:15 -0700

    Change env params handling in the docker start script

--- api-service/start.sh
+++ api-service/start.sh
@@ -4,7 +4,7 @@
 cd /playground && exec node lib/server.js &
 
 # Start python app
-cd /app && OBJC_DISABLE_INITIALIZE_FORK_SAFETY="YES" exec poetry run opentelemetry-instrument --service_name agent-api --exporter_otlp_endpoint \"ingest.lightstep.com:443\" --traces_exporter otlp_proto_grpc --metrics_exporter otlp_proto_grpc gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 14 --timeout 120 app:app -k uvicorn.workers.UvicornWorker &
+cd /app && OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES exec poetry run opentelemetry-instrument --service_name agent-api --exporter_otlp_endpoint 'ingest.lightstep.com:443' --traces_exporter console,otlp_proto_grpc --metrics_exporter console,otlp_proto_grpc gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 12 --timeout 120 app:app -k uvicorn.workers.UvicornWorker &
 
 # Wait for any process to exit
 wait -n
