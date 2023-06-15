88ba918244656b1729883a7ba3ad252d278e2f8a
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-06-15 15:23:45 -0700

    Remove unfinished nextjs otel

--- OTEL-initializer.ts
+++
@@ -1,7 +0,0 @@
-import { trace, context } from '@opentelemetry/api'
-import { registerOTel } from '@vercel/otel'
-
-registerOTel('agent-dashboard')
-
-export const tracer = trace.getTracer('agent-dashboard-tracer')
-export { context }
