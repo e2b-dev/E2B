0b810ac6105964a3f498bd79933ec0f12a9f7064
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-06-16 00:37:08 -0700

    Add example log to LogFile page

--- pages/log/[logFileID].tsx
+++ pages/log/[logFileID].tsx
@@ -1,13 +1,17 @@
-import { LogFile, AgentLogs } from 'utils/agentLogs'
+import { AgentLogs, LogFile } from 'utils/agentLogs'
+import testLogs from './logfile.json'
 
 export interface Props {
-  logFile: LogFile
-  logs: AgentLogs
+  logFile: LogFile & { content: AgentLogs }
 }
 
-function LogFile({
+function LogFile() {
+  const logFile: Props['logFile'] = {
+    id: 'test',
+    name: 'test.json',
+    content: testLogs as any as AgentLogs,
+  }
 
-}) {
   return (
     <main className="overflow-hidden flex flex-col max-h-full">
       <header className="flex items-center justify-between p-4 sm:p-6 lg:px-8">
