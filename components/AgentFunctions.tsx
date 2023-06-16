16f25f23a4ddf598044a4fbbc236158bead951c9
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-06-16 03:19:45 -0700

    render function params

--- components/AgentFunctions.tsx
+++ components/AgentFunctions.tsx
@@ -5,6 +5,30 @@ import clsx from 'clsx'
 import {
   ChevronRight,
 } from 'lucide-react'
+import ReactJson from 'react-json-view'
+
+
+
+const theme = {
+  scheme: 'monokai',
+  author: 'wimer hazenberg (http://www.monokai.nl)',
+  base00: '#272822',
+  base01: '#383830',
+  base02: '#49483e',
+  base03: '#75715e',
+  base04: '#a59f85',
+  base05: '#f8f8f2',
+  base06: '#f5f4f1',
+  base07: '#f9f8f5',
+  base08: '#f92672',
+  base09: '#fd971f',
+  base0A: '#f4bf75',
+  base0B: '#a6e22e',
+  base0C: '#a1efe4',
+  base0D: '#66d9ef',
+  base0E: '#ae81ff',
+  base0F: '#cc6633',
+}
 
 import { AgentFunction } from 'utils/agentLogs'
 
@@ -100,16 +124,22 @@ function AgentFunctions({
                   )}
 
                   {fn.description && (
-                    <div className="flex flex-col space-y-1">
+                    <div className="flex flex-col space-y-1 w-full">
                       <span className="text-sm font-bold text-gray-400">Description</span>
-                      <span className="text-sm text-gray-200">{fn.description}</span>
+                      <span className="text-sm text-gray-200 w-full">{fn.description}</span>
                     </div>
                   )}
 
                   {fn.parameters && (
-                    <div className="flex flex-col space-y-1">
+                    <div className="flex flex-col space-y-1 w-full">
                       <span className="text-sm font-bold text-gray-400">Parameters</span>
-                      <span className="text-sm text-gray-200">{fn.description}</span>
+                      <div className="rounded-md border border-gray-500 overflow-hidden p-2">
+                        <ReactJson
+                          src={fn.parameters}
+                          theme="pop"
+                          name={null}
+                        />
+                      </div>
                     </div>
                   )}
                 </div>
