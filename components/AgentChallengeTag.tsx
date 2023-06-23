4fb761a96c8bad0a6895ce5cfff349089caf24c0
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-06-23 10:40:23 -0700

    fix styling

--- components/AgentChallengeTag.tsx
+++ components/AgentChallengeTag.tsx
@@ -11,7 +11,7 @@ function AgentChallengeTag({
   return (
     <span
       className={clsx(
-        'flex items-center justify-center h-6 px-2 rounded-md text-xs border',
+        'flex items-center justify-center h-6 px-2 rounded-md text-xs border shrink-0',
         tag.severity === 'Success' && 'bg-green-500/10 border-green-500/20 text-green-500',
         tag.severity === 'Warning' && 'bg-orange-500/10 border-orange-500/20 text-orange-500',
         tag.severity === 'Error' && 'bg-red-500/10 border-red-500/20 text-red-500',
