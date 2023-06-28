da367122ceadbccc2ca9d9da8d0554cc5f268e43
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-06-28 00:18:38 -0700

    Disable replays

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -13,19 +13,19 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
+    // new Sentry.Replay({
+    //   // Additional Replay configuration goes in here, for example:
+    //   maskAllText: true,
+    //   blockAllMedia: true,
+    // }),
   ],
   enabled: process.env.NODE_ENV !== 'development',
 })
