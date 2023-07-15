2e19b76fcc46383a1c0c1e7ea24d49b0ce673b9a
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-07-15 00:41:20 -0700

    Replace password input with CSS to remove update password browser dialogs

--- components/ChooseOpenAIKey.tsx
+++ components/ChooseOpenAIKey.tsx
@@ -84,11 +84,10 @@ function ChooseOpenAIKey({
               <li className="ml-4"><b>All costs running the smol developer will be billed to your OpenAI account</b></li>
             </ul>
             <input
-              className="bg-gray-800 block w-full rounded-md border-0 px-3 py-1.5 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-gray-300 outline-none sm:text-sm sm:leading-6 transition-all"
+              className="bg-gray-800 block w-full rounded-md border-0 px-3 py-1.5 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-gray-300 outline-none sm:text-sm sm:leading-6 transition-all security"
               placeholder="OpenAI API Key"
               value={userOpenAIKey}
               onChange={onUserOpenAIKeyChange}
-              type='password'
               autoFocus
             />
           </div>
