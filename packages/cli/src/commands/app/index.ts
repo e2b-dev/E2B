fa196d8163404483871c43eae0d3125c912ae5c9
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-07-26 14:03:53 +0200

    Remove MDX app command from the CLI

--- packages/cli/src/commands/app/index.ts
+++
@@ -1,8 +0,0 @@
-import * as commander from 'commander'
-
-import { devCommand } from './dev'
-
-export const appCommand = new commander.Command('app')
-  .description('Develop and manage Devbook applications')
-  .addCommand(devCommand)
-  .alias('application')
--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,9 +1,7 @@
 import * as commander from 'commander'
-import { appCommand } from './app'
 
 import { envCommand } from './env'
 
 export const program = new commander.Command()
   .description('Tool for interacting with Devbook from command line')
   .addCommand(envCommand, { isDefault: true })
-  .addCommand(appCommand)
