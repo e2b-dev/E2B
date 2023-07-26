fcf54235ce6b9ee8d6bd3ffc272ef4123095351a
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-07-26 18:26:32 +0200

    Fix version increment script causing error when releasin

--- scripts/increment-version.sh
+++ scripts/increment-version.sh
@@ -3,18 +3,20 @@ version_file="VERSION"
 old_version=`cat $version_file`
 new_version=`./scripts/semver.sh -p $old_version`
 
-echo -n $new_version > $version_file
+printf "$new_version" > "$version_file"
 
 increment_package_version() {
   package_path=$1
   version=$2
 
   npm version --prefix $package_path $version
-  npm i --package-lock-only  --prefix $package_path
 }
 
 cli_path="packages/cli" 
-sdk_path="packages/sdk" 
+sdk_path="packages/sdk"
+
+increment_package_version $sdk_path $new_version
+npm i --package-lock-only --prefix $sdk_path
 
 increment_package_version $cli_path $new_version
-increment_package_version $sdk_path $new_version
+# We run npm i only for the sdk because pnpm does not have CLI package version in the lockfile
