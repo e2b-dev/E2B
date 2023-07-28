4feca0fc857142472b01185fc9420da89d445445
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-07-28 16:02:59 +0200

    Handle byte read/write in SDK [WIP]

--- packages/sdk/src/session/filesystem.ts
+++ packages/sdk/src/session/filesystem.ts
@@ -9,7 +9,9 @@ export interface FileInfo {
 
 export interface FilesystemManager {
   readonly write: (path: string, content: string) => Promise<void>
+  readonly writeBytes: (path: string, content: Uint8Array) => Promise<void>
   readonly read: (path: string) => Promise<string>
+  readonly readBytes: (path: string) => Promise<Uint8Array>
   readonly remove: (path: string) => Promise<void>
   readonly list: (path: string) => Promise<FileInfo[]>
   readonly makeDir: (path: string) => Promise<void>
