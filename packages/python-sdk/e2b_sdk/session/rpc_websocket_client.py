55c383c536ac377261351575714a1692b7317c53
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-04 17:24:35 +0200

    Add rpc websocket client

--- packages/python-sdk/e2b_sdk/session/rpc_websocket_client.py
+++ packages/python-sdk/e2b_sdk/session/rpc_websocket_client.py
@@ -1,23 +1,77 @@
-from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
-from tinyrpc.transports.http import HttpPostClientTransport
-from tinyrpc.transports.websocketclient import HttpWebSocketClientTransport
-from tinyrpc import RPCClient
+import asyncio
+import json
+
+from websockets.client import WebSocketClientProtocol, connect
+from websockets.exceptions import WebSocketException
+from typing import Callable, Optional, Any, Dict
+from pydantic import BaseModel, PrivateAttr
+from jsonrpcclient import request_json, Ok, parse_json, Error
+from websockets.typing import Data
+
+from e2b_sdk.utils.future import DeferredFuture
 
 
-remote_server = rpc_client.get_proxy()
+class RpcWebSocketClient(BaseModel):
+    url: str = PrivateAttr()
+    on_open: Callable[[], None] = PrivateAttr()
+    on_error: Callable[[], None] = PrivateAttr()
+    on_close: Callable[[], None] = PrivateAttr()
+    on_message: Callable[[Any], None] = PrivateAttr()
 
-# call a method called 'reverse_string' with a single string argument
-result = remote_server.reverse_string("Hello, World!")
+    waiting_for_replies: Dict[str, DeferredFuture] = PrivateAttr()
 
-print("Server answered:", result)
+    ws: Optional[WebSocketClientProtocol] = PrivateAttr()
 
+    async def connect(self):
+        future_connect = DeferredFuture()
 
-class RpcWebsocketClient:
-    def __init__(
-        self,
-    ):
-        self.session = session
-        self.rpc_client = RPCClient(
-            JSONRPCProtocol(),
-            HttpWebSocketClientTransport(),
-        )
+        async def connect_with_reconnect(retries=2):
+            for i in range(retries + 1):
+                try:
+                    async with connect(self.url) as self.ws:
+                        self.on_open()
+                        future_connect(None)
+                        async for message in self.ws:
+                            asyncio.create_task(self._receive_message(message))
+                except (WebSocketException, OSError):
+                    self.on_error()
+                    print("Connection error occurred. Reconnecting...")
+                    await asyncio.sleep(2**i)
+                self.on_close()
+            print("Connection retries exceeded.")
+
+        asyncio.create_task(connect_with_reconnect())
+        await future_connect
+
+    async def send_message(self, message, *params) -> Any:
+        if not self.ws:
+            raise Exception("Not connected")
+
+        request = request_json(message, list(params))
+        request_with_id = json.loads(request)
+
+        future_reply = DeferredFuture()
+
+        try:
+            self.waiting_for_replies[request_with_id["id"]] = future_reply
+            await self.ws.send(request)
+            return await future_reply
+        finally:
+            del self.waiting_for_replies[request_with_id["id"]]
+
+    async def _receive_message(self, message: Data):
+        data = parse_json(message)
+        if id := getattr(data, "id"):
+            if id in self.waiting_for_replies:
+                if isinstance(data, Ok):
+                    self.waiting_for_replies[id](data)
+                elif isinstance(data, Error):
+                    self.waiting_for_replies[id].reject(data)
+        else:
+            self.on_message(data)
+
+    async def close(self):
+        if self.ws:
+            await self.ws.close()
+        if self.on_close:
+            self.on_close()
