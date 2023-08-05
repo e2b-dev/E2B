a296e78e93e5aa726b0a0fa8a58dedfa136f3012
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-06 00:19:54 +0200

    Cleanup

---
+++ packages/python-sdk/e2b/__init__.py
@@ -0,0 +1,1 @@
+from .session.main import Session
---
+++ packages/python-sdk/example.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from e2b import Session
+
+id = "PPSrlH5TIvFx"
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    session = Session(id=id)
+    await session.open()
+
+    res = await session.filesystem.read("/etc/hosts")
+    print("RESULT", res)
+    # await session.filesystem.write("test.txt", "Hello World")
+
+    # f = await session.filesystem.read("test.txt")
+    # print(f)
+
+    # proc = await session.process.start("ls -la")
+    # await proc.finished
+
+    # await session.close()
+
+
+asyncio.new_event_loop().run_until_complete(main())
