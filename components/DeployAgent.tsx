b6f3df5aea9b7ec8e1ea05709033d1ea5ee202c3
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-08-08 20:41:09 +0200

    Prepare app for the technical test

--- components/DeployAgent.tsx
+++ components/DeployAgent.tsx
@@ -35,13 +35,13 @@ function DeployAgent({
   error,
 }: Props) {
   return (
-    <div className="flex-1 flex flex-col items-start justify-start space-y-2">
-      <div className="flex-1 w-full flex flex-col items-start justify-start space-y-8">
+    <div className="">
+      <div className="">
 
         {isDeploying && (
-          <div className="flex-1 w-full flex space-x-2 items-center justify-center">
-            <SpinnerIcon className="text-gray-300" />
-            <span className="text-gray-300">Deploying...</span>
+          <div className="">
+            <SpinnerIcon className="" />
+            <span className="">Deploying...</span>
           </div>
         )}
         {!isDeploying && error && (
@@ -53,80 +53,80 @@ function DeployAgent({
         {!isDeploying && (
           <>
             {/* OpenAI Key */}
-            <div className="w-full flex flex-col space-y-2 items-start justify-start">
-              <span className="text-sm text-gray-300 font-medium">
+            <div className="">
+              <span className="">
                 OpenAI API Key
               </span>
-              <span className="text-sm font-medium text-gray-100">
+              <span className="">
                 {selectedOpenAIKeyType === 'user' && 'Use your own OpenAI API key'}
                 {selectedOpenAIKeyType === 'e2b' && 'Use e2b\'s OpenAI API key'}
               </span>
             </div>
 
-            <hr className="w-full border-gray-700 rounded" />
+            <hr className="" />
 
             {/* Selected repo */}
-            <div className="w-full flex flex-col space-y-2 items-start justify-start">
-              <div className="w-full flex items-center justify-between">
-                <div className="flex items-center space-x-1">
-                  <Github className="text-gray-300" size={16} />
-                  <span className="text-sm text-gray-300 font-medium">
+            <div className="">
+              <div className="">
+                <div className="">
+                  <Github className="" size={16} />
+                  <span className="">
                     Repository
                   </span>
                 </div>
 
                 <button
                   type="button"
-                  className="rounded w-[136px] bg-white/10 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-white/20"
+                  className=""
                   onClick={onChangeRepo}
                 >
                   Change Repository
                 </button>
               </div>
 
-              <span className="text-sm font-medium text-gray-100">
+              <span className="">
                 {selectedRepo.fullName}
               </span>
             </div>
 
-            <hr className="w-full border-gray-700 rounded" />
+            <hr className="" />
 
             {/* Instructions */}
-            <div className="flex-1 w-full flex flex-col space-y-2 items-start justify-start">
-              <div className="w-full flex items-center justify-between">
-                <div className="flex items-center space-x-1">
-                  <ScrollText className="text-gray-300" size={16} />
-                  <span className="text-sm text-gray-300 font-medium">
+            <div className="">
+              <div className="">
+                <div className="">
+                  <ScrollText className="" size={16} />
+                  <span className="">
                     Instructions
                   </span>
                 </div>
                 <button
                   type="button"
-                  className="rounded w-[136px] bg-white/10 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-white/20"
+                  className=""
                   onClick={onChangeTemplate}
                 >
                   Use Template
                 </button>
               </div>
 
-              <div className="relative bg-gray-950 flex flex-col p-2 w-full flex-1 overflow-auto border border-gray-700 rounded-md">
+              <div className="">
                 <InstructionsEditor
-                  className="absolute inset-0 bg-gray-950 p-4 text-gray-100"
+                  className=""
                   content={instructions}
                   onChange={onInstructionsChange}
                 />
               </div>
             </div>
-            <div className="w-full flex items-center justify-between">
+            <div className="">
               <button
-                className="flex items-center justify-start space-x-1 text-gray-400 hover:text-gray-100 transition-all"
+                className=""
                 onClick={onBack}
               >
                 <ArrowLeft size={14} />
-                <span className="text-sm">Back</span>
+                <span className="">Back</span>
               </button>
               <button
-                className="px-2 py-1 font-medium rounded-md text-sm border border-green-400/30 bg-green-400/10 text-green-400 hover:bg-transparent hover:border-green-400 transition-all"
+                className=""
                 onClick={onDeploy}
               >
                 Deploy on <b>e2b</b>
