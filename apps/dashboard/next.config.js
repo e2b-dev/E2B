f665ddecd3c0bfe46c9b6d12c75d05ef4ca33694
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-18 23:24:37 +0200

    Cleanup

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  experimental: {
-    swcPlugins: [['next-superjson-plugin', {}]],
-  },
   async rewrites() {
     return [
       {
