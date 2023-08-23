0653ec606480f4b4ec01de349f7dd755b102e607
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-08-23 12:27:32 +0200

    posthog: ingestion path

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -21,6 +21,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/ingest/:path*',
+        destination: 'https://app.posthog.com/:path*',
+      },
+      {
         source: '/',
         destination: '/docs',
       }
