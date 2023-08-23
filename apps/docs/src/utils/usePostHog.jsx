d1681c251788c0c4dd5dbc6c7ab7d25bee640fbb
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-08-23 11:23:37 +0200

    npm i & posthog pages

--- apps/docs/src/utils/usePostHog.jsx
+++ apps/docs/src/utils/usePostHog.jsx
@@ -2,11 +2,14 @@
 
 import { PostHogProvider as OriginalPostHogProvider } from 'posthog-js/react'
 import posthog from 'posthog-js'
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export function maybeInit() {
   if (typeof window === 'undefined' || !process.env.NEXT_PUBLIC_POSTHOG_KEY) return
 
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    // Note that PostHog will automatically capture page views and common events
     api_host: `${window.location.protocol}//${window.location.host}/ingest`,
     disable_session_recording: process.env.NODE_ENV !== 'production',
     advanced_disable_toolbar_metrics: true,
@@ -28,4 +31,20 @@ export function PostHogProvider({ children }) {
       {children}
     </OriginalPostHogProvider>
   )
+}
+
+export function PostHogAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    if (pathname) {
+      let url = window.origin + pathname
+      if (searchParams.toString()) url = url + `?${searchParams.toString()}`
+      posthog.capture(
+        '$pageview',
+        { '$current_url': url }
+      )
+    }
+  }, [pathname, searchParams])
+  
 }
