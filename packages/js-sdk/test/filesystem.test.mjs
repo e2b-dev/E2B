94b2be7136677157cbcaeaa497b9483e8de2ea2a
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-08-24 16:06:22 +0200

    chore(js-sdk): JS SDK tests

---
+++ packages/js-sdk/test/filesystem.test.mjs
@@ -0,0 +1,74 @@
+import { Session } from '../src'
+import { expect, test } from 'vitest'
+
+test('list files', async () => {
+  const session = await Session.create({ id: "Nodejs" })
+  await session.filesystem.makeDir("/test/new")
+
+  const ls = await session.filesystem.list("/test")
+  expect(ls.map(x => x.name)).toEqual(["new"])
+
+  await session.close()
+})
+
+test('create file', async () => {
+  const session = await Session.create({id: "Nodejs" })
+  await session.filesystem.makeDir("/test")
+  await session.filesystem.write("/test/test.txt", "Hello World!")
+
+  const ls = await session.filesystem.list("/test")
+  expect(ls.map(x => x.name)).toEqual(["test.txt"])
+  
+  await session.close()
+})
+
+test('read and write', async () => {
+  const session = await Session.create({id: "Nodejs" })
+
+  await session.filesystem.write("/tmp/test.txt", "Hello World!")
+
+  const content = await session.filesystem.read("/tmp/test.txt")
+  expect(content).toEqual("Hello World!")
+  
+
+  await session.close()
+})
+
+test('list delete files', async () => {
+  const session = await Session.create({id: "Nodejs" })
+  await session.filesystem.makeDir("/test/new")
+
+  let ls = await session.filesystem.list("/test")
+  expect(ls.map(x => x.name)).toEqual(["new"])
+
+  await session.filesystem.remove("/test/new")
+
+  ls = await session.filesystem.list("/test")
+  expect(ls.map(x => x.name)).toEqual([])
+
+  await session.close()
+})
+
+test('watch dir', async () => {
+  const session = new Session({ id: "Nodejs" })
+  await session.open()
+  await session.filesystem.write("/tmp/test.txt", "Hello")
+
+  const watcher = await session.filesystem.watchDir("/tmp")
+
+  const events = []
+  watcher.addEventListener(ev => events.push(ev))
+
+  await watcher.start()
+  await session.filesystem.write("/tmp/test.txt", "World!")
+  await new Promise(r => setTimeout(r, 1000))
+  await watcher.stop()
+  
+  expect(events.length).toEqual(1)
+
+  const event = events[0]
+  expect(event["operation"]).toEqual("Write")
+  expect(event["path"]).toEqual("/tmp/test.txt")
+
+  await session.close()
+})
