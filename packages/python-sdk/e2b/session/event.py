8c62aed91933a2b2f44cc87a8865767331e6925f
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-25 19:03:48 +0200

    Add Janus event [WIP]

--- packages/python-sdk/e2b/session/event.py
+++ packages/python-sdk/e2b/session/event.py
@@ -1,7 +1,20 @@
 import asyncio
 
-class Event(asyncio.Event):
+from janus import Queue
+
+class _Event(asyncio.Event):
     # TODO: clear() method
     def set(self):
         # FIXME: The _loop attribute is not documented as public api!
         self._loop.call_soon_threadsafe(super().set)
+
+
+class Event:
+    def __init__(self):
+        self._queue = Queue[bool](1)
+
+    async def wait(self):
+        await self._queue.async_q.get()
+
+    def set(self):
+        self._queue.sync_q.put(True)
