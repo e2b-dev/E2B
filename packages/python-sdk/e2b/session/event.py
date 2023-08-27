674e137e6c37892709d4cdb2c6c07d60102745c4
Jakub Novak <jakub@e2b.dev>
2023-08-27 13:16:09 +0200

    Fix event

--- packages/python-sdk/e2b/session/event.py
+++ packages/python-sdk/e2b/session/event.py
@@ -2,14 +2,20 @@ from janus import Queue
 
 
 class Event:
+    _is_set = False
+
     def __init__(self):
         self._queue = Queue[bool](1)
 
     async def wait(self):
+        if self._is_set:
+            return True
         await self._queue.async_q.get()
+        self._queue.async_q.task_done()
+        self._is_set = True
 
     def is_set(self):
-        return not self._queue.async_q.empty()
+        return self._is_set or (not self._queue.async_q.empty())
 
     def set(self):
         self._queue.sync_q.put(True)
