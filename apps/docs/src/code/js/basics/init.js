effc9872b9fc4552697baa789a63ebb23c08dac0
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-08-29 22:59:52 +0200

    Add very basic api key usage to JS/TS examples

--- apps/docs/src/code/js/basics/init.js
+++ apps/docs/src/code/js/basics/init.js
@@ -1,6 +1,9 @@
 import { Session } from '@e2b/sdk'
 
+const E2B_API_KEY = process.env.E2B_API_KEY
+
 const session = await Session.create({
   id: 'Nodejs',
+  apiKey: E2B_API_KEY,
 })
 await session.close()
--- apps/docs/src/code/js/quickstart.js
+++ apps/docs/src/code/js/quickstart.js
@@ -1,9 +1,13 @@
 import { Session } from '@e2b/sdk'
 
+const E2B_API_KEY = process.env.E2B_API_KEY
+
 // `id` can also be one of:
 // 'Nodejs', 'Bash', 'Python3', 'Java', 'Go', 'Rust', 'PHP', 'Perl', 'DotNET'
 // We're working on custom environments.
 const session = await Session.create({
   id: 'Nodejs',
+  apiKey: E2B_API_KEY,
 })
+
 await session.close()
