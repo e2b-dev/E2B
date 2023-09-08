acbde14af2f4dbc75aa9e646884d5c386b9240ee
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-09-08 08:49:39 +0200

    feat(docs): Convey TS friendliness

---
+++ apps/docs/src/utils/consts.ts
@@ -0,0 +1,17 @@
+// Beware of our conventions:
+// - use `js` for "JavaScript & TypeScript" – the code is actually JavaScript, but we wanna communicate that we are TypeScript friendly
+// @see https://e2b-team.slack.com/archives/C05AGT4UFMJ/p1694097476092009?thread_ts=1694072588.364759&cid=C05AGT4UFMJ
+// - use `javascript` for explicitly JavaScript
+// - use `typescript` for explicitly TypeScript
+
+export const languageNames: Record<string, string> = {
+  js: 'JavaScript & TypeScript',
+  ts: '⚠️ FIXME See note in apps/docs/src/utils/consts.ts', // Please avoid using `ts`, see note above
+  javascript: 'JavaScript',
+  typescript: 'TypeScript',
+
+  php: 'PHP',
+  python: 'Python',
+  ruby: 'Ruby',
+  go: 'Go',
+}
