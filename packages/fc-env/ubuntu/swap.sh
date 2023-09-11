ab76f93e6366a7d57923f285e153414794401583
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-09-11 16:58:46 +0200

    Remove previously used scripts

--- packages/fc-env/ubuntu/swap.sh
+++
@@ -1,9 +0,0 @@
-# Setup swapfile in Ubuntu
-
-# https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-22-04
-
-fallocate -l 500M /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-sysctl vm.swappiness=1
