aaa1d891ec6e061cb2c258e2d5b03edc9741bce2
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-09-12 14:02:37 +0200

    Typo

--- apps/docs/src/code/js/processes/background_processes.js
+++ apps/docs/src/code/js/processes/background_processes.js
@@ -25,7 +25,7 @@ const serverRequest = await session.process.start({ cmd: 'curl localhost:8000' }
 // Wait for the server request to finish running
 const requestOutput = await serverRequest.finished
 
-// Stops the background process (it would otherwise run indefinitely)
+// Stop the background process (it would otherwise run indefinitely)
 await backgroundServer.kill() // $HighlightLine
 
 // Access the server output after the server process is killed
