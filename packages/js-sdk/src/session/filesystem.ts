c04b4452eb6bc4be851ad658fe2fb43621c75111
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-09-20 10:36:42 +0200

    feat(js-sdk): Add an option to upload and download binary files via writeBytes and readBytes (#151)

--- packages/js-sdk/src/session/filesystem.ts
+++ packages/js-sdk/src/session/filesystem.ts
@@ -10,9 +10,9 @@ export interface FileInfo {
 
 export interface FilesystemManager {
   readonly write: (path: string, content: string, opts?: CallOpts) => Promise<void>
-  // readonly writeBytes: (path: string, content: Uint8Array) => Promise<void>
+  readonly writeBytes: (path: string, content: Uint8Array) => Promise<void>
   readonly read: (path: string, opts?: CallOpts) => Promise<string>
-  // readonly readBytes: (path: string) => Promise<Uint8Array>
+  readonly readBytes: (path: string) => Promise<Uint8Array>
   readonly remove: (path: string, opts?: CallOpts) => Promise<void>
   readonly list: (path: string, opts?: CallOpts) => Promise<FileInfo[]>
   readonly makeDir: (path: string, opts?: CallOpts) => Promise<void>
