feb479f6bfa3185b770dadfafbf7a2a8ed30f030
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-09-20 12:09:13 +0200

    yo

--- apps/docs/src/components/MobileNavigation.tsx
+++ apps/docs/src/components/MobileNavigation.tsx
@@ -118,7 +118,7 @@ function MobileNavigationDialog({
           >
             <motion.div
               layoutScroll
-              className="fixed bottom-0 left-0 top-14 w-full overflow-y-auto bg-white px-4 pb-4 pt-6 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/7.5 dark:bg-zinc-900 dark:ring-zinc-800 min-[416px]:max-w-sm sm:px-6 sm:pb-10"
+              className="fixed bottom-0 left-0 top-14 w-full overflow-y-auto overflow-x-hidden bg-white px-4 pb-4 pt-6 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/7.5 dark:bg-zinc-900 dark:ring-zinc-800 min-[416px]:max-w-sm sm:px-6 sm:pb-10"
             >
               <Navigation />
             </motion.div>
