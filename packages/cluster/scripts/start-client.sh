bceac76118f6608fe5a7f2c4a10703ddfb6afd4c
ValentaTomas <valenta.and.thomas@gmail.com>
2023-09-24 20:26:04 +0000

    Fix startup script fc-vm dir

--- packages/cluster/scripts/start-client.sh
+++ packages/cluster/scripts/start-client.sh
@@ -26,7 +26,7 @@ mkdir -p /mnt/disks/envs-pipeline
 gcsfuse -o=allow_other --implicit-dirs e2b-fc-env-pipeline /mnt/disks/envs-pipeline
 
 # Copy the kernel
-env_pipeline_local_dir="/fc-envs"
+env_pipeline_local_dir="/fc-vm"
 mkdir -p $env_pipeline_local_dir
 sudo cp /mnt/disks/envs-pipeline/vmlinux.bin $env_pipeline_local_dir/vmlinux.bin
 sudo cp /mnt/disks/envs-pipeline/envd $env_pipeline_local_dir/envd
