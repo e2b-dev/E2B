071fdf6c8a03da1446eaae95cdf67edef81eb6d9
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-09-25 13:21:36 +0200

    chore(js-sdk): Compat with Node 16

---
+++ packages/js-sdk/test/_smoke.test.mjs
@@ -0,0 +1,15 @@
+import { Session } from '../src'
+import { expect, test } from 'vitest'
+
+const E2B_API_KEY = process.env.E2B_API_KEY
+
+// Smoke test = quick, high-level, basic functionality test
+test('Smoke test', async () => {
+  const session = await Session.create({ id: 'Nodejs', apiKey: E2B_API_KEY })
+  await session.filesystem.makeDir('/test/new')
+  
+  const ls = await session.filesystem.list('/test')
+  expect(ls.map(x => x.name)).toEqual(['new'])
+
+  await session.close()
+})
