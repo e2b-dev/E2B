275a7f5cfe2d7d11cba3ad6e92e6bb48054fea43
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-09-27 14:31:56 +0200

    feat(docs): Numbers for socials

---
+++ apps/docs/config.ts
@@ -0,0 +1,13 @@
+export const config = {
+  discord: {
+    slug: 'U7KEcGErtQ',
+    api: 'https://discord.com/api/guilds/1092455714431180995/widget.json',
+  },
+  github: {
+    url: 'https://github.com/e2b-dev/e2b',
+    api: 'https://api.github.com/repos/e2b-dev/e2b',
+  },
+  twitter: {
+    url: 'https://twitter.com/e2b_dev',
+  },
+}
