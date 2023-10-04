f711431ee68e853a87a4defbe0e8370899278f3a
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-10-04 08:45:41 +0200

    feat(js-sdk,py-sdk): Add Bash support to runCode

--- packages/python-sdk/e2b/session/run_code.py
+++ packages/python-sdk/e2b/session/run_code.py
@@ -8,6 +8,14 @@ from .exception import UnsupportedRuntimeException
 CodeRuntime = Literal[
     "Node16",
     "Python3",
+    "Bash",
+    # TODO: Support all runtimes that our infra supports
+    # DotNET = 'DotNET',
+    # Go = 'Go',
+    # Java = 'Java',
+    # Perl = 'Perl',
+    # PHP = 'PHP',
+    # Rust = 'Rust',
 ]
 
 
@@ -40,6 +48,10 @@ async def run_code(
         env_id = "Python3"
         binary = "python3"
         filepath = "/index.py"
+    elif runtime == "Bash":
+        env_id = "Bash"
+        binary = "bash"
+        filepath = "/main.sh"        
     else:
         raise UnsupportedRuntimeException(
             f'Invalid runtime "{runtime}". Please contact us (hello@e2b.dev) if you need support for this runtime'
