fb8c5232c31d3a1a5ac03bec4b5eebf48db62618
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-10-15 18:14:41 -0700

    Add empty line

--- packages/api/internal/db/users.go
+++ packages/api/internal/db/users.go
@@ -13,6 +13,7 @@ func (db *DB) GetDefaultTeamFromUserID(userID string) (result *models.Team, err 
 	userWhere := models.UsersTeamWhere.UserID.EQ(userID)
 	defaultTeamWhere := models.TeamWhere.IsDefault.EQ(null.BoolFrom(true))
 	joinTeam := qm.InnerJoin(models.TableNames.Teams + " on " + models.TableNames.UsersTeams + "." + models.UsersTeamColumns.TeamID + " = " + models.TableNames.Teams + "." + models.TeamColumns.ID)
+
 	userTeam, err := models.UsersTeams(qm.Load(models.UsersTeamRels.Team), joinTeam, userWhere, defaultTeamWhere).One(db.Client)
 	if err != nil {
 		errMsg := fmt.Errorf("failed to list envs: %w", err)
