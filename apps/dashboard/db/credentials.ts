76e366bbcb9b7c08ff26448e441b6bf14bb9f99f
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-10-16 16:56:20 -0700

    Remove dashboard

--- apps/dashboard/db/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const serverCreds = {
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  supabaseKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
-}
-
-export const clientCreds = {
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-}
--- apps/dashboard/utils/posthog.ts
+++
@@ -1,5 +0,0 @@
-import { PostHog } from 'posthog-node'
-
-export const client = process.env.NEXT_PUBLIC_POSTHOG_KEY ? new PostHog(
-  process.env.NEXT_PUBLIC_POSTHOG_KEY,
-) : undefined
