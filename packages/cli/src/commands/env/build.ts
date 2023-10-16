e12fb430b284ec532fe6613c6fee30b6dd2791d6
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-10-16 14:21:36 -0700

    Add docker logs; Improve messages during build

--- packages/cli/src/commands/env/build.ts
+++ packages/cli/src/commands/env/build.ts
@@ -2,8 +2,6 @@ import * as commander from 'commander'
 import * as commonTags from 'common-tags'
 import * as fs from 'fs'
 import * as fsPromise from 'fs/promises'
-// Remove cross-blob when dropping node 16 support
-// Remove cross-blob when dropping node 16 support
 import * as fData from 'formdata-node' // Remove formdata-node when dropping node 16 support
 import * as nodeFetch from 'node-fetch'
 import * as path from 'path'
@@ -12,9 +10,19 @@ import * as e2b from '@e2b/sdk'
 import { wait } from 'src/utils/wait'
 import { ensureAccessToken } from 'src/api'
 import { getFiles, getRoot } from 'src/utils/filesystem'
-import { asBold, asFormattedError, asLocal, asLocalRelative } from 'src/utils/format'
+import {
+  asBold,
+  asFormattedEnvironment,
+  asFormattedError,
+  asLocalRelative,
+} from 'src/utils/format'
 import { pathOption } from 'src/options'
 import { createBlobFromFiles } from 'src/docker/archive'
+
+const secInMs = 1000
+
+const envCheckInterval = 1000 // 1 sec
+const maxBuildTime = 10 * 60 * 1000 // 10 min
 
 const getEnv = e2b.withAccessToken(e2b.api.path('/envs/{envID}').method('get').create())
 
@@ -54,23 +62,21 @@ export const buildCommand = new commander.Command('build')
 
         if (!filePaths.length) {
           console.log(commonTags.stripIndent`
-          No files found in ${asLocalRelative(root)}.
-          Note that .gitignore and .dockerignore files are respected by default,
+          No allowed files found in ${asLocalRelative(root)}.
+          Note that .gitignore and .dockerignore files are respected by default when building the environment via from Dockerfile,
           use --no-gitignore and --no-dockerignore to override.
        `)
           return
         }
 
-        console.log('📦 Files to be uploaded to create environment:')
-        filePaths.forEach(filePath => {
-          console.log(`• ${filePath.rootPath}`)
-        })
-
         const dockerFilePath = path.join(root, 'Dockerfile')
-
         if (!fs.existsSync(dockerFilePath)) {
-          throw new Error('No Dockerfile found in the root directory.')
+          throw new Error('No Dockerfile found in the root directory')
         }
+
+        console.log(
+          `Preparing environment build (${filePaths.length} files in Docker build context)...`,
+        )
 
         const formData = new fData.FormData()
 
@@ -105,43 +111,45 @@ export const buildCommand = new commander.Command('build')
           public: boolean
           status: 'building' | 'error' | 'ready'
         }
-        console.log(`✅ Env created: ${resJson?.envID}, waiting for build to finish...`)
+        console.log(`Started building environment ${asFormattedEnvironment(resJson)}...`)
 
-        const startedAt = new Date()
-        let completed = false
-
-        while (!completed) {
-          await wait(2000)
-
-          const envResponse = await getEnv(accessToken, {
-            envID: resJson.envID,
-          })
-
-          const env = envResponse.data
-
-          if (env.status === 'building') {
-            const now = new Date()
-            const elapsed = now.getTime() - startedAt.getTime()
-            const elapsedStr = `${Math.floor(elapsed / 1000)}s`
-            console.log(`⏳ Building… (started ${elapsedStr} ago)`) // nicer
-            if (elapsed > 1000 * 60 * 2) {
-              console.log(commonTags.stripIndent`
-              ⚠️ Build taking longer than 2 minutes, something might be wrong.
-              Stopping to wait for result, but it might still finish.
-              Check by yourself by running ${asLocal('e2b env list')}
-            `)
-              completed = true
-            }
-          } else if (env.status === 'ready') {
-            completed = true
-            console.log('✅ Build completed')
-          } else if (env.status === 'error') {
-            throw new Error('Build failed')
-          }
-        }
+        await waitForBuildFinish(accessToken, resJson.envID)
       } catch (err: unknown) {
         console.error(asFormattedError((err as Error).message), err)
         process.exit(1)
       }
     },
   )
+
+async function waitForBuildFinish(accessToken: string, envID: string) {
+  const startedAt = new Date()
+  let logsOffset = 0
+
+  function elapsed() {
+    return Math.floor(Date.now() - startedAt.getTime() / secInMs)
+  }
+
+  let env: Awaited<ReturnType<typeof getEnv>> | undefined
+
+  do {
+    await wait(envCheckInterval)
+    env = await getEnv(accessToken, { envID, logs: logsOffset })
+    logsOffset += env.data.logs.length
+
+    switch (env.data.status) {
+      case 'building':
+        env.data.logs.forEach(console.log)
+        break
+      case 'ready':
+        console.log(
+          `✅ \nBuilding environment ${asFormattedEnvironment(env.data)} finished`,
+        )
+        break
+
+      case 'error':
+        throw new Error(
+          `\nBuilding environment ${asFormattedEnvironment(env.data)} failed`,
+        )
+    }
+  } while (env.data.status === 'building' && elapsed() < maxBuildTime)
+}
