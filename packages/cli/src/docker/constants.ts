376d505d337f24c5e5da312885fa4354113666fc
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-10-16 17:53:52 -0700

    Fix syntax error

--- packages/cli/src/docker/constants.ts
+++ packages/cli/src/docker/constants.ts
@@ -1,5 +1,7 @@
 export const dockerfileName = 'e2b.Dockerfile'
-]
+
+export const dockefileFallbackName = 'Dockerfile'
+
 export const basicDockerfile = `# You can use most Debian-based base images
 FROM ubuntu: 20.04
 
