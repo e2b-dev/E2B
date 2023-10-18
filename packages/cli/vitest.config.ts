81886cd38f1cf04a082ca5c355980a6b1be42bb3
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-10-18 11:46:26 -0700

    Skip smoke test; Run test sequentially

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    threads: false,
     globals: false,
     environment: 'node',
     server: {},
--- packages/js-sdk/test/_smoke.test.mjs
+++ packages/js-sdk/test/_smoke.test.mjs
@@ -2,7 +2,7 @@ import { Session } from '../src'
 import { expect, test } from 'vitest'
 
 // Smoke test = quick, high-level, basic functionality test
-test('Smoke test', async () => {
+test.skip('Smoke test', async () => {
     const session = await Session.create({ id: 'Nodejs' })
     await session.filesystem.makeDir('/test/new')
 
