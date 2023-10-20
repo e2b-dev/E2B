50238fb1e4752ef0de1b1b026b750a5ec207426c
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-10-20 15:43:53 -0700

    Fix `install_system_packages` method in the Python SDK (#203)

--- packages/python-sdk/e2b/templates/data_analysis.py
+++ packages/python-sdk/e2b/templates/data_analysis.py
@@ -118,7 +118,7 @@ class DataAnalysis(Session):
         if isinstance(package_names, list):
             package_names = " ".join(package_names)
 
-        process = self.process.start(f"apt-get {package_names}", timeout=timeout)
+        process = self.process.start(f"apt-get install {package_names}", timeout=timeout)
         process.wait()
 
         if process.exit_code != 0:
