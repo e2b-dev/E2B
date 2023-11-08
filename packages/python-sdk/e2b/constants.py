1411bd6b6e456955a698ef859f3ed6240664c7c9
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-11-08 13:27:06 +0100

    Change DEBUG env var to E2B_DEBUG to prevent accidentally switching SDKs to local mode when trying to print debug logs

--- packages/python-sdk/e2b/constants.py
+++ packages/python-sdk/e2b/constants.py
@@ -5,7 +5,9 @@ SANDBOX_REFRESH_PERIOD = 5  # seconds
 TIMEOUT = 60
 
 API_DOMAIN = "api.e2b.dev"
-API_HOST = "http://localhost:3000" if os.getenv("DEBUG") else f"https://{API_DOMAIN}"
+API_HOST = (
+    "http://localhost:3000" if os.getenv("E2B_DEBUG") else f"https://{API_DOMAIN}"
+)
 
 SANDBOX_DOMAIN = "e2b.dev"
 
