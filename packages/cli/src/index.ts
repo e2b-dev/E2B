b27561f4a4e51e17d5e44803fe1a054429bfc272
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-11-10 15:34:12 +0100

    Fix error parsing in build request

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -14,6 +14,7 @@ updateNotifier.default({
   updateCheckInterval: 1000 * 60 * 60 * 8, // 8 hours
 }).notify()
 
+
 program
   .version(packageJSON.version, undefined, 'Display E2B CLI version')
   .addOption(new Option('-cmd2json').hideHelp()).on('option:-cmd2json', () => {
