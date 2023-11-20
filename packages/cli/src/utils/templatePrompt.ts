43e8f6f1f0364861749412122c9f3a8e22b911a3
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-11-20 13:33:41 +0100

    Cleanup

--- packages/cli/src/utils/templatePrompt.ts
+++ packages/cli/src/utils/templatePrompt.ts
@@ -3,27 +3,6 @@ import * as chalk from 'chalk'
 
 import { asFormattedSandboxTemplate } from 'src/utils/format'
 
-export async function getPromptEnv(
-  envs: e2b.components['schemas']['Environment'][],
-  text: string,
-) {
-  const inquirer = await import('inquirer')
-  const envsAnwsers = await inquirer.default.prompt([
-    {
-      name: 'env',
-      message: chalk.default.underline(text),
-      type: 'list',
-      pageSize: 50,
-      choices: envs.map(e => ({
-        name: asFormattedSandboxTemplate(e),
-        value: e,
-      })),
-    },
-  ])
-
-  return envsAnwsers['env'] as e2b.components['schemas']['Environment']
-}
-
 export async function getPromptEnvs(
   envs: e2b.components['schemas']['Environment'][],
   text: string,
