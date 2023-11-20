532a2a44900acc66e5ade7a7f24c12180ccbce8c
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-11-20 16:47:18 +0100

    Cleanup errors

--- packages/api/go.mod
+++ packages/api/go.mod
@@ -13,7 +13,6 @@ require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/size v0.0.0-20230212012657-e14a14094dc4
 	github.com/gin-gonic/gin v1.9.1
-	github.com/google/go-containerregistry v0.16.1
 	github.com/hashicorp/nomad/api v0.0.0-20231031213001-c1fa145765b2
 	github.com/jellydator/ttlcache/v3 v3.1.0
 	github.com/lib/pq v1.10.9
@@ -46,9 +45,6 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
-	github.com/docker/cli v24.0.0+incompatible // indirect
-	github.com/docker/docker v24.0.0+incompatible // indirect
-	github.com/docker/docker-credential-helpers v0.7.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
@@ -87,15 +83,12 @@ require (
 	github.com/mostynb/go-grpc-compression v1.2.2 // indirect
 	github.com/open-telemetry/otel-arrow v0.6.0 // indirect
 	github.com/open-telemetry/otel-arrow/collector v0.6.0 // indirect
-	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.18 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
 	github.com/sethvargo/go-envconfig v0.9.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.23.10 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
-	github.com/sirupsen/logrus v1.9.1 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
--- packages/api/internal/handlers/env_delete.go
+++ packages/api/internal/handlers/env_delete.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
-	"cloud.google.com/go/artifactregistry/apiv1/artifactregistrypb"
 	"fmt"
 	"net/http"
 	"strings"
+
+	"cloud.google.com/go/artifactregistry/apiv1/artifactregistrypb"
+	"go.opentelemetry.io/otel/attribute"
 
 	"github.com/e2b-dev/infra/packages/api/internal/api"
 	"github.com/e2b-dev/infra/packages/api/internal/utils"
@@ -15,7 +17,7 @@ import (
 // DeleteEnvsEnvID serves to delete an env (e.g. in CLI)
 func (a *APIStore) DeleteEnvsEnvID(c *gin.Context, aliasOrEnvID api.EnvID) {
 	ctx := c.Request.Context()
-	//span := trace.SpanFromContext(ctx)
+	// span := trace.SpanFromContext(ctx)
 
 	cleanedAliasOrEnvID, err := utils.CleanEnvID(aliasOrEnvID)
 	if err != nil {
@@ -49,6 +51,13 @@ func (a *APIStore) DeleteEnvsEnvID(c *gin.Context, aliasOrEnvID api.EnvID) {
 		return
 	}
 
+	telemetry.SetAttributes(ctx,
+		attribute.String("env.user_id", userID.String()),
+		attribute.String("env.team_id", team.ID.String()),
+		attribute.String("team_name", team.Name),
+		attribute.String("env.id", envID),
+	)
+
 	if !hasAccess {
 		a.sendAPIStoreError(c, http.StatusForbidden, "You don't have access to this sandbox template")
 
@@ -58,41 +67,51 @@ func (a *APIStore) DeleteEnvsEnvID(c *gin.Context, aliasOrEnvID api.EnvID) {
 		return
 	}
 
-	err = a.nomad.DeleteEnv(a.tracer, ctx, envID)
-	if err != nil {
-		errMsg := fmt.Errorf("error when deleting env: %w", err)
+	deleteJobErr := a.nomad.DeleteEnv(a.tracer, ctx, envID)
+	if deleteJobErr != nil {
+		errMsg := fmt.Errorf("error when deleting env files from fc-envs disk: %w", deleteJobErr)
 		telemetry.ReportCriticalError(ctx, errMsg)
+	} else {
+		telemetry.ReportEvent(ctx, "deleted env from fc-envs disk")
+	}
+
+	dockerContextDelErr := a.cloudStorage.deleteFolder(ctx, strings.Join([]string{"v1", envID}, "/"))
+	if dockerContextDelErr != nil {
+		errMsg := fmt.Errorf("error when deleting env docker context from storage bucket: %w", dockerContextDelErr)
+		telemetry.ReportCriticalError(ctx, errMsg)
+	} else {
+		telemetry.ReportEvent(ctx, "deleted env docker context form storage bucket")
+	}
+
+	op, artifactRegistryDeleteErr := a.artifactRegistry.DeletePackage(ctx, &artifactregistrypb.DeletePackageRequest{Name: "projects/e2b-prod/locations/us-central1/repositories/custom-environments/packages/" + envID})
+	if artifactRegistryDeleteErr != nil {
+		errMsg := fmt.Errorf("error when deleting env image from registry: %w", artifactRegistryDeleteErr)
+		telemetry.ReportCriticalError(ctx, errMsg)
+	} else {
+		telemetry.ReportEvent(ctx, "started deleting env image from registry")
+
+		err = op.Wait(ctx)
+		if err != nil {
+			errMsg := fmt.Errorf("error when waiting for env image deleting from registry: %w", err)
+			telemetry.ReportCriticalError(ctx, errMsg)
+		} else {
+			telemetry.ReportEvent(ctx, "deleted env image from registry")
+		}
+	}
+
+	// TODO: Add cascade
+	dbErr := a.supabase.DeleteEnv(ctx, envID)
+	if dbErr != nil {
+		errMsg := fmt.Errorf("error when deleting env from db: %w", dbErr)
+		telemetry.ReportCriticalError(ctx, errMsg)
+
+		a.sendAPIStoreError(c, http.StatusInternalServerError, "Error when deleting env")
 
 		return
 	}
 
-	// TODO: Remove retunrs
-	// TODO: Remove docker context
-	err = a.cloudStorage.deleteFolder(ctx, strings.Join([]string{"v1", envID}, "/"))
-	if err != nil {
-		errMsg := fmt.Errorf("error when deleting env: %w", err)
-		telemetry.ReportCriticalError(ctx, errMsg)
+	telemetry.ReportEvent(ctx, "deleted env from db")
 
-		return
-	}
-	// TODO: Remove docker image
-	op, err := a.artifactRegistry.DeletePackage(ctx, &artifactregistrypb.DeletePackageRequest{Name: "projects/e2b-prod/locations/us-central1/repositories/custom-environments/packages/" + envID})
-	if err != nil {
-		errMsg := fmt.Errorf("error when deleting env: %w", err)
-		telemetry.ReportCriticalError(ctx, errMsg)
-
-		return
-	}
-
-	err = op.Wait(ctx)
-	if err != nil {
-		errMsg := fmt.Errorf("error when deleting env: %w", err)
-		telemetry.ReportCriticalError(ctx, errMsg)
-
-		return
-	}
-
-	// TODO: Remove from DB (cascade?)
 	properties := a.GetPackageToPosthogProperties(&c.Request.Header)
 	a.IdentifyAnalyticsTeam(team.ID.String(), team.Name)
 	a.CreateAnalyticsUserEvent(userID.String(), team.ID.String(), "deleted environment", properties.Set("environment", envID))
