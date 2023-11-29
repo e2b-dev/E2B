61f72ed7a0b0c2e9f6beaf3c4c1c6bf644383b3e
Jakub Novák <jakub@e2b.dev>
2023-11-29 13:10:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0, < 1.6.0"
   backend "gcs" {
-    bucket = "e2b-intra-test-terraform-state"
+    bucket = "e2b-terraform-state"
     prefix = "terraform/orchestration/state"
   }
   required_providers {
