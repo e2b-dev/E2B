c2d106eb39c091e36af5ffb62cdfd2bb25760d02
Jakub Novak <jakub@e2b.dev>
2024-01-19 15:15:06 +0100

    Generate sitemap in docs

---
+++ apps/docs/src/app/sitemap.ts
@@ -0,0 +1,43 @@
+import { MetadataRoute } from "next";
+import fs from "fs";
+import path from "path";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+ const appDirectory = path.join(process.cwd(), "src", "app");
+ const mdxFilePaths = getAllMdxFilePaths(appDirectory);
+
+ const sitemap = mdxFilePaths.map((filePath) => {
+   const pathname = path.relative(appDirectory, filePath).replace(/\/page\.mdx$/, "");
+   if (pathname === "page.mdx") {
+     return null;
+   }
+   const url = `https://e2b.dev/docs/${pathname}`;
+   const lastModified = fs.statSync(filePath).mtime;
+   return {
+     url,
+     lastModified,
+   };
+ }).filter(Boolean);
+
+ sitemap.unshift({
+   url: "https://e2b.dev",
+   lastModified: new Date(),
+ });
+
+ return sitemap;
+}
+
+function getAllMdxFilePaths(directory: string): string[] {
+ const fileNames = fs.readdirSync(directory);
+ const filePaths = fileNames.map((fileName) => {
+   const filePath = path.join(directory, fileName);
+   const stat = fs.statSync(filePath);
+   if (stat.isDirectory()) {
+     return getAllMdxFilePaths(filePath);
+   } else if (path.basename(filePath) === "page.mdx") {
+     return filePath;
+   }
+ }).filter(Boolean);
+
+ return Array.prototype.concat(...filePaths);
+}
