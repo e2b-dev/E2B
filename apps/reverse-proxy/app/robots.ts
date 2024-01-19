7654e04fc9e6ca283c75c8780d2417e74bc217cd
Jakub Novak <jakub@e2b.dev>
2024-01-19 14:58:52 +0100

    Add robots.txt

---
+++ apps/reverse-proxy/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+ return {
+   rules: {
+     userAgent: '*',
+     
+     allow: '/',
+   },
+
+   sitemap: 'https://e2b.dev/sitemap.xml',
+ }
+}
