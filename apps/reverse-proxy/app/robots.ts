555cebb450d3e4c1eaa0bbe43f86594056f928d3
Jakub Novak <jakub@e2b.dev>
2024-01-19 16:09:06 +0100

    Add sitemaps

--- apps/reverse-proxy/app/robots.ts
+++ apps/reverse-proxy/app/robots.ts
@@ -1,13 +1,13 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
- return {
-   rules: {
-     userAgent: '*',
-     
-     allow: '/',
-   },
+  return {
+    rules: {
+      userAgent: "*",
 
-   sitemap: 'https://e2b.dev/sitemap.xml',
- }
+      allow: "/",
+    },
+
+    sitemap: "https://e2b.dev/sitemap.xml",
+  };
 }
---
+++ apps/reverse-proxy/app/sitemap.ts
@@ -0,0 +1,105 @@
+import { MetadataRoute } from "next";
+import { XMLParser } from "fast-xml-parser";
+
+type ChangeFrequency =
+  | "always"
+  | "hourly"
+  | "daily"
+  | "weekly"
+  | "monthly"
+  | "yearly"
+  | "never";
+
+type FramerWebsite = {
+  sitemapUrl: string;
+  lastModified?: string | Date;
+  changeFrequency?: ChangeFrequency;
+  priority?: number;
+};
+
+const framerWebsites: FramerWebsite[] = [
+  {
+    sitemapUrl: "https://e2b-landing-page.framer.website/sitemap.xml",
+    priority: 1.0,
+    changeFrequency: "daily",
+  },
+  {
+    sitemapUrl: "https://e2b-blog.framer.website/sitemap.xml",
+    priority: 0.9,
+    changeFrequency: "daily",
+  },
+  {
+    sitemapUrl: "https://e2b-changelog.framer.website/sitemap.xml",
+    priority: 0.2,
+    changeFrequency: "weekly",
+  },
+];
+
+const otherApps = ["https://e2b.dev/docs/sitemap.xml"];
+
+type SitemapData = {
+  loc: string;
+  lastmod?: string | Date;
+  changefreq?: ChangeFrequency;
+  priority?: number;
+};
+
+type Sitemap = {
+  urlset: {
+    url: SitemapData | SitemapData[];
+  };
+};
+
+async function getXmlData(url: string): Promise<Sitemap> {
+  const parser = new XMLParser();
+
+  const response = await fetch(url);
+  const text = await response.text();
+
+  return parser.parse(text) as Sitemap;
+}
+async function getSitemap(
+  url: string,
+  priority?: number,
+  changeFrequency?: ChangeFrequency
+): Promise<MetadataRoute.Sitemap> {
+  const data = await getXmlData(url);
+
+  if (Array.isArray(data.urlset.url)) {
+    return data.urlset.url.map((line) => {
+      return {
+        url: line.loc,
+        priority: line?.priority || priority,
+        changeFrequency: line?.changefreq || changeFrequency,
+      };
+    });
+  } else {
+    return [
+      {
+        url: data.urlset.url.loc,
+        priority: data.urlset.url?.priority || priority,
+        changeFrequency: data.urlset.url?.changefreq || changeFrequency,
+      },
+    ];
+  }
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  let mergedSitemap: MetadataRoute.Sitemap = [];
+
+  for (const nativeUrl of otherApps) {
+    const urls = await getSitemap(nativeUrl);
+    mergedSitemap = mergedSitemap.concat(...urls);
+  }
+
+  for (const framerWebsite of framerWebsites) {
+    const urls = await getSitemap(
+      framerWebsite.sitemapUrl,
+      framerWebsite.priority,
+      framerWebsite.changeFrequency
+    );
+    mergedSitemap = mergedSitemap.concat(...urls);
+  }
+
+  return mergedSitemap;
+}
