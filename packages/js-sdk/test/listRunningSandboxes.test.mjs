95f8c0eccafc9889641ad662239646c2de74a20d
Jakub Novak <jakub@e2b.dev>
2024-01-29 17:20:55 +0100

    Fix tests

--- packages/js-sdk/test/listRunningSandboxes.test.mjs
+++ packages/js-sdk/test/listRunningSandboxes.test.mjs
@@ -7,14 +7,14 @@ test('list running sandboxes', async () => {
     sandboxPromises.push(Sandbox.create({
       id: 'base',
       apiKey: process.env.E2B_API_KEY,
-      metadata: { n: i.toString() }
+      metadata: { n: `js${i}` }
     }))
   }
   const sandboxes = await Promise.all(sandboxPromises)
 
-  const running = await Sandbox.list()
+  const running = (await Sandbox.list()).filter(s => s.metadata?.n?.startsWith('js'))
   expect(running.length).toEqual(3)
-  expect(new Set(running.map(s => s.metadata['n']))).toEqual(new Set(['0', '1', '2']))
+  expect(new Set(running.map(s => s.metadata['n']))).toEqual(new Set(['js0', 'js1', 'js2']))
 
   for (const sandbox of sandboxes) {
     await sandbox.close()
