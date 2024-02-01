5ab6198312bce944cfa8d6464d14c961e59db786
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-02-01 15:34:29 +0100

    Add kernel bucket and upload scripts

--- packages/buckets/main.tf
+++ packages/buckets/main.tf
@@ -9,10 +9,20 @@ resource "google_storage_bucket" "envs_docker_context" {
   labels = var.labels
 }
 
-
 resource "google_storage_bucket" "setup_bucket" {
   location = var.gcp_region
   name     = "${var.gcp_project_id}-instance-setup"
+
+  public_access_prevention    = "enforced"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  labels = var.labels
+}
+
+resource "google_storage_bucket" "fc_kernels_bucket" {
+  location = var.gcp_region
+  name     = "${var.gcp_project_id}-fc-kernels"
 
   public_access_prevention    = "enforced"
   storage_class               = "STANDARD"
@@ -50,3 +60,8 @@ resource "google_storage_bucket_iam_member" "instance_setup_bucket_iam" {
   member = "serviceAccount:${var.gcp_service_account_email}"
 }
 
+resource "google_storage_bucket_iam_member" "fc_kernels_bucket_iam" {
+  bucket = google_storage_bucket.fc_kernels_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${var.gcp_service_account_email}"
+}
---
+++ packages/fc-kernels/upload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GCP_PROJECT_ID=$1
+
+mkdir -p downloads
+rm -rf downloads/*
+
+# Check if the file exists
+if [ -f "kernel_versions.txt" ]; then
+  # Read kernel versions from the file
+  while IFS= read -r version; do
+    # Download kernel
+    echo "Downloading kernel ${version}..."
+    mkdir -p "downloads/${version}"
+    curl "https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/${version}/x86_64/vmlinux-${version}" -o "downloads/${version}/vmlinux.bin"
+
+    # Upload kernel to GCP bucket
+    gsutil -h "Cache-Control:no-cache, max-age=0" cp -n -r "downloads/${version}" gs://e2b-test-bucket-2
+    # gsutil -h "Cache-Control:no-cache, max-age=0" cp "vmlinux-${version}" "gs://${GCP_PROJECT_ID}-/vmlinux-${version}"
+
+    rm -rf "downloads/${version}"
+  done <"kernel_versions.txt"
+
+  echo "All kernels downloaded and uploaded to GCP bucket successfully."
+else
+  echo "Error: kernel_versions.txt not found."
+fi
+
+rm -rf downloads/*
