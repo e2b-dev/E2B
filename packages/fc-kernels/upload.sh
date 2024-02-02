da66bfd3f11eeba472adeed788801aaae9174d0b
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-02-02 13:19:22 +0100

    Prevent downloading nonexisting kernels

--- packages/fc-kernels/upload.sh
+++ packages/fc-kernels/upload.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 GCP_PROJECT_ID=$1
 
+FC_VERSION="v1.7"
+
 mkdir -p downloads
 rm -rf downloads/*
 
@@ -12,14 +14,19 @@ if [ -f "kernel_versions.txt" ]; then
   # Read kernel versions from the file
   while IFS= read -r version; do
     # Download kernel
-    echo "Downloading kernel ${version}..."
-    mkdir -p "downloads/vmlinux-${version}"
-    curl "https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/${version}/x86_64/vmlinux-${version}" -o "downloads/vmlinux-${version}/vmlinux.bin"
+
+    stringarray=($version)
+    fc_version=${stringarray[0]}
+    kernel_version=${stringarray[1]}
+
+    echo "Downloading kernel ${kernel_version}..."
+    mkdir -p "downloads/vmlinux-${kernel_version}"
+    curl --fail "https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/${fc_version}/x86_64/vmlinux-${kernel_version}" -o "downloads/vmlinux-${kernel_version}/vmlinux.bin"
 
     # Upload kernel to GCP bucket
-    gsutil -h "Cache-Control:no-cache, max-age=0" cp -n -r "downloads/vmlinux-${version}" "gs://${GCP_PROJECT_ID}-fc-kernels"
+    gsutil -h "Cache-Control:no-cache, max-age=0" cp -r "downloads/vmlinux-${kernel_version}" "gs://${GCP_PROJECT_ID}-fc-kernels"
 
-    rm -rf "downloads/${version}"
+    rm -rf "downloads/${kernel_version}"
   done <"kernel_versions.txt"
 
   echo "All kernels downloaded and uploaded to GCP bucket successfully."
