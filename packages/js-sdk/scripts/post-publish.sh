acd3eee209dd31b9fa2fb547e894a6f73a338a4d
Jakub Novak <jakub@e2b.dev>
2024-02-03 17:11:01 +0100

    Dont fail

--- packages/js-sdk/scripts/post-publish.sh
+++ packages/js-sdk/scripts/post-publish.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 npm pkg set 'name'='@e2b/sdk'
 npm publish --no-git-checks
 npm pkg set 'name'='e2b'
-npm deprecate "@e2b/sdk@$(npm pkg get version | tr -d \")" 'The package "@e2b/sdk" has been renamed to "e2b". Please uninstall the old one and install the new "npm uninstall @e2b/sdk && npm install e2b".' || true
+npm deprecate "@e2b/sdk@$(npm pkg get version | tr -d \")" "The package @e2b/sdk has been renamed to e2b. Please uninstall the old one and install the new by running following command: npm uninstall @e2b/sdk && npm install e2b"
