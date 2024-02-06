7cd857f252de14323d07908b59d443dade47c21a
Jakub Novak <jakub@e2b.dev>
2024-02-06 15:54:00 -0800

    Fix build

--- packages/fc-kernels/build.sh
+++ packages/fc-kernels/build.sh
@@ -8,17 +8,20 @@ function build_version {
   kernel_version=${stringarray[1]}
   git checkout "$version"
 
-  cp ../configs/"${kernel_version}.config" .config
+  cp configs/"${kernel_version}.config" linux/.config
+
+  cd linux
+
   make vmlinux -j "$(nproc)"
 
   mkdir -p "../builds/vmlinux-${kernel_version}"
   cp vmlinux "../builds/vmlinux-${kernel_version}/vmlinux.bin"
+
+  cd ..
 }
 
 
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux
-
-cd linux
 
 grep -v '^ *#' < kernel_versions.txt | while IFS= read -r version
 do
