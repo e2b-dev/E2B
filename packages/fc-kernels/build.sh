a083a68535508e53db7dc7aeb85b528cbcf5e201
Jakub Novak <jakub@e2b.dev>
2024-02-06 16:15:34 -0800

    Fix folders

--- packages/fc-kernels/build.sh
+++ packages/fc-kernels/build.sh
@@ -7,24 +7,22 @@ function build_version {
   stringarray=($version)
   kernel_version=${stringarray[1]}
 
-  cp configs/"${kernel_version}.config" linux/.config
+  cp ../configs/"${kernel_version}.config" .config
 
-  cd linux
   git checkout "v${kernel_version}"
 
   make vmlinux -j "$(nproc)"
 
   mkdir -p "../builds/vmlinux-${kernel_version}"
   cp vmlinux "../builds/vmlinux-${kernel_version}/vmlinux.bin"
-
-  cd ..
 }
 
 
 git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux
+cd linux
 git fetch --tags
 
-grep -v '^ *#' < kernel_versions.txt | while IFS= read -r version
+grep -v '^ *#' < ../kernel_versions.txt | while IFS= read -r version
 do
   build_version "$version"
 done
