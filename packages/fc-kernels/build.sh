22a8f280c9e26e94ce4c4179739a5065a6902f7d
Jakub Novak <jakub@e2b.dev>
2024-02-06 16:41:22 -0800

    Add echos

--- packages/fc-kernels/build.sh
+++ packages/fc-kernels/build.sh
@@ -4,15 +4,20 @@ set -euo pipefail
 
 function build_version {
   local version=$1
+  echo "Starting build for kernel version: $version"
+
   stringarray=($version)
   kernel_version=${stringarray[1]}
 
   cp ../configs/"${kernel_version}.config" .config
 
+  echo "Checking out repo for kernel at version: $kernel_version"
   git checkout "v${kernel_version}"
 
+  echo "Building kernel version: $kernel_version"
   make vmlinux -j "$(nproc)"
 
+  echo "Copying finished build to builds directory"
   mkdir -p "../builds/vmlinux-${kernel_version}"
   cp vmlinux "../builds/vmlinux-${kernel_version}/vmlinux.bin"
 }
