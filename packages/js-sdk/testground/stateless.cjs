841b24a06d3ce16d744bd2842b662000ceeb1f16
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-02-18 14:13:55 -0800

    Update example

--- packages/js-sdk/testground/stateless.cjs
+++ packages/js-sdk/testground/stateless.cjs
@@ -10,15 +10,19 @@ async function main() {
   const apiKey = '...'
   const lifetime = 60_000 * 5 // 5 minutes
 
+  console.log('> Creating sandbox...')
   const sandboxID = await create({ apiKey }, {
     keepAliveFor: lifetime,
   })
+  console.log('...sandbox created, sandbox id -', sandboxID)
 
-  exec({
+  const cmd = 'echo hello world'
+  console.log(`\n> Executing command "${cmd}"...`)
+  await exec({
     apiKey,
     sandboxID
   }, {
-    cmd: 'echo hello',
+    cmd,
     onStdout: (data) => {
       console.log(data)
     },
@@ -26,28 +30,26 @@ async function main() {
       console.error(data)
     },
   })
-    .then(() => {
-      console.log('Kill sandbox...')
-      return kill({
-        apiKey,
-        sandboxID,
-      })
-    })
-    .then(() => {
-      console.log('...killed')
-    })
+  console.log('...command finished')
 
+  console.log('\n> Downloading file...')
   const content = await downloadFile({
     apiKey,
     sandboxID,
   }, {
     path: '/etc/hosts'
   })
-  console.log('downloaded file', content)
-  console.log('downloaded file content as string:', content.toString())
+  console.log('...downloaded file:\n', content.toString())
+
+  console.log(`\n> Killing sandbox "${sandboxID}"...`)
+  await kill({
+    apiKey,
+    sandboxID,
+  })
+  console.log('...sandbox killed')
 }
 
 main()
   .then(() => {
-    console.log('done')
+    console.log('\n\nFinished')
   })
