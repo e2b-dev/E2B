888d907af029939d8213b5c3cfd27c2819142861
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-02-20 17:31:04 -0800

    Change displayed data

--- apps/docs/src/app/usage/Credits.tsx
+++ apps/docs/src/app/usage/Credits.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 import {
   Ticket,
 } from 'lucide-react'
+import Link from 'next/link'
 
+import ManageBilling from '@/components/ManageBilling'
 import { useUsage } from '@/utils/useUsage'
 import { useUser } from '@/utils/useUser'
 
@@ -17,10 +19,10 @@ function formatCurrency(value: number) {
 }
 
 function Credits() {
-  // TODO: If not logged in, show a message to log in
   const { user } = useUser()
 
   // TODO: Show user their plan
+
   // TODO: Add "switch tier"/"manage billing" button
   const { credits, costs } = useUsage()
 
@@ -33,14 +35,18 @@ function Credits() {
   }
 
   return (
-    <div className="flex flex-col justify-start items-start gap-4">
-      <span>Your sandbox usage for the past 3 months</span>
+    <div className="flex flex-col justify-start items-start gap-3">
+      <ManageBilling />
+      <div className="flex items-center justify-start gap-1">
+        <Link href="/pricing">Change plan</Link>
+      </div>
 
       <div className="flex flex-col justify-start items-start gap-1">
         <div className="flex items-center gap-1">
-          <Ticket size={20} />
-          <span className="font-medium">Credits left</span>
+          <Ticket className="text-white" size={20} />
+          <span className="font-medium text-white">Credits left</span>
         </div>
+        <span className="text-sm">Will be used when generating invoice</span>
         <span className="text-sm font-mono text-green-300/80">${credits}</span>
       </div>
 
@@ -59,12 +65,7 @@ function Credits() {
 
               <div className="flex flex-col items-start justify-start gap-1">
                 <span className="font-bold">{new Date().getMonth() + 1 === cost.month ? 'To pay' : 'Paid'}</span>
-                <div className="flex items-center gap-3">
-                  <span className="text-sm font-mono">${formatCurrency(cost.unpaid_costs)}</span>
-                  {usedCredits(cost.total_costs, cost.unpaid_costs) > 0 &&
-                    <span className="text-sm font-mono text-green-300/80">(${formatCurrency(usedCredits(cost.total_costs, cost.unpaid_costs))} credits used)</span>
-                  }
-                </div>
+                <span className="text-sm font-mono">${formatCurrency(cost.unpaid_costs)}</span>
               </div>
             </div>
           </React.Fragment>
