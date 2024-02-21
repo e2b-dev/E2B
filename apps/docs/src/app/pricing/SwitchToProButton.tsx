8682403a5dfade7c3d6d43bf90e26b8ada179bf6
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-02-21 09:49:28 -0800

    Add text for pro users without credit card

--- apps/docs/src/app/pricing/SwitchToProButton.tsx
+++ apps/docs/src/app/pricing/SwitchToProButton.tsx
@@ -52,11 +52,20 @@ function SwitchTierButton() {
         )}
 
 
+
         {user.pricingTier.id !== tiers.pro.id && (
           <Button
             onClick={createCheckoutSession}
           >
             Switch to Pro
+          </Button>
+        )}
+
+        {(user.pricingTier.id === tiers.pro.id && !user.teams[0].team_billing[0].credit_card_added) && (
+          <Button
+            onClick={createCheckoutSession}
+          >
+            Add Credit Card
           </Button>
         )}
       </div>
--- apps/docs/src/utils/useUser.tsx
+++ apps/docs/src/utils/useUser.tsx
@@ -13,6 +13,9 @@ type Team = {
   tier: string
   is_default: boolean
   email: string
+  team_billing: {
+    credit_card_added: boolean
+  }[]
 }
 
 type UserContextType = {
@@ -103,12 +106,14 @@ export const CustomUserContextProvider = (props) => {
       // @ts-ignore
       const { data: userTeams, teamsError } = await supabase
         .from('users_teams')
-        .select('teams (id, name, is_default, tier, email)')
+        .select('teams (id, name, is_default, tier, email, team_billing (credit_card_added))')
         .eq('user_id', session?.user.id) // Due to RLS, we could also safely just fetch all, but let's be explicit for sure
+      console.log('>>>>>>>>>>>>>>>>>>>> userTeams', userTeams)
       if (teamsError) Sentry.captureException(teamsError)
       // TODO: Adjust when user can be part of multiple teams
       // @ts-ignore
       const teams = userTeams?.map(userTeam => userTeam.teams as Team)
+
       const defaultTeam = teams?.find(team => team.is_default)
 
       if (!defaultTeam) {
