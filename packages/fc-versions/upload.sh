c383fe5cc243b6c9729e7c659c3f8b5f33a60d22
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-02-20 19:22:09 -0800

    Fix FCs upload

--- packages/fc-versions/upload.sh
+++ packages/fc-versions/upload.sh
@@ -4,19 +4,6 @@ set -euo pipefail
 
 GCP_PROJECT_ID=$1
 
-# Check if the file exists
-if [ -f "firecracker_versions.txt" ]; then
-  while IFS= read -r version; do
-
-    # Upload kernel to GCP bucket
-    gsutil -h "Cache-Control:no-cache, max-age=0" cp -r "builds/${version}" "gs://${GCP_PROJECT_ID}-fc-versions"
-
-    rm -rf "builds/${firecracker_version}"
-  done <"firecracker_versions.txt"
-
-  echo "All FC versions uploaded to GCP bucket successfully."
-else
-  echo "Error: firecracker_versions.txt not found."
-fi
+gsutil -h "Cache-Control:no-cache, max-age=0" cp -r "builds/*" "gs://${GCP_PROJECT_ID}-fc-versions"
 
 rm -rf builds/*
