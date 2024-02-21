91334befcd4a66dd9718e0c3cb0814e3cc2517a8
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-02-20 18:34:09 -0800

    Simplify FC version handling in API

--- packages/api/internal/sandbox/sandbox_features.go
+++ packages/api/internal/sandbox/sandbox_features.go
@@ -1,17 +1,14 @@
 package sandbox
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
 )
 
 type VersionInfo struct {
-	commitsAfterLastTag int
-	commitHash          string
-	lastReleaseVersion  semver.Version
+	commitHash         string
+	lastReleaseVersion semver.Version
 }
 
 func stripVersionPrefix(version string) string {
@@ -19,79 +16,28 @@ func stripVersionPrefix(version string) string {
 }
 
 func NewVersionInfo(fcVersion string) (info VersionInfo, err error) {
-	// The structure of the fcVersion is <last_tag[-prerelease]>[-commits_after_tag][-commit_hash]
-	// Example: v1.0.0-1-g1234567
-	// The following code extracts the last tag and the commits after tag in code
+	// The structure of the fcVersion is last_tag[-prerelease]_commit_hash
+	// Example: v1.0.0-release_1234567
 
-	parts := strings.Split(fcVersion, "-")
+	parts := strings.Split(fcVersion, "_")
 
-	if len(parts) == 1 {
-		// There is only tag without any prelease
-		versionString := stripVersionPrefix(parts[0])
+	versionString := stripVersionPrefix(parts[0])
 
-		version, versionErr := semver.NewVersion(versionString)
-		if versionErr != nil {
-			return info, versionErr
-		}
-
-		info.lastReleaseVersion = *version
-	} else if len(parts) == 2 {
-		// There is a tag and a prelease
-		versionString := fmt.Sprintf("%s-%s", stripVersionPrefix(parts[0]), parts[1])
-
-		version, versionErr := semver.NewVersion(versionString)
-		if versionErr != nil {
-			return info, versionErr
-		}
-
-		info.lastReleaseVersion = *version
-	} else if len(parts) == 3 {
-		// There is a tag, commits after tag and a commit hash
-		versionString := stripVersionPrefix(parts[0])
-
-		version, versionErr := semver.NewVersion(versionString)
-		if versionErr != nil {
-			return info, versionErr
-		}
-
-		info.lastReleaseVersion = *version
-
-		commitsAfter, err := strconv.Atoi(parts[1])
-		if err != nil {
-			return info, err
-		}
-
-		info.commitsAfterLastTag = commitsAfter
-		info.commitHash = parts[2]
-	} else if len(parts) == 4 {
-		// There is a tag, prelease, commits after tag and a commit hash
-		versionString := fmt.Sprintf("%s-%s", stripVersionPrefix(parts[0]), parts[1])
-
-		version, versionErr := semver.NewVersion(versionString)
-		if versionErr != nil {
-			return info, versionErr
-		}
-
-		info.lastReleaseVersion = *version
-
-		commitsAfter, err := strconv.Atoi(parts[1])
-		if err != nil {
-			return info, err
-		}
-
-		info.commitsAfterLastTag = commitsAfter
-		info.commitHash = parts[3]
+	version, versionErr := semver.NewVersion(versionString)
+	if versionErr != nil {
+		return info, versionErr
 	}
+
+	info.lastReleaseVersion = *version
+	info.commitHash = parts[1]
 
 	return info, nil
 }
 
 func (v *VersionInfo) HasHugePages() bool {
-	if v.lastReleaseVersion.Major() >= 1 && v.lastReleaseVersion.Minor() >= 7 && v.lastReleaseVersion.Prerelease() == "dev" && v.commitsAfterLastTag >= 252 {
+	if v.lastReleaseVersion.Major() >= 1 && v.lastReleaseVersion.Minor() >= 7 {
 		return true
 	}
-
-	// TODO: When the next release is out (the huge pages should be in dev preview then), we should fixate this check only on the major version+minor version
 
 	return false
 }
