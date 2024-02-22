333f9e18bd90646fa0dae2c96e4751f813e4e848
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-02-22 22:46:11 +0000

    Cleanup

--- packages/env-instance-task-driver/internal/instance/mock_instance.go
+++ packages/env-instance-task-driver/internal/instance/mock_instance.go
@@ -24,19 +24,22 @@ func MockInstance(envID, instanceID string) {
 		ctx,
 		tracer,
 		&InstanceConfig{
-			EnvID:            envID,
-			AllocID:          "test",
-			InstanceID:       instanceID,
-			TraceID:          "test",
-			TeamID:           "test",
-			ConsulToken:      os.Getenv("CONSUL_TOKEN"),
-			LogsProxyAddress: "",
-			NodeID:           "testtesttest",
-			EnvsDisk:         "/mnt/disks/fc-envs/v1",
-			KernelVersion:    "5.10.186",
-			KernelMountDir:   "/fc-vm",
-			KernelsDir:       "/fc-kernels",
-			KernelName:       "vmlinux.bin",
+			EnvID:                 envID,
+			AllocID:               "test",
+			InstanceID:            instanceID,
+			TraceID:               "test",
+			TeamID:                "test",
+			ConsulToken:           os.Getenv("CONSUL_TOKEN"),
+			LogsProxyAddress:      "",
+			NodeID:                "testtesttest",
+			EnvsDisk:              "/mnt/disks/fc-envs/v1",
+			KernelVersion:         "vmlinux-5.10.186",
+			KernelMountDir:        "/fc-vm",
+			KernelsDir:            "/fc-kernels",
+			KernelName:            "vmlinux.bin",
+			UFFDBinaryPath:        "/fc-versions/v1.7.0-dev_8bb88311/uffd",
+			HugePages:             false,
+			FirecrackerBinaryPath: "/fc-versions/v1.7.0-dev_8bb88311/firecracker",
 		},
 		hosts,
 	)
@@ -44,5 +47,10 @@ func MockInstance(envID, instanceID string) {
 		panic(err)
 	}
 
-	instance.CleanupAfterFCStop(ctx, tracer, hosts)
+	defer instance.CleanupAfterFCStop(ctx, tracer, hosts)
+
+	err = instance.FC.Stop(ctx, tracer)
+	if err != nil {
+		panic(err)
+	}
 }
