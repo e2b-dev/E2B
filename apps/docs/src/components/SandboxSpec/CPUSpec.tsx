5ac925116bb1f2f1a3dc40209b02173dc0a65078
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-02-28 11:20:56 -0800

    Change title

--- apps/docs/src/components/SandboxSpec/CPUSpec.tsx
+++ apps/docs/src/components/SandboxSpec/CPUSpec.tsx
@@ -16,7 +16,7 @@ function CPUSpec({
     <div className="w-full flex flex-col gap-2 items-start justify-start">
       <div className="flex items-center justify-between w-full px-2 bg-brand-500/10 rounded">
         <div className="flex items-center justify-start space-between w-full w-[300px]">
-          <span className="text-sm flex-1 text-white font-bold">CPUs</span>
+          <span className="text-sm flex-1 text-white font-bold">vCPUs</span>
           <span className="text-sm text-white font-bold text-left w-[150px]">Plan</span>
         </div>
         <span className="text-sm text-white font-bold">Costs</span>
