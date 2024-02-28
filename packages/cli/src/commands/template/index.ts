f6ce4aef3d11203e5613f13b0e8e6308be603315
Jakub Novak <jakub@e2b.dev>
2024-02-27 21:22:46 -0800

    Move spawn command to sandbox group in CLI

--- packages/cli/src/commands/template/index.ts
+++ packages/cli/src/commands/template/index.ts
@@ -2,7 +2,6 @@ import * as commander from 'commander'
 
 import { buildCommand } from './build'
 import { listCommand } from './list'
-import { spawnCommand } from './spawn'
 import { initCommand } from './init'
 import { deleteCommand } from './delete'
 
@@ -10,6 +9,5 @@ export const templateCommand = new commander.Command('template').description('Ma
   .alias('tpl')
   .addCommand(buildCommand)
   .addCommand(listCommand)
-  .addCommand(spawnCommand)
   .addCommand(initCommand)
   .addCommand(deleteCommand)
