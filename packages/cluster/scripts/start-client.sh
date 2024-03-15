c106f65b43fc251373d09b19fdd42e03f8f5d710
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-03-15 15:32:23 -0700

    Use default compaction proactivness; Don't use overcommited huge pages

--- packages/cluster/scripts/start-client.sh
+++ packages/cluster/scripts/start-client.sh
@@ -18,15 +18,14 @@ echo "[Setting up huge pages]"
 sudo mkdir -p /mnt/hugepages
 mount -t hugetlbfs none /mnt/hugepages
 # Increase proactive compaction to reduce memory fragmentation for using overcomitted huge pages
-echo 40 >/proc/sys/vm/compaction_proactiveness
 
 available_ram=$(grep MemTotal /proc/meminfo | awk '{print $2}') # in KiB
 available_ram=$(($available_ram / 1024))                        # in MiB
 echo "- Total memory: $available_ram MiB"
 
-min_normal_ram=$((4 * 1024))                            # 4 GiB
-min_normal_percentage_ram=$(($available_ram * 8 / 100)) # 8% of the total memory
-max_normal_ram=$((32 * 1024))                           # 32 GiB
+min_normal_ram=$((4 * 1024))                             # 4 GiB
+min_normal_percentage_ram=$(($available_ram * 16 / 100)) # 16% of the total memory
+max_normal_ram=$((42 * 1024))                            # 42 GiB
 
 max() {
     if (($1 > $2)); then
@@ -71,7 +70,7 @@ echo "- Huge page size: $hugepage_size_in_mib MiB"
 hugepages=$(($hugepages_ram / $hugepage_size_in_mib))
 
 # This percentage will be permanently allocated for huge pages and in monitoring it will be shown as used.
-base_hugepages_percentage=60
+base_hugepages_percentage=100
 base_hugepages=$(($hugepages * $base_hugepages_percentage / 100))
 base_hugepages=$(remove_decimal $base_hugepages)
 echo "- Allocating $base_hugepages huge pages ($base_hugepages_percentage%) for base usage"
