0c766bafdef290530d2e323684c6f6a1bd41d6f6
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-03-20 23:55:58 +0000

    Cleanup envd sync

--- packages/envd/internal/clock/service.go
+++ packages/envd/internal/clock/service.go
@@ -3,52 +3,44 @@ package clock
 import (
 	"os/exec"
 	"sync"
-	"time"
 
 	"go.uber.org/zap"
 )
 
 type Service struct {
 	logger *zap.SugaredLogger
-	shell  string
-	mu     sync.Mutex
+	mu     sync.RWMutex
 }
 
-func NewService(logger *zap.SugaredLogger, shell string) *Service {
+func NewService(logger *zap.SugaredLogger) *Service {
 	return &Service{
 		logger: logger,
-		mu:     sync.Mutex{},
-		shell:  shell,
+		mu:     sync.RWMutex{},
 	}
 }
 
 func (s *Service) Sync() {
+	s.logger.Debug("Syncing clock")
 	s.mu.Lock()
 
 	go func() {
 		defer s.mu.Unlock()
-		s.logger.Debug("Syncing clock")
 
-		start := time.Now()
-
-		// err := exec.Command("/usr/bin/chronyc", "-a", "makestep").Run()
-		err := exec.Command("/usr/bin/bash", "-c", "/usr/bin/date -s @$(/usr/sbin/phc_ctl /dev/ptp0 get | awk '{print $5}')").Run()
+		err := exec.Command("/usr/bin/chronyc", "-a", "makestep").Run()
 		if err != nil {
 			s.logger.Errorw("Failed to sync clock:",
 				"error", err,
 			)
 		} else {
-
-			end := time.Since(start)
-			s.logger.Debugw("Clock synced", "duration", end)
+			s.logger.Debugw("Clock synced")
 		}
 	}()
 }
 
 func (s *Service) Wait() {
 	s.logger.Debug("Waiting for clock sync lock")
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.mu.RLock()
+	s.mu.RUnlock()
 
 	s.logger.Debug("Clock sync lock passsed")
 }
