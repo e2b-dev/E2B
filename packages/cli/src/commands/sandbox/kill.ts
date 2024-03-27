3b416549000c501eb131d4505b86e98a432b874a
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-03-26 20:38:26 -0700

    Fix sandbox kill command

--- packages/cli/src/commands/sandbox/kill.ts
+++ packages/cli/src/commands/sandbox/kill.ts
@@ -4,10 +4,6 @@ import { ensureAPIKey } from 'src/api'
 import { asBold } from 'src/utils/format'
 import * as e2b from 'e2b'
 
-
-const killSandbox = e2b.withAPIKey(
-  e2b.api.path('/sandboxes/{sandboxID}').method('delete').create(),
-)
 export const killCommand = new commander.Command('kill')
   .description('kill sandbox')
   .argument('<sandboxID>', `kill the sandbox specified by ${asBold('<sandboxID>')}`)
@@ -21,7 +17,7 @@ export const killCommand = new commander.Command('kill')
         process.exit(1)
       }
 
-      await killSandbox(apiKey, { sandboxID })
+      await e2b.Sandbox.kill(sandboxID, apiKey)
 
       console.log(`Sandbox ${asBold(sandboxID)} has been killed`)
     } catch (err: any) {
