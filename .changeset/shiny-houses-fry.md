168e59faf6f7f139494497dbc3aad7e54cd24f72
Jakub Novák <jakub@e2b.dev>
2024-04-05 18:32:36 -0700

    Python dict race condition (#351)
    Fixes #340

---
+++ .changeset/shiny-houses-fry.md
@@ -0,0 +1,5 @@
+---
+"@e2b/python-sdk": patch
+---
+
+Fix race condition in python SDK
--- packages/python-sdk/e2b/sandbox/sandbox_rpc.py
+++ packages/python-sdk/e2b/sandbox/sandbox_rpc.py
@@ -186,7 +186,8 @@ class SandboxRpc(BaseModel):
 
         self._process_cleanup.clear()
 
-        for handler in self._waiting_for_replies.values():
+        # .copy() prevents a RuntimeError: dictionary changed size during iteration
+        for handler in self._waiting_for_replies.copy().values():
             handler.cancel()
             del handler
 
