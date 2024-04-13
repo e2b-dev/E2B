c8b70fc5aa43f9194715ad02f818f6d5873e635e
Jakub Novak <jakub@e2b.dev>
2024-04-12 22:07:47 -0700

    Apply change from staging

--- packages/orchestrator/internal/sandbox/mock_instance.go
+++ packages/orchestrator/internal/sandbox/mock_instance.go
@@ -12,7 +12,7 @@ import (
 )
 
 func MockInstance(envID, instanceID string, dns *DNS, keepAlive time.Duration) {
-	ctx, cancel := context.WithTimeout(context.WithValue(context.Background(), telemetry.DebugID, instanceID), time.Minute*3)
+	ctx, cancel := context.WithTimeout(context.WithValue(context.Background(), telemetry.DebugID, instanceID), time.Second*30)
 	defer cancel()
 
 	tracer := otel.Tracer(fmt.Sprintf("instance-%s", instanceID))
