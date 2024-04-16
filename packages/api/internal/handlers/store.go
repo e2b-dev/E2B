992ed7a8224a8f721964b912e985ae3b01d55ef3
Jakub Novak <jakub@e2b.dev>
2024-04-16 12:39:32 -0700

    Fix

--- packages/api/internal/handlers/store.go
+++ packages/api/internal/handlers/store.go
@@ -23,7 +23,6 @@ import (
 	"github.com/e2b-dev/infra/packages/api/internal/nomad/cache/instance"
 	"github.com/e2b-dev/infra/packages/api/internal/orchestrator"
 	"github.com/e2b-dev/infra/packages/api/internal/template-manager"
-	"github.com/e2b-dev/infra/packages/api/internal/utils"
 	"github.com/e2b-dev/infra/packages/shared/pkg/db"
 	"github.com/e2b-dev/infra/packages/shared/pkg/env"
 	"github.com/e2b-dev/infra/packages/shared/pkg/logging"
