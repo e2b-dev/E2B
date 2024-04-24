f0f53677d92c88ca42a861336e3b816e6cbebef5
Jakub Novak <jakub@e2b.dev>
2024-04-24 09:26:37 -0700

    Fix local debug option in python SDK

--- packages/python-sdk/e2b/constants.py
+++ packages/python-sdk/e2b/constants.py
@@ -4,13 +4,11 @@ SANDBOX_REFRESH_PERIOD = 5  # seconds
 
 TIMEOUT = 60
 
-
 SECURE = os.getenv("E2B_SECURE", "TRUE").upper() == "TRUE"
 DEBUG = os.getenv("E2B_DEBUG") or False
 PROTOCOL = "https" if SECURE and not DEBUG else "http"
 
-
-DOMAIN = os.getenv("E2B_DOMAIN") or "e2b.dev"
+DOMAIN = os.getenv("E2B_DOMAIN") or (DEBUG and "localhost:3000") or "e2b.dev"
 
 ENVD_PORT = 49982
 WS_ROUTE = "/ws"
