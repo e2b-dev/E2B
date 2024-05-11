f097641832bdb9efe5aa62a725946f30528d9107
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-05-10 20:31:04 -0700

    Add readfile

--- packages/envd/internal/services/filesystem/filesystem.go
+++ packages/envd/internal/services/filesystem/filesystem.go
@@ -1,8 +1,11 @@
 package filesystem
 
 import (
-	"github.com/e2b-dev/infra/packages/envd/internal/services/spec"
+	"io"
+	"log"
+	"os"
 
+	"github.com/e2b-dev/infra/packages/envd/internal/services/spec"
 	"github.com/gogo/status"
 	"google.golang.org/grpc/codes"
 )
@@ -16,5 +19,44 @@ func New() *Service {
 }
 
 func (s *Service) ReadFile(req *spec.ReadFileRequest, stream spec.Filesystem_ReadFileServer) error {
-	return status.Errorf(codes.Unimplemented, "method ReadFile not implemented")
+	path := req.GetPath()
+	log.Printf("Received request for path: %s", path)
+
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return status.Errorf(codes.NotFound, "file not found")
+	}
+
+	if fileInfo.IsDir() {
+		return status.Errorf(codes.InvalidArgument, "file is a directory")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return status.Errorf(codes.NotFound, "file cannot be opened")
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 2*1024*1024)
+
+	for {
+		bytesread, err := file.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				return status.Errorf(codes.Internal, "error reading file")
+			}
+
+			break
+		}
+
+		err = stream.Send(&spec.ReadFileResponse{
+			Data: buffer[:bytesread],
+		})
+		if err != nil {
+			return status.Errorf(codes.Internal, "error sending line")
+		}
+	}
+
+	return nil
+	// return status.Errorf(codes.Unimplemented, "method ReadFile not implemented")
 }
