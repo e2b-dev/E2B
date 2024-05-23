763ba25aeeaf29c5c6d4ebe39a0ddaaa7b8a2a3d
ErikKaumk <erik.kaum@gmail.com>
2024-05-23 13:38:51 +0300

    navigation in query param

---
+++ apps/docs/src/components/Dashboard/AccountSelector.tsx
@@ -0,0 +1,45 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { ChevronRight, PlusCircle } from 'lucide-react'
+
+export const AccountSelector = ({ user }) => { 
+
+  const teams = user?.teams.map((team: any) => ({
+    id: team.id,
+    name: team.name,
+    default: team.is_default,
+  }))
+
+  const defaultTeam = teams?.find((teams: any) => teams.default)
+
+  return(
+    <DropdownMenu>
+    <DropdownMenuTrigger
+      className='dropdown-trigger group outline-none flex w-full items-center justify-between rounded-lg p-2 mb-6 hover:bg-zinc-800 hover:cursor-pointer'
+    >
+      <div className='flex items-start flex-col'>
+        <h3 className='font-bold'>{defaultTeam.name}</h3>
+        {/* <p className='text-sm'>Team account</p> */}
+      </div> 
+      <ChevronRight className='transform transition-transform duration-300 group-hover:rotate-90' />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className='flex flex-col w-48 bg-zinc-900 border border-white/30'>
+      {teams?.map((team: any) => (
+        <DropdownMenuItem key={team.id}>
+          {team.name}
+        </DropdownMenuItem>
+      ))}
+      <DropdownMenuSeparator />
+      <DropdownMenuItem className='flex items-center space-x-1'>
+        <PlusCircle width={15} height={15} />
+        <p>Create Team</p>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
