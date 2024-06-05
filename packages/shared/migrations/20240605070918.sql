de5b11885ca3a2de3566b613bfacaa6615b33fe0
Jakub Novak <jakub@e2b.dev>
2024-06-05 16:22:20 +0200

    Add defaults for team api key and access token

--- packages/shared/migrations/20240605070918.sql
+++ packages/shared/migrations/20240605070918.sql
@@ -14,6 +14,38 @@ BEGIN
 END
 $extra_for_post_user_signup$ SECURITY DEFINER SET search_path = public;
 
+CREATE OR REPLACE FUNCTION public.generate_team_api_key()
+    RETURNS string
+    LANGUAGE plpgsql
+AS $generate_team_api_key$
+DECLARE
+    team_api_key_prefix TEXT := 'e2b_';
+    generated_key TEXT;
+BEGIN
+    -- Generate a random 20 byte string and encode it as hex, so it's 40 characters
+    generated_key := encode(extensions.gen_random_bytes(20), 'hex');
+    RETURN team_api_key_prefix || generated_key;
+END
+$generate_team_api_key$ SECURITY DEFINER SET search_path = public;
+
+ALTER TABLE public.team_api_keys ALTER COLUMN api_key SET DEFAULT public.generate_team_api_key();
+
+CREATE OR REPLACE FUNCTION public.generate_access_token()
+    RETURNS string
+    LANGUAGE plpgsql
+AS $extra_for_post_user_signup$
+DECLARE
+    access_token_prefix TEXT := 'sk_e2b_';
+    generated_token TEXT;
+BEGIN
+    -- Generate a random 20 byte string and encode it as hex, so it's 40 characters
+    generated_token := encode(extensions.gen_random_bytes(20), 'hex');
+    RETURN access_token_prefix || generated_token;
+END
+$extra_for_post_user_signup$ SECURITY DEFINER SET search_path = public;
+
+ALTER TABLE public.access_tokens ALTER COLUMN access_token SET DEFAULT public.generate_access_token();
+
 ALTER FUNCTION public.extra_for_post_user_signup() OWNER TO trigger_user;
 
 CREATE OR REPLACE FUNCTION public.post_user_signup()
@@ -22,10 +54,6 @@ CREATE OR REPLACE FUNCTION public.post_user_signup()
 AS $post_user_signup$
 DECLARE
     team_id                 uuid;
-    team_api_key_prefix TEXT := 'e2b_';
-    generated_key TEXT;
-    access_token_prefix TEXT := 'sk_e2b_';
-    generated_token TEXT;
 BEGIN
     RAISE NOTICE 'Creating default team for user %', NEW.id;
     INSERT INTO public.teams(name, is_default, tier, email) VALUES (NEW.email, true, 'base_v1', NEW.email) RETURNING id INTO team_id;
@@ -33,14 +61,11 @@ BEGIN
     RAISE NOTICE 'Created default team for user % and team %', NEW.id, team_id;
 
     -- Generate a random 20 byte string and encode it as hex, so it's 40 characters
-    generated_key := encode(extensions.gen_random_bytes(20), 'hex');
-    INSERT INTO public.team_api_keys (team_id, api_key)
-    VALUES (team_id, team_api_key_prefix || generated_key);
+    INSERT INTO public.team_api_keys (team_id)
+    VALUES (team_id);
 
-    -- Generate a random 20 byte string and encode it as hex, so it's 40 characters
-    generated_token := encode(extensions.gen_random_bytes(20), 'hex');
-    INSERT INTO public.access_tokens (user_id, access_token)
-    VALUES (NEW.id, access_token_prefix || generated_token);
+    INSERT INTO public.access_tokens (user_id)
+    VALUES (NEW.id);
 
     PERFORM public.extra_for_post_user_signup(NEW.id, team_id);
 
