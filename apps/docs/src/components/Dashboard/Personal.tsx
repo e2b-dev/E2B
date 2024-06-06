4df10be2101c5185e7595b4998c27df50385a24e
ErikKaumk <erik.kaum@gmail.com>
2024-06-06 10:30:26 +0300

    update progress

--- apps/docs/src/components/Dashboard/Personal.tsx
+++ apps/docs/src/components/Dashboard/Personal.tsx
@@ -24,6 +24,15 @@ export const PersonalContent = ({user}: {user: User}) => {
     })
   }
 
+  const resetPassword = async() => {
+    const supabase = createPagesBrowserClient()
+    await supabase.auth.resetPasswordForEmail('hello@example.com', {
+      redirectTo: 'http://example.com/account/update-password',
+    })
+  }
+
+  console.log(user.app_metadata.provider)
+
   return(
   <div className="flex flex-col w-full h-full">
     <div className="flex flex-col h-full">
@@ -42,18 +51,22 @@ export const PersonalContent = ({user}: {user: User}) => {
         />
         <Button variant='outline'>Save changes</Button>
       </div>
+      
+      {user.app_metadata.provider === 'email' && (
+        <>
+        <h1 className="font-bold pb-4 text-xl">
+            Reset password
+          </h1>
+          <span className='text-neutral-300 pb-4'>
+            Resetting the will send an email with a link to reset the password. <br/>
+            The action cannot be undone.
+          </span>
+          <div className='flex items-center space-x-2 pb-10'>
+            <Button variant='desctructive' onClick={resetPassword}>Reset password</Button>
+          </div>
+        </>
+      )}
 
-      <span className='text-neutral-300 pb-2'>Change password:</span>
-      <div className='flex items-center space-x-2 pb-10'>
-        <input
-          type="text"
-          className="w-1/2 border border-white/10 text-sm focus:outline-none outline-none rounded-md p-2"
-          placeholder={user.email}
-          value={user.email}
-          onSubmit={updateUserEmail}
-        />
-        <Button variant='outline'>Change password</Button>
-      </div>
       </div>
     </div>
   )
