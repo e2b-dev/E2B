f1e1b065cfb417add235ee62118fa87f2244cac3
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-06-06 14:33:20 +0200

    Fix relative path resolution with user

--- packages/envd/internal/services/permissions/path.go
+++ packages/envd/internal/services/permissions/path.go
@@ -24,12 +24,19 @@ func expand(path, homedir string) (string, error) {
 }
 
 func ExpandAndResolve(path string, user *user.User) (string, error) {
-	expanded, err := expand(path, user.HomeDir)
+	path, err := expand(path, user.HomeDir)
 	if err != nil {
 		return "", fmt.Errorf("failed to expand path '%s' for user '%s': %w", path, user.Username, err)
 	}
 
-	abs, err := filepath.Abs(expanded)
+	if filepath.IsAbs(path) {
+		return path, nil
+	}
+
+	// The filepath.Abs can correctly resolve paths like /home/user/../file
+	path = filepath.Join(user.HomeDir, path)
+
+	abs, err := filepath.Abs(path)
 	if err != nil {
 		return "", fmt.Errorf("failed to resolve path '%s' for user '%s': %w", path, user.Username, err)
 	}
