1549e9c36d3ba3921f2d1ac49c782731e12af534
ErikKaumk <erik.kaum@gmail.com>
2024-06-23 17:25:08 +0200

    charts

--- apps/docs/src/components/Dashboard/Chart.tsx
+++ apps/docs/src/components/Dashboard/Chart.tsx
@@ -1,31 +1,42 @@
-import { ResponsiveBar } from '@nivo/bar'
+import { ResponsiveLine } from '@nivo/line'
 
-const generateTicks = (minValue: number, maxValue: number, numTicks: number) => {
+const generateTicks = (minValue: number, maxValue: number, numTicks: number): number[] => {
   const range = maxValue - minValue
   const step = range / (numTicks - 1)
-  return Array.from({ length: numTicks }, (_, i) => (minValue + i * step).toFixed(2))
+
+  // Determine magnitude of the step to round it to a sensible value
+  const magnitude = Math.pow(10, Math.floor(Math.log10(step)))
+  const roundedStep = Math.ceil(step / magnitude) * magnitude
+
+  // Round minValue down to the nearest roundedStep
+  const start = Math.floor(minValue / roundedStep) * roundedStep
+
+  const ticks: number[] = []
+  for (let i = 0; i < numTicks; i++) {
+    ticks.push(start + i * roundedStep)
+  }
+
+  if (ticks[ticks.length - 1] < maxValue) {
+    ticks.push(Math.ceil(maxValue / roundedStep) * roundedStep)
+  }
+
+  return ticks.map((tick) => parseFloat(tick.toFixed(2)))
 }
 
-// If you wonder how to style this chart, I highly recommend this interactive tool: https://nivo.rocks/bar/
-export default function BarChart({ usages, ...props }) {
+// If you wonder how to style this chart, I highly recommend this interactive tool: https://nivo.rocks/line/
+export default function LineChart({ series, ...props }) {
+  
+  const allValues = series.flatMap((s: any) => s.data.map((d: any) => d.y))
+  const maxValue = Math.max(...allValues) // Add a 10% buffer to the maximum value
 
-  const allValues = usages.data.flatMap((item: any) =>
-    Object.values(item).filter((value) => typeof value === 'number')
-  )
-  let maxValue = Math.max(...allValues)
-  maxValue += maxValue * 0.1 // add a 10% buffer
-
-  const ticks = generateTicks(0, maxValue, 5)
+  const ticks = generateTicks(0, maxValue, 6)
 
   return (
     <div {...props}>
-      <ResponsiveBar
-        data={usages.data}
-        keys={usages.keys}
-        indexBy="name"
-        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-        padding={0.2}
-        colors={['#FFAF78']}
+      <ResponsiveLine
+        data={series}
+        margin={{ top: 60, right: 30, bottom: 40, left: 50 }}
+        colors={['#FF9F33']}
         axisBottom={{
           tickSize: 0,
           tickPadding: 16,
@@ -33,7 +44,9 @@ export default function BarChart({ usages, ...props }) {
         axisLeft={{
           tickSize: 0,
           tickPadding: 16,
+          tickValues: ticks,
         }}
+        gridXValues={[]}
         gridYValues={ticks}
         theme={{
           axis: {
@@ -41,25 +54,29 @@ export default function BarChart({ usages, ...props }) {
               text: {
                 fill: '#f3f4f6',
                 fontSize: 12,
+                fontFamily: 'monospace',
               },
             },
           },
           grid: {
             line: {
-              stroke: '#f3f4f6',
+              stroke: '#525252',
             },
           },
         }}
         isInteractive={false}
-        borderRadius={4}
-        tooltipLabel={({ id }) => `${id}`}
-        label={(d) => `${d.value!.toFixed(4)}`}
-        enableLabel={true}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        maxValue={maxValue}
-        role="application"
-        ariaLabel={`A bar chart showing ${props.type}-hours per month broken down by template`}
+        enableArea={true}
+        defs={[
+          {
+            id: 'gradientA',
+            type: 'linearGradient',
+            colors: [
+              { offset: 0, color: '#FF9F33' },
+              { offset: 100, color: '#FF9F33', opacity: 0 },
+            ],
+          },
+        ]}
+        fill={[{ match: '*', id: 'gradientA' }]}
       />
     </div>
   )
