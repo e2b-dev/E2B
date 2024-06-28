9360f9d43ac39eaf88225e985ff9f255bae21d2a
ErikKaumk <erik.kaum@gmail.com>
2024-06-28 22:09:36 +0200

    revert miss

--- apps/docs/src/app/(main)/api/layout.tsx
+++ apps/docs/src/app/(main)/api/layout.tsx
@@ -16,3 +16,14 @@ export const metadata: Metadata = {
   },
 }
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
