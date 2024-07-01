b70be08e2fa2141814d0eb8900611e982a3be7a6
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-07-01 13:59:26 +0200

    tar docker context to support deep directory structure

--- packages/cli/src/utils/docker.ts
+++ packages/cli/src/utils/docker.ts
@@ -1,10 +1,15 @@
 import fs from 'fs/promises'
+import path from 'path'
 import ignore from '@balena/dockerignore'
+import { pack } from 'tar-fs'
 
-export async function prepareDockerContext (root: string): Promise<string[]> {
+export async function prepareDockerContext (root: string): Promise<NodeJS.ReadableStream> {
   const dockerignore = await fs.readFile(`${root}/.dockerignore`, 'utf-8').catch(() => '')
   const ig = ignore().add(dockerignore.split('\n'))
 
-  const files = await fs.readdir(root)
-  return files.filter(file => !ig.ignores(file))
+  return pack(root, {
+    ignore: (name: string) => {
+      return ig.ignores(path.relative(root, name))
+    }
+  })
 }
