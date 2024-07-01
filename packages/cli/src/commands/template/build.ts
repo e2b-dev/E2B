9c10e92b32b05d6a1b8f55c8fdc87966fb835a0e
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-07-01 13:33:42 +0200

    resolved issues with docker build

--- packages/cli/src/commands/template/build.ts
+++ packages/cli/src/commands/template/build.ts
@@ -20,6 +20,7 @@ import {
   asTypescript,
   withDelimiter,
 } from 'src/utils/format'
+import { prepareDockerContext } from 'src/utils/docker'
 import { configOption, pathOption } from 'src/options'
 import {
   defaultDockerfileName,
@@ -282,11 +283,12 @@ export const buildCommand = new commander.Command('build')
         const dockerImageTag = `docker.${e2b.SANDBOX_DOMAIN}/e2b/custom-envs/${templateID}:${template.buildID}`
         const buildStream = await docker.buildImage({
           context: root,
-          src: [dockerfileRelativePath],
+          src: await prepareDockerContext(root)
         }, {
           t: dockerImageTag,
           platform: 'linux/amd64',
           buildargs: dockerBuildArgs,
+          dockerfile: dockerfileRelativePath,
         })
 
         for await (const chunk of buildStream) {
--- packages/cli/src/utils/format.ts
+++ packages/cli/src/utils/format.ts
@@ -127,8 +127,8 @@ export function withDelimiter(content: string, title: string, isLast?: boolean) 
 export function prettyPrintDockerLogs(c: string, key: string = 'stream') {
   c.split('\n').forEach((chunk) => {
     try {
-      const line = JSON.parse(chunk)
-      if (line[key]) {
+      const line = JSON.parse(chunk)[key]
+      if (line) {
         process.stdout.write(asBuildLogs(line.trim()) + '\n')
       }
     } catch (e) {
