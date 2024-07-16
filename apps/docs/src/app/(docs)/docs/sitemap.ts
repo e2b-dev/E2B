9c3ce03503427a08cdb4c7b76365d519996c719e
Jakub Novak <jakub@e2b.dev>
2024-07-16 16:35:47 +0200

    Fix filter for sitemap

--- apps/docs/src/app/(docs)/docs/sitemap.ts
+++ apps/docs/src/app/(docs)/docs/sitemap.ts
@@ -5,6 +5,6 @@ import { getPageForSitemap } from '@/utils/sitemap'
 export default function sitemap(): MetadataRoute.Sitemap {
   const appDirectory = path.join(process.cwd(), 'src', 'app', '(docs)', 'docs')
   return getPageForSitemap(appDirectory, 'https://e2b.dev/docs/', 0.5).filter(
-    (page) => page.url.startsWith('https://e2b.dev/docs/api/'),
+    (page) => !page.url.startsWith('https://e2b.dev/docs/api/'),
   )
 }
