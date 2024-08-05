921696231b19fc11083e45b86511add762e0e0cb
Jakub Novak <jakub@e2b.dev>
2024-08-05 13:39:13 +0200

    Fix layout

--- apps/web/src/app/(auth)/auth/layout.tsx
+++ apps/web/src/app/(auth)/auth/layout.tsx
@@ -1,7 +1,10 @@
+import {Footer} from '@/components/Footer'
+
 export default async function Layout({ children }) {
   return (
     <div className="pt-12">
       {children}
+      <Footer />
     </div>
   )
 }
