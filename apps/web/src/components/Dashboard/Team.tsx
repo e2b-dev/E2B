89984335f997549d29eab819672b74782d193e2e
Jakub Novak <jakub@e2b.dev>
2024-10-29 22:32:00 -0700

    Validate user ID with regex

--- apps/web/src/components/Dashboard/Team.tsx
+++ apps/web/src/components/Dashboard/Team.tsx
@@ -5,8 +5,25 @@ import { Button } from '../Button'
 import { E2BUser, Team } from '@/utils/useUser'
 import { toast } from '../ui/use-toast'
 import { Copy } from 'lucide-react'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '../ui/alert-dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../ui/table'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../ui/alert-dialog'
 import Spinner from '@/components/Spinner'
 
 interface TeamMember {
@@ -14,8 +31,23 @@ interface TeamMember {
   email: string
 }
 
+const uuidRegex = new RegExp(
+  '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+)
 
-export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { team: Team, user: E2BUser, teams: Team[], setTeams: (teams: Team[]) => void, setCurrentTeam: (team: Team) => void }) => {
+export const TeamContent = ({
+  team,
+  user,
+  teams,
+  setTeams,
+  setCurrentTeam,
+}: {
+  team: Team
+  user: E2BUser
+  teams: Team[]
+  setTeams: (teams: Team[]) => void
+  setCurrentTeam: (team: Team) => void
+}) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [currentMemberId, setCurrentMemberId] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
@@ -26,11 +58,14 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
 
   useEffect(() => {
     const getTeamMembers = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`, {
-        headers: {
-          'X-Team-API-Key': team.apiKeys[0],
-        },
-      })
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`,
+        {
+          headers: {
+            'X-Team-API-Key': team.apiKeys[0],
+          },
+        }
+      )
 
       if (!res.ok) {
         toast({
@@ -41,7 +76,9 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
         // TODO: Add sentry event here
         return
       }
-      const members = (await res.json()).filter((member: TeamMember) => member.id !== user.id)
+      const members = (await res.json()).filter(
+        (member: TeamMember) => member.id !== user.id
+      )
       setMembers(members)
       setIsLoading(false)
     }
@@ -60,14 +97,17 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
   }
 
   const deleteUserFromTeam = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`, {
-      method: 'DELETE',
-      headers: {
-        'X-User-Access-Token': user.accessToken,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({user_id: currentMemberId}),
-    })
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`,
+      {
+        method: 'DELETE',
+        headers: {
+          'X-User-Access-Token': user.accessToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user_id: currentMemberId }),
+      }
+    )
 
     if (!res.ok) {
       toast({
@@ -78,19 +118,22 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
       // TODO: Add sentry event here
       return
     }
-    setMembers(members.filter(member => member.id !== currentMemberId))
+    setMembers(members.filter((member) => member.id !== currentMemberId))
     closeDialog()
   }
 
   const changeTeamName = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}`, {
-      headers: {
-        'X-Team-API-Key': team.apiKeys[0],
-        'Content-Type': 'application/json',
-      },
-      method: 'PATCH',
-      body: JSON.stringify({name: teamName}),
-    })
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}`,
+      {
+        headers: {
+          'X-Team-API-Key': team.apiKeys[0],
+          'Content-Type': 'application/json',
+        },
+        method: 'PATCH',
+        body: JSON.stringify({ name: teamName }),
+      }
+    )
 
     if (!res.ok) {
       toast({
@@ -101,24 +144,36 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
       return
     }
 
-
     toast({
       title: 'Team name changed',
     })
     setTeamName(teamName)
-    setTeams(teams.map(t => t.id === team.id ? { ...t, name: teamName } : t))
+    setTeams(
+      teams.map((t) => (t.id === team.id ? { ...t, name: teamName } : t))
+    )
     setCurrentTeam({ ...team, name: teamName })
   }
 
   const addUserToTeam = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`, {
-      headers: {
-        'X-User-Access-Token': user.accessToken,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({user_id: userToAdd}),
-    })
+    if (!uuidRegex.test(userToAdd)) {
+      toast({
+        title: 'Invalid user ID',
+        description: 'The user ID must be a valid UUID',
+      })
+      return
+    }
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BILLING_API_URL}/teams/${team.id}/users`,
+      {
+        headers: {
+          'X-User-Access-Token': user.accessToken,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ user_id: userToAdd.trim() }),
+      }
+    )
 
     if (!res.ok) {
       toast({
@@ -137,9 +192,9 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
   }
 
   return (
-    <div className='flex flex-col justify-center pb-10'>
+    <div className="flex flex-col justify-center pb-10">
       <h3 className="text-lg font-medium pb-4">Team name</h3>
-      <div className='flex items-center space-x-2 pb-4'>
+      <div className="flex items-center space-x-2 pb-4">
         <input
           type="text"
           className="w-1/2 md:w-1/3 border border-white/10 text-sm focus:outline-none outline-none rounded-md p-2"
@@ -150,10 +205,12 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
             setTeamName(e.target.value)
           }}
         />
-        <Button variant='outline' onClick={() => changeTeamName()}>Save changes</Button>
+        <Button variant="outline" onClick={() => changeTeamName()}>
+          Save changes
+        </Button>
       </div>
       <h3 className="text-lg font-medium pb-4">Team ID</h3>
-      <div className='flex items-center space-x-2 pb-4'>
+      <div className="flex items-center space-x-2 pb-4">
         <input
           readOnly
           type="text"
@@ -163,7 +220,7 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
       </div>
 
       <span
-        className='flex pb-10 w-fit text-sm text-orange-500 hover:cursor-pointer hover:text-orange-500/30 space-x-2 items-center'
+        className="flex pb-10 w-fit text-sm text-orange-500 hover:cursor-pointer hover:text-orange-500/30 space-x-2 items-center"
         onClick={() => {
           navigator.clipboard.writeText(team.id)
           toast({
@@ -172,11 +229,11 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
         }}
       >
         <p>Copy your team ID</p>
-        <Copy className='h-4 w-4' />
+        <Copy className="h-4 w-4" />
       </span>
 
       <h2 className="text-xl font-bold pb-4">Add members to your team</h2>
-      <div className='flex items-center space-x-2 pb-4'>
+      <div className="flex items-center space-x-2 pb-4">
         <input
           type="text"
           className="w-1/2 md:w-1/3 border border-white/10 text-sm focus:outline-none outline-none rounded-md p-2"
@@ -187,11 +244,13 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
             setUserToAdd(e.target.value)
           }}
         />
-        <Button variant='outline' onClick={() => addUserToTeam()}>Add user</Button>
+        <Button variant="outline" onClick={() => addUserToTeam()}>
+          Add user
+        </Button>
       </div>
 
       <h3 className="text-lg font-medium pb-4">Your user ID</h3>
-      <div className='flex items-center space-x-2 pb-4'>
+      <div className="flex items-center space-x-2 pb-4">
         <input
           readOnly
           type="text"
@@ -201,7 +260,7 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
       </div>
 
       <span
-        className='flex pb-10 w-fit text-sm text-orange-500 hover:cursor-pointer hover:text-orange-500/30 space-x-2 items-center'
+        className="flex pb-10 w-fit text-sm text-orange-500 hover:cursor-pointer hover:text-orange-500/30 space-x-2 items-center"
         onClick={() => {
           navigator.clipboard.writeText(user.id)
           toast({
@@ -210,35 +269,42 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
         }}
       >
         <p>Copy your user ID</p>
-        <Copy className='h-4 w-4' />
+        <Copy className="h-4 w-4" />
       </span>
 
       <h2 className="text-xl font-bold pb-4">Team members</h2>
-      {isLoading ? (<div className="flex items-center w-full pl-4 p-2">
-        <Spinner size="24px" />
-      </div>) : (
+      {isLoading ? (
+        <div className="flex items-center w-full pl-4 p-2">
+          <Spinner size="24px" />
+        </div>
+      ) : (
         <Table>
           <TableHeader>
-            <TableRow className='hover:bg-inherit dark:hover:bg-inherit border-b border-white/5'>
+            <TableRow className="hover:bg-inherit dark:hover:bg-inherit border-b border-white/5">
               <TableHead>Email</TableHead>
               <TableHead></TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {members.length === 0 ? (
-              <TableRow className='border-b border-white/5'>
-                <TableCell colSpan={2} className='text-center'>
+              <TableRow className="border-b border-white/5">
+                <TableCell colSpan={2} className="text-center">
                   No members found
                 </TableCell>
               </TableRow>
             ) : (
               members.map((user) => (
                 <TableRow
-                  className='hover:bg-orange-300/10 dark:hover:bg-orange-300/10 border-b border-white/5'
-                  key={user.id}>
+                  className="hover:bg-orange-300/10 dark:hover:bg-orange-300/10 border-b border-white/5"
+                  key={user.id}
+                >
                   <TableCell>{user.email}</TableCell>
                   <TableCell>
-                    <Button className='text-sm' variant='desctructive' onClick={() => openDialog(user.id)}>
+                    <Button
+                      className="text-sm"
+                      variant="desctructive"
+                      onClick={() => openDialog(user.id)}
+                    >
                       Remove team member
                     </Button>
                   </TableCell>
@@ -246,29 +312,41 @@ export const TeamContent = ({ team, user, teams, setTeams, setCurrentTeam }: { t
               ))
             )}
           </TableBody>
-        </Table>)}
-
+        </Table>
+      )}
 
       <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <AlertDialogTrigger asChild>
-          <Button variant="outline" style={{ display: 'none' }}>Show Dialog</Button>
+          <Button variant="outline" style={{ display: 'none' }}>
+            Show Dialog
+          </Button>
         </AlertDialogTrigger>
         <AlertDialogContent className="bg-inherit text-white border-black">
           <AlertDialogHeader>
-            <AlertDialogTitle>You are about to delete a member from the team</AlertDialogTitle>
-            <AlertDialogDescription className='text-white/90'>
+            <AlertDialogTitle>
+              You are about to delete a member from the team
+            </AlertDialogTitle>
+            <AlertDialogDescription className="text-white/90">
               This action cannot be undone. This will permanently delete the
               member from the team.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogCancel className='border-white/10' onClick={closeDialog}>Cancel</AlertDialogCancel>
-            <AlertDialogAction className='bg-red-500 text-white hover:bg-red-600'
-              onClick={() => deleteUserFromTeam()}>Continue</AlertDialogAction>
+            <AlertDialogCancel
+              className="border-white/10"
+              onClick={closeDialog}
+            >
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-500 text-white hover:bg-red-600"
+              onClick={() => deleteUserFromTeam()}
+            >
+              Continue
+            </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-
     </div>
   )
 }
