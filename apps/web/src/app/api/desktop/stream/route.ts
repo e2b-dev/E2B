15c52349ac69a2d903150ac1048dda5d33c1575f
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-11-22 23:47:29 -0800

    Add route for creating streams

---
+++ apps/web/src/app/api/desktop/stream/route.ts
@@ -0,0 +1,46 @@
+import Mux from '@mux/mux-node'
+import { NextResponse } from 'next/server'
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+
+const supabase = createClient(supabaseUrl, supabaseServiceKey)
+
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET
+})
+
+export async function POST(request: Request) {
+  // TODO: UseG apiKey
+  // const apiKey = request.headers.get('X-API-Key')
+  const { sandboxId } = await request.json()
+  console.log('sandboxId', sandboxId)
+
+  // if (!apiKey) {
+  //   return NextResponse.json({ error: 'Missing X-API-Key' }, { status: 400 })
+  // }
+
+  if (!sandboxId) {
+    return NextResponse.json({ error: 'Missing sandboxId' }, { status: 400 })
+  }
+
+  const liveStream = await mux.video.liveStreams.create({
+    latency_mode: 'low',
+    reconnect_window: 60,
+    playback_policy: ['public'],
+    new_asset_settings: { playback_policy: ['public'] },
+  })
+
+  if (!liveStream.playback_ids?.[0]?.id) {
+    return NextResponse.json({ error: 'Failed to create live stream' }, { status: 500 })
+  }
+
+  await supabase
+    .from('sandbox_streams')
+    .insert([{ sandbox_id: sandboxId, playback_id: liveStream.playback_ids[0].id }])
+
+  return NextResponse.json({ streamKey: liveStream.stream_key, playbackId: liveStream.playback_ids[0].id }, { status: 201 })
+}
