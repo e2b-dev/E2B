4437a779eb9a3c757d9e97878b3b1d3f08334be1
0div <tgft73@gmail.com>
2024-12-16 11:02:50 -0800

    add deeper snapshot tests around memory, disk and long running processes

--- packages/js-sdk/tests/sandbox/snapshot.test.ts
+++ packages/js-sdk/tests/sandbox/snapshot.test.ts
@@ -14,3 +14,85 @@ sandboxTest.skipIf(isDebug)('pause and resume a sandbox', async ({ sandbox }) =>
 
   assert.isTrue(await sandbox.isRunning())
 })
+
+sandboxTest.skipIf(isDebug)('pause and resume a sandbox with env vars', async ({ template }) => {
+  // Environment variables of a process exist at runtime, and are not stored in some file or so.
+  // They are stored in the process's own memory
+  const sandbox = await Sandbox.create(template, { envs: { TEST_VAR: 'sfisback' } })
+
+  try {
+    const cmd = await sandbox.commands.run('echo "$TEST_VAR"')
+
+    assert.equal(cmd.exitCode, 0)
+    assert.equal(cmd.stdout.trim(), 'sfisback')
+  } catch {
+    sandbox.kill()
+  } 
+
+  await sandbox.pause()
+  assert.isFalse(await sandbox.isRunning())
+
+  await Sandbox.resume(sandbox.sandboxId)
+  assert.isTrue(await sandbox.isRunning())
+
+  try {
+    const cmd = await sandbox.commands.run('echo "$TEST_VAR"')
+
+    assert.equal(cmd.exitCode, 0)
+    assert.equal(cmd.stdout.trim(), 'sfisback')
+  } finally {
+    await sandbox.kill()
+  }
+})
+
+sandboxTest.skipIf(isDebug)('pause and resume a sandbox with file', async ({ sandbox }) => {
+  const filename = 'test_snapshot.txt'
+  const content = 'This is a snapshot test file.'
+
+  const info = await sandbox.files.write(filename, content)
+  assert.equal(info.name, filename)
+  assert.equal(info.type, 'file')
+  assert.equal(info.path, `/home/user/${filename}`)
+
+  const exists = await sandbox.files.exists(filename)
+  assert.isTrue(exists)
+  const readContent = await sandbox.files.read(filename)
+  assert.equal(readContent, content)
+
+
+  await sandbox.pause()
+  assert.isFalse(await sandbox.isRunning())
+
+  await Sandbox.resume(sandbox.sandboxId)
+  assert.isTrue(await sandbox.isRunning())
+
+  const exists2 = await sandbox.files.exists(filename)
+  assert.isTrue(exists2)
+  const readContent2 = await sandbox.files.read(filename)
+  assert.equal(readContent2, content)
+})
+
+sandboxTest.skipIf(isDebug)('pause and resume a sandbox long running process', async ({ sandbox }) => {
+  const filename = 'test_long_running.txt'
+
+  sandbox.commands.run(`sleep 2 && echo "done" > /home/user/${filename}`)
+
+  // the file should not exist before 2 seconds have elapsed
+  const exists = await sandbox.files.exists(filename)
+  assert.isFalse(exists)
+
+  await sandbox.pause()
+  assert.isFalse(await sandbox.isRunning())
+
+  await Sandbox.resume(sandbox.sandboxId)
+  assert.isTrue(await sandbox.isRunning())
+
+  // the file should be created after more than 2 seconds have elapsed
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  const exists2 = await sandbox.files.exists(filename)
+  assert.isTrue(exists2)
+  const readContent2 = await sandbox.files.read(filename)
+  assert.equal(readContent2.trim(), 'done')
+})
+ 
