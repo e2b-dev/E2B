a9c283e6d30d2000ed7c9655f01eff66ff9dfb9f
0div <tgft73@gmail.com>
2025-01-16 19:29:13 -0800

    edits based on feedback

--- packages/js-sdk/tests/sandbox/host.test.ts
+++ packages/js-sdk/tests/sandbox/host.test.ts
@@ -1,7 +1,6 @@
 import { assert } from 'vitest'
 
-import Sandbox from '../../src/index.js'
-import { isDebug, sandboxTest, template, wait } from '../setup.js'
+import { isDebug, sandboxTest, wait } from '../setup.js'
 
 sandboxTest.skipIf(isDebug)(
   'ping server in running sandbox',
@@ -28,34 +27,35 @@ sandboxTest.skipIf(isDebug)(
   }
 )
 
-sandboxTest.skipIf(isDebug)('ping server in non-running sandbox', async () => {
-  const sbx = await Sandbox.create(template, { timeoutMs: 120_000 })
+sandboxTest.skipIf(isDebug)(
+  'ping server in non-running sandbox',
+  async ({ sandbox }) => {
+    const cmd = await sandbox.commands.run('python -m http.server 8000', {
+      background: true,
+    })
 
-  const cmd = await sbx.commands.run('python -m http.server 8000', {
-    background: true,
-  })
-
-  try {
-    await wait(20_000)
-
-    const host = sbx.getHost(8000)
-
-    const res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
-
-    assert.equal(res.status, 200)
-
-    await sbx.kill()
-
-    const res2 = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
-    assert.equal(res2.status, 502)
-
-    const text = await res2.text()
-    assert.equal(text, 'Sandbox does not exist.')
-  } finally {
     try {
-      await cmd.kill()
-    } catch (e) {
-      console.error(e)
+      await wait(20_000)
+
+      const host = sandbox.getHost(8000)
+
+      const res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
+
+      assert.equal(res.status, 200)
+
+      await sandbox.kill()
+
+      const res2 = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
+      assert.equal(res2.status, 502)
+
+      const text = await res2.text()
+      assert.equal(text, 'Sandbox does not exist.')
+    } finally {
+      try {
+        await cmd.kill()
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
-})
+)
