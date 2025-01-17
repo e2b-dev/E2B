81b5002a3e7b332bfc5215bc94de92540b40f9f3
Jakub Novak <jakub@e2b.dev>
2025-01-17 11:41:41 -0800

    Add tests to Python SDK

--- packages/python-sdk/tests/async/api_async/test_sbx_list.py
+++ packages/python-sdk/tests/async/api_async/test_sbx_list.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import pytest
 
 from e2b import AsyncSandbox
@@ -8,3 +11,12 @@ async def test_list_sandboxes(async_sandbox: AsyncSandbox):
     sandboxes = await AsyncSandbox.list()
     assert len(sandboxes) > 0
     assert async_sandbox.sandbox_id in [sbx.sandbox_id for sbx in sandboxes]
+
+
+@pytest.mark.skip_debug()
+async def test_list_sandboxes_with_filter(async_sandbox: AsyncSandbox):
+    unique_id = "".join(random.choices(string.ascii_letters, k=5))
+    await AsyncSandbox.create(metadata={"unique_id": unique_id})
+    sandboxes = await AsyncSandbox.list()
+    assert len(sandboxes) == 1
+    assert sandboxes[0].metadata["unique_id"] == unique_id
--- packages/python-sdk/tests/sync/api_sync/test_sbx_list.py
+++ packages/python-sdk/tests/sync/api_sync/test_sbx_list.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import pytest
 
 from e2b import Sandbox
@@ -8,3 +11,12 @@ def test_list_sandboxes(sandbox: Sandbox):
     sandboxes = Sandbox.list()
     assert len(sandboxes) > 0
     assert sandbox.sandbox_id in [sbx.sandbox_id for sbx in sandboxes]
+
+
+@pytest.mark.skip_debug()
+def test_list_sandboxes_with_filter(sandbox: Sandbox):
+    unique_id = "".join(random.choices(string.ascii_letters, k=5))
+    Sandbox(metadata={"unique_id": unique_id})
+    sandboxes = Sandbox.list()
+    assert len(sandboxes) == 1
+    assert sandboxes[0].metadata["unique_id"] == unique_id
