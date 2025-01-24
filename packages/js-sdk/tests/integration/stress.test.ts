3055d0e0242ea777e24bdf6c5cae752526490fcb
0div <tgft73@gmail.com>
2025-01-23 18:39:24 -0800

    add stress test for getMetrics

--- packages/js-sdk/tests/integration/stress.test.ts
+++ packages/js-sdk/tests/integration/stress.test.ts
@@ -1,6 +1,6 @@
 import { test } from 'vitest'
 
-import Sandbox from '../../src/index.js'
+import Sandbox, { components } from '../../src/index.js'
 import { isIntegrationTest, wait } from '../setup.js'
 
 const heavyArray = new ArrayBuffer(256 * 1024 * 1024) // 256 MiB = 256 * 1024 * 1024 bytes
@@ -10,7 +10,58 @@ for (let i = 0; i < view.length; i++) {
 }
 
 const integrationTestTemplate = 'integration-test-v1'
-const sanboxCount = 10
+const sanboxCount = 1_000
+const batchSize = 10
+
+test.skipIf(!isIntegrationTest)(
+  'create a bunch of sandboxes and get metrics',
+  async () => {
+    const promises: Array<Promise<Sandbox | void>> = []
+    for (let i = 0; i < sanboxCount; i++) {
+      promises.push(
+        Sandbox.create('base', { timeoutMs: 600 }).then(async (sbx) => {
+          if (await sbx.isRunning()) {
+            sbx.commands.run('yes', { background: true })
+          } else {
+            console.log('sandbox is not running', sbx.sandboxId)
+          }
+          return sbx
+        })
+      )
+    }
+    const sbxs = await Promise.all(promises)
+
+    await wait(10_000)
+
+    for (let i = 0; i < sbxs.length; i++) {
+      // Process metrics in groups of 10 sandboxes
+      if (i % batchSize === 0) {
+        const metricsPromises: Array<
+          Promise<components['schemas']['SandboxMetric'][]>
+        > = sbxs.slice(i, i + batchSize).map((sbx) => {
+          if (sbx) return sbx.getMetrics()
+          return Promise.resolve([])
+        })
+        const metricsResults = await Promise.all(metricsPromises)
+
+        console.log('metricsResults', metricsResults)
+        // Log metrics for each sandbox in the group
+        metricsResults.forEach((metrics, idx) => {
+          const sbx = sbxs[i + idx]
+          if (sbx) {
+            console.log('##### metrics START#####->', i + idx)
+            console.log('~~~', sbx.sandboxId)
+            console.log(metrics)
+            console.log('##### metrics END #####->', i + idx)
+          }
+        })
+      }
+      i++
+      continue
+    }
+  },
+  { timeout: 600_000 }
+)
 
 test.skipIf(!isIntegrationTest)(
   'stress test heavy file writes and reads',
