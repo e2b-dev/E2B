1f7f1edd8ab539c95eebc57d1a783785f6d64b6d
r33drichards <rwendt1337@gmail.com>
2025-03-03 16:45:58 -0800

    move conditional logic to only set variables

--- packages/cli/src/api.ts
+++ packages/cli/src/api.ts
@@ -8,48 +8,38 @@ export let apiKey = process.env.E2B_API_KEY
 export let accessToken = process.env.E2B_ACCESS_TOKEN
 
 const authErrorBox = (keyName: string) => {
+  let link
+  let msg
   switch (keyName) {
     case 'E2B_API_KEY':
-      return boxen.default(
-        `You must be logged in to use this command. Run ${asBold('e2b auth login')}.
-    
-    If you are seeing this message in CI/CD you may need to set the ${asBold(
-          `${keyName}`
-        )} environment variable.
-    Visit ${asPrimary(
-          'https://e2b.dev/dashboard?tab=keys'
-        )} to get the API key.`,
-        {
-          width: 70,
-          float: 'center',
-          padding: 0.5,
-          margin: 1,
-          borderStyle: 'round',
-          borderColor: 'redBright',
-        }
-      )
+      link = 'https://e2b.dev/dashboard?tab=keys'
+      msg = 'API key'
+
     case 'E2B_ACCESS_TOKEN':
-      return boxen.default(
-        `You must be logged in to use this command. Run ${asBold('e2b auth login')}.
-    
-    If you are seeing this message in CI/CD you may need to set the ${asBold(
-          `${keyName}`
-        )} environment variable.
-    Visit ${asPrimary(
-          'https://e2b.dev/dashboard?tab=personal'
-        )} to get the access token.`,
-        {
-          width: 70,
-          float: 'center',
-          padding: 0.5,
-          margin: 1,
-          borderStyle: 'round',
-          borderColor: 'redBright',
-        }
-      )
-    default:
-      throw new Error(`Unknown key name: ${keyName}`)
+      link = 'https://e2b.dev/dashboard?tab=personal'
+      msg = 'access token'
   }
+  // throwing error in default in switch statement results in unreachable code,
+  // so we need to check if link and msg are defined here instead
+  if (!link || !msg) {
+    throw new Error(`Unknown key name: ${keyName}`)
+  }
+  return boxen.default(
+    `You must be logged in to use this command. Run ${asBold('e2b auth login')}.
+
+If you are seeing this message in CI/CD you may need to set the ${asBold(
+      `${keyName}`
+    )} environment variable.
+Visit ${asPrimary(link)} to get the ${msg}.`,
+    {
+      width: 70,
+      float: 'center',
+      padding: 0.5,
+      margin: 1,
+      borderStyle: 'round',
+      borderColor: 'redBright',
+    }
+  )
 }
 
 export function ensureAPIKey() {
