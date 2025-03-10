436333ac6442f25b07376014763a6475d592f2d3
r33drichards <rwendt1337@gmail.com>
2025-03-10 13:08:37 -0700

    rm test file

--- packages/python-sdk/oof.py
+++
@@ -1,6 +0,0 @@
-from e2b import Sandbox
-
-with Sandbox(auto_pause=True) as sandbox:
-    sandbox.run_code("x = 1")
-    execution = sandbox.run_code("x+=1; x")
-    print(execution.text)  # outputs 2
