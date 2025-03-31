1f4373868da57aeb5f548121a499bb82c07caa93
0div <tgft73@gmail.com>
2025-03-31 13:14:18 -0700

    add missing api error handler to python sdk sanbox api

--- packages/python-sdk/e2b/sandbox_async/sandbox_api.py
+++ packages/python-sdk/e2b/sandbox_async/sandbox_api.py
@@ -24,6 +24,7 @@ from e2b.connection_config import ConnectionConfig
 from e2b.exceptions import TemplateException, NotFoundException
 from e2b.sandbox.sandbox_api import SandboxApiBase, SandboxInfo, SandboxMetrics
 from packaging.version import Version
+from e2b.api import handle_api_exception
 
 
 class SandboxApi(SandboxApiBase):
