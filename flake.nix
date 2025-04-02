f3bf394c15ad492a7f892d8adeba12d203848f26
r33drichards <rwendt1337@gmail.com>
2025-04-02 14:45:25 -0700

    line

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     (flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-
         devShells.default =
           pkgs.mkShell { buildInputs = [ pkgs.python39 pkgs.poetry ]; };
       }));
