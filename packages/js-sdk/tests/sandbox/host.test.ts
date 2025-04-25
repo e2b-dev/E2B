8bfd3c0eed0745ad5e7448f41756fb58529708b1
0div <98087403+0div@users.noreply.github.com>
2025-04-25 23:44:39 +0200

    Test new error handling indicating when the sandbox is not found  (#540)
    Added a test in `js-sdk` for https://github.com/e2b-dev/infra/pull/231
    
    ---------
    
    Co-authored-by: Jakub Novák <jakub@e2b.dev>

--- packages/js-sdk/tests/sandbox/host.test.ts
+++ packages/js-sdk/tests/sandbox/host.test.ts
@@ -2,30 +2,59 @@ import { assert } from 'vitest'
 
 import { isDebug, sandboxTest, wait } from '../setup.js'
 
-sandboxTest('ping server in sandbox', async ({ sandbox }) => {
-  const cmd = await sandbox.commands.run('python -m http.server 8000', { background: true })
+sandboxTest(
+  'ping server in running sandbox',
+  async ({ sandbox }) => {
+    const cmd = await sandbox.commands.run('python -m http.server 8000', {
+      background: true,
+    })
 
-  try {
-    await wait(1000)
-
-    const host = sandbox.getHost(8000)
-
-    let res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
-
-    for (let i = 0; i < 20; i++) {
-      if (res.status === 200) {
-        break
-      }
-
-      res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
-      await wait(500)
-    }
-    assert.equal(res.status, 200)
-  } finally {
     try {
-      await cmd.kill()
-    } catch (e) {
-      console.error(e)
+      await wait(1000)
+
+      const host = sandbox.getHost(8000)
+
+      let res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
+
+      for (let i = 0; i < 20; i++) {
+        if (res.status === 200) {
+          break
+        }
+
+        res = await fetch(`${isDebug ? 'http' : 'https'}://${host}`)
+        await wait(500)
+      }
+      assert.equal(res.status, 200)
+    } finally {
+      try {
+        await cmd.kill()
+      } catch (e) {
+        console.error(e)
+      }
     }
+  },
+  60_000
+)
+
+sandboxTest.skipIf(isDebug)(
+  'ping server in non-running sandbox',
+  async ({ sandbox }) => {
+    const host = sandbox.getHost(3000)
+    const url = `https://${host}`
+
+    await sandbox.kill()
+
+    const res = await fetch(url)
+    assert.equal(res.status, 502)
+
+    const text = await res.text()
+    const json = JSON.parse(text) as {
+      message: string
+      sandboxId: string
+      code: number
+    }
+    assert.equal(json.message, 'Sandbox not found')
+    assert.isTrue(sandbox.sandboxId.startsWith(json.sandboxId))
+    assert.equal(json.code, 0)
   }
-}, 60_000 )
+)
