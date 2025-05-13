5657e77bc7c977e37eef586c40c18bc7acba9f10
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-13 16:55:29 +0200

    test python sdk with connect to running secured sandbox

--- packages/python-sdk/tests/async/sandbox_async/test_secure.py
+++ packages/python-sdk/tests/async/sandbox_async/test_secure.py
@@ -11,3 +11,18 @@ async def test_start_secured(template):
         assert sbx._envd_access_token is not None
     finally:
         await sbx.kill()
+
+
+async def test_connect_to_secured(template):
+    sbx = await AsyncSandbox.create(template, timeout=100, secure=True)
+    try:
+        assert await sbx.is_running()
+        assert sbx._envd_version is not None
+        assert sbx._envd_access_token is not None
+
+        sbx_connection = await AsyncSandbox.connect(sbx.sandbox_id)
+        assert await sbx_connection.is_running()
+        assert sbx_connection._envd_version is not None
+        assert sbx_connection._envd_access_token is not None
+    finally:
+        await sbx.kill()
