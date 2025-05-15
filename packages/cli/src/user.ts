325eea01408e54ddc43013d3e6563c45ae7f2919
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-05-15 16:57:50 +0200

    improve: user config deprecation notice

--- packages/cli/src/user.ts
+++ packages/cli/src/user.ts
@@ -2,19 +2,32 @@ import * as os from 'os'
 import * as path from 'path'
 import * as fs from 'fs'
 
+/**
+ * User configuration stored in ~/.e2b/config.json
+ */
 export interface UserConfig {
   email: string
   accessToken: string
-  defaultTeamApiKey?: string
-  defaultTeamId?: string
   teamName: string
   teamId: string
   teamApiKey: string
   dockerProxySet?: boolean
+
+  /**
+   * @deprecated Kept for backward compatibility. Use {@link UserConfig.teamApiKey} instead.
+   */
+  defaultTeamApiKey?: string
+
+  /**
+   * @deprecated Kept for backward compatibility. Use {@link UserConfig.teamId} instead.
+   */
+  defaultTeamId?: string
 }
 
 export const USER_CONFIG_PATH = path.join(os.homedir(), '.e2b', 'config.json') // TODO: Keep in Keychain
-export const DOCS_BASE = process.env.E2B_DOCS_BASE || `https://${process.env.E2B_DOMAIN || 'e2b.dev'}/docs`
+export const DOCS_BASE =
+  process.env.E2B_DOCS_BASE ||
+  `https://${process.env.E2B_DOMAIN || 'e2b.dev'}/docs`
 
 export function getUserConfig(): UserConfig | null {
   if (!fs.existsSync(USER_CONFIG_PATH)) return null
