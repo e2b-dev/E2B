292b9ee8a87c2e64aac1b1d59b9962bc2072b5e4
Mish <10400064+mishushakov@users.noreply.github.com>
2025-05-20 13:24:26 +0200

    fixes flaky tests

--- packages/python-sdk/tests/async/sandbox_async/files/test_info.py
+++ packages/python-sdk/tests/async/sandbox_async/files/test_info.py
@@ -16,7 +16,7 @@ async def test_get_info_of_file(async_sandbox: AsyncSandbox):
     assert info.path == f"{current_path.stdout.strip()}/{filename}"
     assert info.size == 4
     assert info.mode == 0o644
-    assert info.permissions == "rw-r--r--"
+    assert info.permissions == "-rw-r--r--"
     assert info.owner == "user"
     assert info.group == "user"
     assert info.modified_time is not None
@@ -43,7 +43,7 @@ async def test_get_info_of_directory(async_sandbox: AsyncSandbox):
     assert info.path == f"{current_path.stdout.strip()}/{dirname}"
     assert info.size == 0
     assert info.mode == 0o755
-    assert info.permissions == "rwxr-xr-x"
+    assert info.permissions == "drwxr-xr-x"
     assert info.owner == "user"
     assert info.group == "user"
     assert info.modified_time is not None
