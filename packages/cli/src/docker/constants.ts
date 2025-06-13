49619e61018e256036e2d57dcc805618c427e4c8
Alessio Fanelli <fanahova@gmail.com>
2025-06-13 11:14:28 -0700

    Update constants.ts

--- packages/cli/src/docker/constants.ts
+++ packages/cli/src/docker/constants.ts
@@ -2,7 +2,7 @@ export const defaultDockerfileName = 'e2b.Dockerfile'
 export const fallbackDockerfileName = 'Dockerfile'
 
 export const basicDockerfile = `# You can use most Debian-based base images
-FROM ubuntu:22.04
+FROM e2bdev/code-interpreter:latest
 
 # Install dependencies and customize sandbox
 `
