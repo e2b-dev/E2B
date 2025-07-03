f30ba2fef5d2386286fe21184a0944284d0b7921
0div <tgft73@gmail.com>
2025-07-03 12:28:17 -0700

    add tests for default sandbox env vars and more advance env var tests for scopped commands in python-sdk

--- packages/python-sdk/tests/async/sandbox_async/commands/test_env_vars.py
+++ packages/python-sdk/tests/async/sandbox_async/commands/test_env_vars.py
@@ -16,3 +16,45 @@ async def test_sandbox_envs(template):
         assert cmd.stdout.strip() == "bar"
     finally:
         await sbx.kill()
+
+
+async def test_bash_command_scoped_env_vars(async_sandbox: AsyncSandbox):
+    cmd = await async_sandbox.commands.run("echo $FOO", envs={"FOO": "bar"})
+    assert cmd.exit_code == 0
+    assert cmd.stdout.strip() == "bar"
+
+    # test that it is secure and not accessible to subsequent commands
+    cmd2 = await async_sandbox.commands.run('sudo echo "$FOO"')
+    assert cmd2.exit_code == 0
+    assert cmd2.stdout.strip() == ""
+
+
+async def test_python_command_scoped_env_vars(async_sandbox: AsyncSandbox):
+    cmd = await async_sandbox.commands.run(
+        "python3 -c \"import os; print(os.environ['FOO'])\"", envs={"FOO": "bar"}
+    )
+    assert cmd.exit_code == 0
+    assert cmd.stdout.strip() == "bar"
+
+
+@pytest.mark.skip_debug()
+async def test_default_env_vars(template):
+    try:
+        sandbox = await AsyncSandbox.create(template)
+        cmd = await sandbox.commands.run('echo "$E2B_SANDBOX"')
+        assert cmd.exit_code == 0
+        assert cmd.stdout.strip() == "true"
+
+        cmd2 = await sandbox.commands.run("cat /run/e2b/.E2B_SANDBOX")
+        assert cmd2.exit_code == 0
+        assert cmd2.stdout.strip() == "true"
+
+        cmd3 = await sandbox.commands.run('echo "$E2B_SANDBOX_ID"')
+        assert cmd3.exit_code == 0
+        assert cmd3.stdout.strip() == sandbox.sandbox_id.split("-")[0]
+
+        cmd4 = await sandbox.commands.run("cat /run/e2b/.E2B_SANDBOX_ID")
+        assert cmd4.exit_code == 0
+        assert cmd4.stdout.strip() == sandbox.sandbox_id.split("-")[0]
+    finally:
+        await sandbox.kill()
--- packages/python-sdk/tests/sync/sandbox_sync/commands/test_env_vars.py
+++ packages/python-sdk/tests/sync/sandbox_sync/commands/test_env_vars.py
@@ -16,3 +16,45 @@ def test_sandbox_envs(template):
         assert cmd.stdout.strip() == "bar"
     finally:
         sandbox.kill()
+
+
+def test_bash_command_scoped_env_vars(sandbox: Sandbox):
+    cmd = sandbox.commands.run("echo $FOO", envs={"FOO": "bar"})
+    assert cmd.exit_code == 0
+    assert cmd.stdout.strip() == "bar"
+
+    # test that it is secure and not accessible to subsequent commands
+    cmd2 = sandbox.commands.run('sudo echo "$FOO"')
+    assert cmd2.exit_code == 0
+    assert cmd2.stdout.strip() == ""
+
+
+def test_python_command_scoped_env_vars(sandbox: Sandbox):
+    cmd = sandbox.commands.run(
+        "python3 -c \"import os; print(os.environ['FOO'])\"", envs={"FOO": "bar"}
+    )
+    assert cmd.exit_code == 0
+    assert cmd.stdout.strip() == "bar"
+
+
+@pytest.mark.skip_debug()
+def test_default_env_vars(template):
+    sandbox = Sandbox(template)
+    try:
+        cmd = sandbox.commands.run('echo "$E2B_SANDBOX"')
+        assert cmd.exit_code == 0
+        assert cmd.stdout.strip() == "true"
+
+        cmd2 = sandbox.commands.run("cat /run/e2b/.E2B_SANDBOX")
+        assert cmd2.exit_code == 0
+        assert cmd2.stdout.strip() == "true"
+
+        cmd3 = sandbox.commands.run('echo "$E2B_SANDBOX_ID"')
+        assert cmd3.exit_code == 0
+        assert cmd3.stdout.strip() == sandbox.sandbox_id.split("-")[0]
+
+        cmd4 = sandbox.commands.run("cat /run/e2b/.E2B_SANDBOX_ID")
+        assert cmd4.exit_code == 0
+        assert cmd4.stdout.strip() == sandbox.sandbox_id.split("-")[0]
+    finally:
+        sandbox.kill()
