c95e391438d2ef20671f74393e9f2d242a65d339
Jakub Dobry <jakub.dobry8@gmail.com>
2025-08-28 10:16:41 +0200

    add user and workdir for backwards compatibility

--- packages/cli/tests/commands/template/fixtures/custom-commands/expected/python-sync/template.py
+++ packages/cli/tests/commands/template/fixtures/custom-commands/expected/python-sync/template.py
@@ -5,5 +5,7 @@ template = (
     .from_image("node:18")
     .set_workdir("/app")
     .copy("server.js", ".")
+    .set_user("root")
+    .set_workdir("/home/user")
     .set_start_cmd("node server.js", "curl -f http://localhost:3000/health || exit 1")
 )
