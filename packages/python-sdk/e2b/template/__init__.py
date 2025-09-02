859851d8a369fa83ba0411e27bcfe1e57b118df3
Mish <10400064+mishushakov@users.noreply.github.com>
2025-09-02 18:27:31 +0200

    remove unnecessary python exports

--- packages/python-sdk/e2b/template/__init__.py
+++ packages/python-sdk/e2b/template/__init__.py
@@ -3,15 +3,10 @@ from e2b.template.main import (
     TemplateFinal,
     TemplateBase,
 )
-from e2b.template.types import CopyItem, Instruction, Step, TemplateType
 from e2b.template_sync import Template
 from e2b.template_async import AsyncTemplate
 
 __all__ = [
-    "CopyItem",
-    "Instruction",
-    "Step",
-    "TemplateType",
     "Template",
     "AsyncTemplate",
     "TemplateBuilder",
