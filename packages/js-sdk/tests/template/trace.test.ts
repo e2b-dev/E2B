a6c323bda2b0589b161e90da69ff0d22c1d982ef
Mish <10400064+mishushakov@users.noreply.github.com>
2025-09-09 20:16:06 +0200

    updated test

--- packages/js-sdk/tests/template/trace.test.ts
+++ packages/js-sdk/tests/template/trace.test.ts
@@ -6,6 +6,8 @@ test('build template', { timeout: 180000 }, async () => {
   const template = Template()
     .fromImage('ubuntu:22.04')
     .runCmd('cat folder/test.txt')
+    .setWorkdir('/app')
+    .setStartCmd('echo "Hello, world!"', waitForTimeout(10_000))
 
   try {
     await Template.build(template, {
