2a346bfcd2a2f71bf50370e9ca67bb2dc2e2efd3
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2025-09-12 21:47:07 +0200

    Added format option to CLI sandbox + templates list (#903)
    This PR is based on https://github.com/e2b-dev/E2B/pull/902, please
    proceed with that first
    
    **Changelog**
    
    - Added `--format` option to "e2b sandbox list" and "e2b template list".
    Can either be "table" or "json"
    
    Sandboxes list in JSON format
    
    <img width="471" height="341" alt="Screenshot 2025-09-12 at 11 17 45"
    src="https://github.com/user-attachments/assets/13455b62-5efb-4b11-8fe3-a39b3fb6dffc"
    />
    
    Templates list in JSON format
    
    <img width="449" height="377" alt="Screenshot 2025-09-12 at 11 18 55"
    src="https://github.com/user-attachments/assets/47fa3c2b-2e4c-4854-a87f-65c325261521"
    />

---
+++ .changeset/popular-badgers-hear.md
@@ -0,0 +1,5 @@
+---
+'@e2b/cli': minor
+---
+
+added json output format to template list and sandbox list commands
--- packages/cli/src/commands/sandbox/list.ts
+++ packages/cli/src/commands/sandbox/list.ts
@@ -5,6 +5,14 @@ import { components, Sandbox, SandboxInfo } from 'e2b'
 import { ensureAPIKey } from 'src/api'
 import { parseMetadata } from './utils'
 
+function getStateTitle(state?: components['schemas']['SandboxState'][]) {
+  if (state?.length === 1) {
+    if (state?.includes('running')) return 'Running sandboxes'
+    if (state?.includes('paused')) return 'Paused sandboxes'
+  }
+  return 'Sandboxes'
+}
+
 export const listCommand = new commander.Command('list')
   .description('list all sandboxes, by default it list only running ones')
   .alias('ls')
@@ -19,86 +27,102 @@ export const listCommand = new commander.Command('list')
     'limit the number of sandboxes returned',
     (value) => parseInt(value)
   )
+  .option('-f, --format <format>', 'output format, eg. json, table')
   .action(async (options) => {
     try {
       const state = options.state || ['running']
+      const format = options.format || 'table'
       const sandboxes = await listSandboxes({
         limit: options.limit,
         state,
         metadataRaw: options.metadata,
       })
 
-      if (!sandboxes?.length) {
-        console.log('No sandboxes found')
+      if (format === 'table') {
+        renderTable(sandboxes, state)
+      } else if (format === 'json') {
+        console.log(JSON.stringify(sandboxes, null, 2))
       } else {
-        const table = new tablePrinter.Table({
-          title: 'Running sandboxes',
-          columns: [
-            { name: 'sandboxID', alignment: 'left', title: 'Sandbox ID' },
-            {
-              name: 'templateID',
-              alignment: 'left',
-              title: 'Template ID',
-              maxLen: 20,
-            },
-            { name: 'alias', alignment: 'left', title: 'Alias' },
-            { name: 'startedAt', alignment: 'left', title: 'Started at' },
-            { name: 'endAt', alignment: 'left', title: 'End at' },
-            { name: 'state', alignment: 'left', title: 'State' },
-            { name: 'cpuCount', alignment: 'left', title: 'vCPUs' },
-            { name: 'memoryMB', alignment: 'left', title: 'RAM MiB' },
-            { name: 'metadata', alignment: 'left', title: 'Metadata' },
-          ],
-          disabledColumns: ['clientID'],
-          rows: sandboxes
-            .map((sandbox) => ({
-              ...sandbox,
-              sandboxID: sandbox.sandboxId,
-              startedAt: new Date(sandbox.startedAt).toLocaleString(),
-              endAt: new Date(sandbox.endAt).toLocaleString(),
-              state:
-                sandbox.state.charAt(0).toUpperCase() + sandbox.state.slice(1), // capitalize
-              metadata: JSON.stringify(sandbox.metadata),
-            }))
-            .sort(
-              (a, b) =>
-                a.startedAt.localeCompare(b.startedAt) ||
-                a.sandboxID.localeCompare(b.sandboxID)
-            ),
-          style: {
-            headerTop: {
-              left: '',
-              right: '',
-              mid: '',
-              other: '',
-            },
-            headerBottom: {
-              left: '',
-              right: '',
-              mid: '',
-              other: '',
-            },
-            tableBottom: {
-              left: '',
-              right: '',
-              mid: '',
-              other: '',
-            },
-            vertical: '',
-          },
-          colorMap: {
-            orange: '\x1b[38;5;216m',
-          },
-        })
-        table.printTable()
-
-        process.stdout.write('\n')
+        console.error(`Unsupported output format: ${format}`)
+        process.exit(1)
       }
     } catch (err: any) {
       console.error(err)
       process.exit(1)
     }
   })
+
+function renderTable(
+  sandboxes: SandboxInfo[],
+  state: components['schemas']['SandboxState'][]
+) {
+  if (!sandboxes?.length) {
+    console.log('No sandboxes found')
+    return
+  }
+
+  const table = new tablePrinter.Table({
+    title: getStateTitle(state),
+    columns: [
+      { name: 'sandboxId', alignment: 'left', title: 'Sandbox ID' },
+      {
+        name: 'templateId',
+        alignment: 'left',
+        title: 'Template ID',
+        maxLen: 20,
+      },
+      { name: 'name', alignment: 'left', title: 'Alias' },
+      { name: 'startedAt', alignment: 'left', title: 'Started at' },
+      { name: 'endAt', alignment: 'left', title: 'End at' },
+      { name: 'state', alignment: 'left', title: 'State' },
+      { name: 'cpuCount', alignment: 'left', title: 'vCPUs' },
+      { name: 'memoryMB', alignment: 'left', title: 'RAM MiB' },
+      { name: 'envdVersion', alignment: 'left', title: 'Envd version' },
+      { name: 'metadata', alignment: 'left', title: 'Metadata' },
+    ],
+    disabledColumns: ['clientID'],
+    rows: sandboxes
+      .map((sandbox) => ({
+        ...sandbox,
+        startedAt: new Date(sandbox.startedAt).toLocaleString(),
+        endAt: new Date(sandbox.endAt).toLocaleString(),
+        state: sandbox.state.charAt(0).toUpperCase() + sandbox.state.slice(1), // capitalize
+        metadata: JSON.stringify(sandbox.metadata),
+      }))
+      .sort(
+        (a, b) =>
+          a.startedAt.localeCompare(b.startedAt) ||
+          a.sandboxId.localeCompare(b.sandboxId)
+      ),
+    style: {
+      headerTop: {
+        left: '',
+        right: '',
+        mid: '',
+        other: '',
+      },
+      headerBottom: {
+        left: '',
+        right: '',
+        mid: '',
+        other: '',
+      },
+      tableBottom: {
+        left: '',
+        right: '',
+        mid: '',
+        other: '',
+      },
+      vertical: '',
+    },
+    colorMap: {
+      orange: '\x1b[38;5;216m',
+    },
+  })
+  table.printTable()
+
+  process.stdout.write('\n')
+}
 
 type ListSandboxesOptions = {
   limit?: number
