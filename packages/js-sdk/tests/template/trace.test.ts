85c8f58f0e766b28e62d5f0f98bd056ee245b410
Mish <10400064+mishushakov@users.noreply.github.com>
2025-09-15 19:09:00 +0200

    improved traces test wip

--- packages/js-sdk/tests/template/trace.test.ts
+++ packages/js-sdk/tests/template/trace.test.ts
@@ -1,13 +1,30 @@
 import { assert, test } from 'vitest'
 import { Template, waitForTimeout } from '../../src'
 import { randomUUID } from 'node:crypto'
+import fs from 'node:fs'
 
-test('build template', { timeout: 180000 }, async () => {
-  const template = Template()
-    .fromImage('ubuntu:22.04')
-    .runCmd('cat folder/test.txt')
-    .setWorkdir('/app')
-    .setStartCmd('echo "Hello, world!"', waitForTimeout(10_000))
+// read current file content
+const __fileContent = fs.readFileSync(__filename, 'utf8')
+
+function getStackTraceCallerMethod(fileContent: string, stackTrace: string) {
+  const stackTraceLines = stackTrace.split('\n')
+  const callerTrace = stackTraceLines[0]
+
+  const [, line, column] = callerTrace.split(':')
+  const lineNumber = parseInt(line)
+  const columnNumber = parseInt(column)
+
+  const lines = fileContent.split('\n')
+  const parsedLine = lines[lineNumber - 1].slice(columnNumber - 1)
+  const match = parsedLine.match(/^(\w+)\s*\(/)
+  if (match) {
+    return match[1]
+  }
+  return null
+}
+
+test('traces on fromImage', { timeout: 180000 }, async () => {
+  const template = Template().fromImage('e2b.dev/this-image-does-not-exist')
 
   try {
     await Template.build(template, {
@@ -16,6 +33,28 @@ test('build template', { timeout: 180000 }, async () => {
       memoryMB: 1024,
     })
   } catch (error) {
-    assert(error.stack.split('\n')[0].includes(__filename))
+    assert.include(
+      getStackTraceCallerMethod(__fileContent, error.stack),
+      'fromImage'
+    )
+  }
+})
+
+test('traces on runCmd', { timeout: 180000 }, async () => {
+  const template = Template()
+    .fromImage('ubuntu:22.04')
+    .runCmd('cat folder/test.txt')
+
+  try {
+    await Template.build(template, {
+      alias: randomUUID(),
+      cpuCount: 1,
+      memoryMB: 1024,
+    })
+  } catch (error) {
+    assert.include(
+      getStackTraceCallerMethod(__fileContent, error.stack),
+      'runCmd'
+    )
   }
 })
