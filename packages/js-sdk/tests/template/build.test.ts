97ce0ebfa2b81f972eda56168bdafb5b17d71c8f
Mish <10400064+mishushakov@users.noreply.github.com>
2025-09-26 17:40:55 +0200

    symlink tests + fixture

--- packages/js-sdk/tests/template/build.test.ts
+++ packages/js-sdk/tests/template/build.test.ts
@@ -1,31 +1,72 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll } from 'vitest'
+import { buildTemplateTest } from '../setup'
 import { Template, waitForTimeout } from '../../src'
-import { randomUUID } from 'node:crypto'
 import path from 'node:path'
 import fs from 'node:fs'
 
-test('build template', { timeout: 180000 }, async () => {
-  const folderPath = path.join(__dirname, 'folder')
+const folderPath = path.join(__dirname, 'folder')
+
+beforeAll(async () => {
   fs.mkdirSync(folderPath, { recursive: true })
   fs.writeFileSync(path.join(folderPath, 'test.txt'), 'This is a test file.')
 
-  const template = Template()
-    .fromImage('ubuntu:22.04')
-    .copy('folder/*.txt', 'folder', { forceUpload: true })
-    .copy('folder', 'folder2', { forceUpload: true })
-    .setEnvs({
-      ENV_1: 'value1',
-      ENV_2: 'value2',
-    })
-    .runCmd('cat folder/test.txt')
-    .setWorkdir('/app')
-    .setStartCmd('echo "Hello, world!"', waitForTimeout(10_000))
+  // Create relative symlink
+  fs.symlinkSync('test.txt', path.join(folderPath, 'symlink.txt'))
 
-  await Template.build(template, {
-    alias: randomUUID(),
-    cpuCount: 1,
-    memoryMB: 1024,
-  })
+  // Create absolute symlink
+  fs.symlinkSync(
+    path.join(folderPath, 'test.txt'),
+    path.join(folderPath, 'symlink2.txt')
+  )
 
+  // Create a symlink to a file that does not exist
+  fs.symlinkSync('12345test.txt', path.join(folderPath, 'symlink3.txt'))
+})
+
+afterAll(() => {
   fs.rmSync(folderPath, { recursive: true })
 })
+
+buildTemplateTest(
+  'build template',
+  { timeout: 180000 },
+  async ({ buildTemplate }) => {
+    const template = Template()
+      .fromImage('ubuntu:22.04')
+      .copy('folder/*', 'folder', { forceUpload: true })
+      .runCmd('cat folder/test.txt')
+      .setWorkdir('/app')
+      .setStartCmd('echo "Hello, world!"', waitForTimeout(10_000))
+
+    await buildTemplate(template)
+  }
+)
+
+buildTemplateTest(
+  'build template with symlinks',
+  { timeout: 180000 },
+  async ({ buildTemplate }) => {
+    const template = Template()
+      .fromImage('ubuntu:22.04')
+      .copy('folder/*', 'folder', { forceUpload: true })
+      .runCmd('cat folder/symlink.txt')
+
+    await buildTemplate(template)
+  }
+)
+
+buildTemplateTest(
+  'build template with resolveSymlinks',
+  { timeout: 180000 },
+  async ({ buildTemplate }) => {
+    const template = Template()
+      .fromImage('ubuntu:22.04')
+      .copy('folder/symlink.txt', 'folder/symlink.txt', {
+        forceUpload: true,
+        resolveSymlinks: true,
+      })
+      .runCmd('cat folder/symlink.txt')
+
+    await buildTemplate(template)
+  }
+)
