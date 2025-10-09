544c9bdf39730b01f732e17ceff4c019a1948c3f
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-10-08 17:22:12 -0700

    Automate JS SDK mcp spec generation

--- packages/js-sdk/src/sandbox/sandboxApi.ts
+++ packages/js-sdk/src/sandbox/sandboxApi.ts
@@ -7,7 +7,7 @@ import {
 import { compareVersions } from 'compare-versions'
 import { NotFoundError, TemplateError } from '../errors'
 import { timeoutToSeconds } from '../utils'
-import { McpServer } from './mcp'
+import type { McpServer } from './mcp'
 
 /**
  * Options for request to the Sandbox API.
@@ -18,7 +18,7 @@ export interface SandboxApiOpts
       ConnectionOpts,
       'apiKey' | 'headers' | 'debug' | 'domain' | 'requestTimeoutMs'
     >
-  > {}
+  > { }
 
 /**
  * Options for creating a new Sandbox.
@@ -224,7 +224,7 @@ export interface SandboxMetrics {
 }
 
 export class SandboxApi {
-  protected constructor() {}
+  protected constructor() { }
 
   /**
    * Kill the sandbox specified by sandbox ID.
@@ -477,7 +477,7 @@ export class SandboxApi {
       await this.kill(res.data!.sandboxID, opts)
       throw new TemplateError(
         'You need to update the template to use the new SDK. ' +
-          'You can do this by running `e2b template build` in the directory with the template.'
+        'You can do this by running `e2b template build` in the directory with the template.'
       )
     }
 
