cba88c1c5429280fe89433ea39498356518af4ca
Joe Lombrozo <joe@djeebus.net>
2025-10-29 17:08:05 -0700

    remove some "with" lines

--- packages/python-sdk/e2b/template_sync/main.py
+++ packages/python-sdk/e2b/template_sync/main.py
@@ -78,7 +78,7 @@ class Template(TemplateBase):
             config = ConnectionConfig(
                 domain=domain, api_key=api_key or os.environ.get("E2B_API_KEY")
             )
-            client = ApiClient(
+            api_client = ApiClient(
                 config,
                 require_api_key=True,
                 require_access_token=False,
@@ -89,135 +89,130 @@ class Template(TemplateBase):
             if skip_cache:
                 template._template._force = True
 
-            with client as api_client:
-                # Create template
-                if on_build_logs:
-                    on_build_logs(
-                        LogEntry(
-                            timestamp=datetime.now(),
-                            level="info",
-                            message=f"Requesting build for template: {alias}",
-                        )
+            # Create template
+            if on_build_logs:
+                on_build_logs(
+                    LogEntry(
+                        timestamp=datetime.now(),
+                        level="info",
+                        message=f"Requesting build for template: {alias}",
                     )
-
-                response = request_build(
-                    api_client,
-                    name=alias,
-                    cpu_count=cpu_count,
-                    memory_mb=memory_mb,
                 )
 
-                template_id = response.template_id
-                build_id = response.build_id
+            response = request_build(
+                api_client,
+                name=alias,
+                cpu_count=cpu_count,
+                memory_mb=memory_mb,
+            )
 
-                if on_build_logs:
-                    on_build_logs(
-                        LogEntry(
-                            timestamp=datetime.now(),
-                            level="info",
-                            message=f"Template created with ID: {template_id}, Build ID: {build_id}",
-                        )
+            template_id = response.template_id
+            build_id = response.build_id
+
+            if on_build_logs:
+                on_build_logs(
+                    LogEntry(
+                        timestamp=datetime.now(),
+                        level="info",
+                        message=f"Template created with ID: {template_id}, Build ID: {build_id}",
                     )
-
-                instructions_with_hashes = (
-                    template._template._instructions_with_hashes()
                 )
 
-                # Upload files
-                for index, file_upload in enumerate(instructions_with_hashes):
-                    if file_upload["type"] != InstructionType.COPY:
-                        continue
+            instructions_with_hashes = template._template._instructions_with_hashes()
 
-                    args = file_upload.get("args", [])
-                    src = args[0] if len(args) > 0 else None
-                    force_upload = file_upload.get("forceUpload")
-                    files_hash = file_upload.get("filesHash", None)
-                    resolve_symlinks = file_upload.get(
-                        "resolveSymlinks", RESOLVE_SYMLINKS
+            # Upload files
+            for index, file_upload in enumerate(instructions_with_hashes):
+                if file_upload["type"] != InstructionType.COPY:
+                    continue
+
+                args = file_upload.get("args", [])
+                src = args[0] if len(args) > 0 else None
+                force_upload = file_upload.get("forceUpload")
+                files_hash = file_upload.get("filesHash", None)
+                resolve_symlinks = file_upload.get("resolveSymlinks", RESOLVE_SYMLINKS)
+
+                if src is None or files_hash is None:
+                    raise ValueError("Source path and files hash are required")
+
+                stack_trace = None
+                if index + 1 < len(template._template._stack_traces):
+                    stack_trace = template._template._stack_traces[index + 1]
+
+                file_info = get_file_upload_link(
+                    api_client, template_id, files_hash, stack_trace
+                )
+
+                if (force_upload and file_info.url) or (
+                    file_info.present is False and file_info.url
+                ):
+                    upload_file(
+                        src,
+                        template._template._file_context_path,
+                        file_info.url,
+                        resolve_symlinks,
+                        stack_trace,
                     )
-
-                    if src is None or files_hash is None:
-                        raise ValueError("Source path and files hash are required")
-
-                    stack_trace = None
-                    if index + 1 < len(template._template._stack_traces):
-                        stack_trace = template._template._stack_traces[index + 1]
-
-                    file_info = get_file_upload_link(
-                        api_client, template_id, files_hash, stack_trace
-                    )
-
-                    if (force_upload and file_info.url) or (
-                        file_info.present is False and file_info.url
-                    ):
-                        upload_file(
-                            src,
-                            template._template._file_context_path,
-                            file_info.url,
-                            resolve_symlinks,
-                            stack_trace,
-                        )
-                        if on_build_logs:
-                            on_build_logs(
-                                LogEntry(
-                                    timestamp=datetime.now(),
-                                    level="info",
-                                    message=f"Uploaded '{src}'",
-                                )
+                    if on_build_logs:
+                        on_build_logs(
+                            LogEntry(
+                                timestamp=datetime.now(),
+                                level="info",
+                                message=f"Uploaded '{src}'",
                             )
-                    else:
-                        if on_build_logs:
-                            on_build_logs(
-                                LogEntry(
-                                    timestamp=datetime.now(),
-                                    level="info",
-                                    message=f"Skipping upload of '{src}', already cached",
-                                )
+                        )
+                else:
+                    if on_build_logs:
+                        on_build_logs(
+                            LogEntry(
+                                timestamp=datetime.now(),
+                                level="info",
+                                message=f"Skipping upload of '{src}', already cached",
                             )
-
-                if on_build_logs:
-                    on_build_logs(
-                        LogEntry(
-                            timestamp=datetime.now(),
-                            level="info",
-                            message="All file uploads completed",
                         )
-                    )
 
-                # Start build
-                if on_build_logs:
-                    on_build_logs(
-                        LogEntry(
-                            timestamp=datetime.now(),
-                            level="info",
-                            message="Starting building...",
-                        )
+            if on_build_logs:
+                on_build_logs(
+                    LogEntry(
+                        timestamp=datetime.now(),
+                        level="info",
+                        message="All file uploads completed",
                     )
-
-                trigger_build(
-                    api_client,
-                    template_id,
-                    build_id,
-                    template._template._serialize(instructions_with_hashes),
                 )
 
-                if on_build_logs:
-                    on_build_logs(
-                        LogEntry(
-                            timestamp=datetime.now(),
-                            level="info",
-                            message="Waiting for logs...",
-                        )
+            # Start build
+            if on_build_logs:
+                on_build_logs(
+                    LogEntry(
+                        timestamp=datetime.now(),
+                        level="info",
+                        message="Starting building...",
                     )
-
-                wait_for_build_finish(
-                    api_client,
-                    template_id,
-                    build_id,
-                    on_build_logs,
-                    logs_refresh_frequency=TemplateBase._logs_refresh_frequency,
-                    stack_traces=template._template._stack_traces,
                 )
+
+            trigger_build(
+                api_client,
+                template_id,
+                build_id,
+                template._template._serialize(instructions_with_hashes),
+            )
+
+            if on_build_logs:
+                on_build_logs(
+                    LogEntry(
+                        timestamp=datetime.now(),
+                        level="info",
+                        message="Waiting for logs...",
+                    )
+                )
+
+            wait_for_build_finish(
+                api_client,
+                template_id,
+                build_id,
+                on_build_logs,
+                logs_refresh_frequency=TemplateBase._logs_refresh_frequency,
+                stack_traces=template._template._stack_traces,
+            )
         finally:
             if on_build_logs:
                 on_build_logs(
