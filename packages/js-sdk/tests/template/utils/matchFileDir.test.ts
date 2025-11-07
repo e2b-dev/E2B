5f79d23c605078f8fa5d327ce8dfea5564db2db2
Mish <10400064+mishushakov@users.noreply.github.com>
2025-11-07 19:37:44 +0100

    handle windows file patterns in default context directory

--- packages/js-sdk/tests/template/utils/matchFileDir.test.ts
+++ packages/js-sdk/tests/template/utils/matchFileDir.test.ts
@@ -8,3 +8,23 @@ test('matchFileDir', () => {
 test('matchFileDir (anonymous)', () => {
   expect(matchFileDir('at <anonymous> (/path/to/file.js:1:1)')).toBe('/path/to')
 })
+
+test('matchFileDir Windows path', () => {
+  expect(matchFileDir('at C:/path/to/file.js:1:1')).toBe('C:/path/to')
+})
+
+test('matchFileDir Windows path (anonymous)', () => {
+  expect(matchFileDir('at <anonymous> (C:/path/to/file.js:1:1)')).toBe(
+    'C:/path/to'
+  )
+})
+
+test('matchFileDir file:// protocol Windows', () => {
+  expect(matchFileDir('at <anonymous> (file:///C:/path/to/file.js:1:1)')).toBe(
+    'C:/path/to'
+  )
+})
+
+test('matchFileDir file:// protocol Windows (no anonymous)', () => {
+  expect(matchFileDir('at (file:///C:/path/to/file.js:1:1)')).toBe('C:/path/to')
+})
