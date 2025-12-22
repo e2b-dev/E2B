1ebc6dfae86e4fee9d5fc5e3ef102f8746f98f61
Berry <105915738+RatArt@users.noreply.github.com>
2025-12-22 18:48:45 +0100

    Template env vars (#1052)
    Add a note to the template environment variables that they exist only
    during template build, so users don't get confused.
    
    ---------
    
    Co-authored-by: Mish <10400064+mishushakov@users.noreply.github.com>

--- packages/python-sdk/e2b/template/main.py
+++ packages/python-sdk/e2b/template/main.py
@@ -633,7 +633,8 @@ class TemplateBuilder:
 
     def set_envs(self, envs: Dict[str, str]) -> "TemplateBuilder":
         """
-        Set environment variables in the template.
+        Set environment variables.
+        Note: Environment variables defined here are available only during template build.
 
         :param envs: Dictionary of environment variable names and values
 
