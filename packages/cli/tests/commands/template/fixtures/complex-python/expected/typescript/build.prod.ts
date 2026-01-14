626280dddb8d8a590ab0ea8d8e08a2b1d0489430
Jakub Dobry <jakub.dobry8@gmail.com>
2026-01-14 15:01:42 +0100

    update the CLI to use the new template build format

--- packages/cli/tests/commands/template/fixtures/complex-python/expected/typescript/build.prod.ts
+++ packages/cli/tests/commands/template/fixtures/complex-python/expected/typescript/build.prod.ts
@@ -2,8 +2,7 @@ import { Template, defaultBuildLogger } from 'e2b'
 import { template } from './template'
 
 async function main() {
-  await Template.build(template, {
-    alias: 'complex-python-app',
+  await Template.build(template, 'complex-python-app', {
     onBuildLogs: defaultBuildLogger(),
   });
 }
--- packages/cli/tests/commands/template/fixtures/custom-commands/expected/python-async/build_prod.py
+++ packages/cli/tests/commands/template/fixtures/custom-commands/expected/python-async/build_prod.py
@@ -6,7 +6,7 @@ from .template import template
 async def main():
     await AsyncTemplate.build(
         template,
-        alias="custom-app",
+        "custom-app",
         on_build_logs=default_build_logger(),
     )
 
