193471366608e4bcb4425729bc41d9d0d85b0678
Jakub Novak <jakub@e2b.dev>
2026-01-22 16:46:33 +0100

    Fix pytest skipif

--- packages/python-sdk/tests/shared/template/utils/test_is_safe_relative.py
+++ packages/python-sdk/tests/shared/template/utils/test_is_safe_relative.py
@@ -14,11 +14,11 @@ class TestParentDirectoryTraversal:
     def test_should_return_true_for_parent_directory_only(self):
         assert is_safe_relative("..") is False
 
-    @pytest.skipif(not os.name == "posix", reason="Unix-specific path tests")
+    @pytest.mark.skipif(not os.name == "posix", reason="Unix-specific path tests")
     def test_should_return_true_for_paths_starting_with_dot_dot_slash(self):
         assert is_safe_relative("../file.txt") is False
 
-    @pytest.skipif(not os.name == "nt", reason="Windows-specific path tests")
+    @pytest.mark.skipif(not os.name == "nt", reason="Windows-specific path tests")
     def test_should_return_true_for_paths_starting_with_dot_dot_backslash(self):
         assert is_safe_relative("..\\file.txt") is False
 
