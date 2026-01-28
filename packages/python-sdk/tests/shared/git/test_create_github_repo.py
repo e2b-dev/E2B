285b085acc3a54fb12cd6765f98701f3338078ea
Matt Brockman <matt.brockman@e2b.dev>
2026-01-28 11:25:07 -0800

    rm github create repo

--- packages/python-sdk/tests/shared/git/test_create_github_repo.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from e2b.exceptions import InvalidArgumentException
-
-
-@pytest.mark.skip_debug()
-def test_create_github_repo_requires_name(git_sandbox):
-    with pytest.raises(InvalidArgumentException):
-        git_sandbox.git.create_github_repo("")
