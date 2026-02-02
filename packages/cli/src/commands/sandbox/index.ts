b8ec694472d4bda49132b2c4adfc443ec1f9f91a
Matt Brockman <matt.brockman@e2b.dev>
2026-02-02 01:50:23 -0800

    cli: create-headless just gets a box id, doesn't open shell

--- packages/cli/src/commands/sandbox/index.ts
+++ packages/cli/src/commands/sandbox/index.ts
@@ -4,6 +4,7 @@ import { connectCommand } from './connect'
 import { listCommand } from './list'
 import { killCommand } from './kill'
 import { createCommand } from './create'
+import { createHeadlessCommand } from './create_headless'
 import { logsCommand } from './logs'
 import { metricsCommand } from './metrics'
 import { execCommand } from './exec'
@@ -16,6 +17,7 @@ export const sandboxCommand = new commander.Command('sandbox')
   .addCommand(killCommand)
   .addCommand(createCommand('create', 'cr', false))
   .addCommand(createCommand('spawn', 'sp', true), { hidden: true })
+  .addCommand(createHeadlessCommand)
   .addCommand(logsCommand)
   .addCommand(metricsCommand)
   .addCommand(execCommand)
