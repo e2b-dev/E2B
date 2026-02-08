d1ed417a04ab1722e59adbc13241ec7143ff496d
Matt Brockman <matt.brockman@e2b.dev>
2026-02-08 14:47:16 -0800

    rm headless, that can go in another PR

--- packages/cli/src/commands/sandbox/index.ts
+++ packages/cli/src/commands/sandbox/index.ts
@@ -4,7 +4,6 @@ import { connectCommand } from './connect'
 import { listCommand } from './list'
 import { killCommand } from './kill'
 import { createCommand } from './create'
-import { createHeadlessCommand } from './create_headless'
 import { logsCommand } from './logs'
 import { metricsCommand } from './metrics'
 import { execCommand } from './exec'
@@ -17,7 +16,6 @@ export const sandboxCommand = new commander.Command('sandbox')
   .addCommand(killCommand)
   .addCommand(createCommand('create', 'cr', false))
   .addCommand(createCommand('spawn', 'sp', true), { hidden: true })
-  .addCommand(createHeadlessCommand)
   .addCommand(logsCommand)
   .addCommand(metricsCommand)
   .addCommand(execCommand)
