4fe7564e591c45f4c3767b711a2e39830b5d79c8
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2026-02-10 23:14:26 +0100

    updated volume crud client

--- packages/js-sdk/src/sandbox/sandboxApi.ts
+++ packages/js-sdk/src/sandbox/sandboxApi.ts
@@ -88,7 +88,7 @@ export interface SandboxApiOpts
       ConnectionOpts,
       'apiKey' | 'headers' | 'debug' | 'domain' | 'requestTimeoutMs'
     >
-  > {}
+  > { }
 
 /**
  * Options for creating a new Sandbox.
@@ -153,7 +153,7 @@ export interface SandboxOpts extends ConnectionOpts {
    *
    * @default undefined
    */
-  volumeMounts?: Record<string, { volumeId: string; name: string } | string>
+  volumeMounts?: Record<string, { name: string } | string>
 
   /**
    * Sandbox URL. Used for local development
@@ -384,7 +384,7 @@ function getLifecycle(
 }
 
 export class SandboxApi {
-  protected constructor() {}
+  protected constructor() { }
 
   /**
    * Kill the sandbox specified by sandbox ID.
@@ -767,7 +767,7 @@ export class SandboxApi {
       await this.kill(res.data!.sandboxID, opts)
       throw new TemplateError(
         'You need to update the template to use the new SDK. ' +
-          'You can do this by running `e2b template build` in the directory with the template.'
+        'You can do this by running `e2b template build` in the directory with the template.'
       )
     }
 
