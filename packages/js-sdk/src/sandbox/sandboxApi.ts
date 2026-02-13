12da3134cf1dcb96098c7f71b045369786279f54
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2026-02-13 20:35:20 +0100

    formatting

--- packages/js-sdk/src/sandbox/sandboxApi.ts
+++ packages/js-sdk/src/sandbox/sandboxApi.ts
@@ -88,7 +88,7 @@ export interface SandboxApiOpts
       ConnectionOpts,
       'apiKey' | 'headers' | 'debug' | 'domain' | 'requestTimeoutMs'
     >
-  > { }
+  > {}
 
 /**
  * Options for creating a new Sandbox.
@@ -389,7 +389,7 @@ function getLifecycle(
 }
 
 export class SandboxApi {
-  protected constructor() { }
+  protected constructor() {}
 
   /**
    * Kill the sandbox specified by sandbox ID.
@@ -576,7 +576,7 @@ export class SandboxApi {
       cpuCount: res.data.cpuCount,
       memoryMB: res.data.memoryMB,
       sandboxDomain: res.data.domain || undefined,
-      volumeMounts: res.data.volumeMounts
+      volumeMounts: res.data.volumeMounts,
     }
   }
 
@@ -773,7 +773,7 @@ export class SandboxApi {
       await this.kill(res.data!.sandboxID, opts)
       throw new TemplateError(
         'You need to update the template to use the new SDK. ' +
-        'You can do this by running `e2b template build` in the directory with the template.'
+          'You can do this by running `e2b template build` in the directory with the template.'
       )
     }
 
