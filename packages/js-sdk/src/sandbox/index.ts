4b0e913cca25d41a54b68a87f26b1fee71af2c82
Matt Brockman <matt.brockman@e2b.dev>
2026-05-01 22:31:25 -0700

    Enable HTTP/2 for JS envd requests

--- packages/js-sdk/src/sandbox/index.ts
+++ packages/js-sdk/src/sandbox/index.ts
@@ -8,6 +8,7 @@ import {
   Username,
 } from '../connectionConfig'
 import { EnvdApiClient, handleEnvdApiError } from '../envd/api'
+import { createEnvdFetch } from '../envd/http2'
 import { createRpcLogger } from '../logs'
 import { Commands, Pty } from './commands'
 import { Filesystem } from './filesystem'
@@ -150,6 +151,7 @@ export class Sandbox extends SandboxApi {
       'E2b-Sandbox-Id': this.sandboxId,
       'E2b-Sandbox-Port': this.envdPort.toString(),
     }
+    const envdFetch = createEnvdFetch()
 
     const rpcTransport = createConnectTransport({
       baseUrl: this.envdApiUrl,
@@ -179,7 +181,7 @@ export class Sandbox extends SandboxApi {
           redirect: 'follow',
         }
 
-        return fetch(url, options)
+        return envdFetch(url, options)
       },
     })
 
@@ -195,6 +197,7 @@ export class Sandbox extends SandboxApi {
             ? { 'X-Access-Token': this.envdAccessToken }
             : {}),
         },
+        fetch: (request) => envdFetch(request),
       },
       {
         version: opts.envdVersion,
